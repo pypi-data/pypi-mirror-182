2ee8fb719a48829c05d1fac875e2c7546b29cc2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:01 +0000

    Add package kolibri-ml/1.1.11/kolibri_ml-1.1.11-py3-none-any.whl

---
+++ packages/kolibri-ml/kolibri_ml-1.1.11-py3-none-any.whl/test_backup/dnn/text/test_classification/test_cnn_model.py
@@ -0,0 +1,18 @@
+import unittest
+
+import tests.dnn.text.test_classification.test_bi_lstm_model as base
+from kolibri.backend.tensorflow.tasks.text.classification import CNN_Model
+from kolibri.backend.tensorflow.embeddings import WordEmbedding
+from tests.dnn.text.test_embeddings.test_word_embedding import sample_w2v_path
+
+
+class TestBiGRU_Model(base.TestBiLSTM_Model):
+    @classmethod
+    def setUpClass(cls):
+        cls.EPOCH_COUNT = 1
+        cls.TASK_MODEL_CLASS = CNN_Model
+        cls.w2v_embedding = WordEmbedding(sample_w2v_path)
+
+
+if __name__ == "__main__":
+    unittest.main()
