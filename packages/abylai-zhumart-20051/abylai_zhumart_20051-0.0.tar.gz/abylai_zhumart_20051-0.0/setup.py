90ba8db53b17a5de2d2086e4bdef4702df6aba84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:14 +0000

    Add package abylai-zhumart-20051/0.0/abylai_zhumart_20051-0.0.tar.gz

---
+++ packages/abylai-zhumart-20051/abylai_zhumart_20051-0.0.tar.gz/abylai_zhumart_20051-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='abylai_zhumart_20051',
+      version='0.0',
+      description='Math library',
+      packages=find_packages(include=['abylai_zhumart_20051']),
+      author_email='cstawer2005@mail.ru',
+      zip_safe=False)
