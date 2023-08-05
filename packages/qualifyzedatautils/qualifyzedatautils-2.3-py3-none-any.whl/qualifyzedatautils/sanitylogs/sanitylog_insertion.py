550f600e091952b8d711e2689ea1b4d10f5860dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:45 +0000

    Add package qualifyzedatautils/2.3/qualifyzedatautils-2.3-py3-none-any.whl

---
+++ packages/qualifyzedatautils/qualifyzedatautils-2.3-py3-none-any.whl/qualifyzedatautils/sanitylogs/sanitylog_insertion.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import redshift_connector
+
+def post_sanitylog( redshift_valid_connection, type, source, affected, response="" ):
+
+    insertion_ts = datetime.now()
+
+    try:
+        cursor_sanitylog = redshift_valid_connection.cursor()
+        cursor_sanitylog.execute(
+            'INSERT INTO dev.sanity_logs (type, source, affected, response, log_ts) VALUES (%s, %s, %s, %s, %s)',
+            (type, source, affected, response, insertion_ts))
+        cursor_sanitylog.close()
+        redshift_valid_connection.commit()
+
+        return print("The Sanity Log was successfully inserted!")
+    except:
+        return print("The Sanity Log was not inserted. A problem occurred during the execution of the process. Please review that all your inputs are valid.")
+
