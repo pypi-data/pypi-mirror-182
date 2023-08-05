582c03dd4b51365c5f6957fef411b55fa5bb6606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:10 +0000

    Add package mypy-boto3-kinesisvideo/1.26.33/mypy-boto3-kinesisvideo-1.26.33.tar.gz

---
+++ packages/mypy-boto3-kinesisvideo/mypy-boto3-kinesisvideo-1.26.33.tar.gz/mypy-boto3-kinesisvideo-1.26.33/mypy_boto3_kinesisvideo/__init__.py
@@ -0,0 +1,40 @@
+"""
+Main interface for kinesisvideo service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_kinesisvideo import (
+        Client,
+        DescribeMappedResourceConfigurationPaginator,
+        KinesisVideoClient,
+        ListSignalingChannelsPaginator,
+        ListStreamsPaginator,
+    )
+
+    session = Session()
+    client: KinesisVideoClient = session.client("kinesisvideo")
+
+    describe_mapped_resource_configuration_paginator: DescribeMappedResourceConfigurationPaginator = client.get_paginator("describe_mapped_resource_configuration")
+    list_signaling_channels_paginator: ListSignalingChannelsPaginator = client.get_paginator("list_signaling_channels")
+    list_streams_paginator: ListStreamsPaginator = client.get_paginator("list_streams")
+    ```
+"""
+from .client import KinesisVideoClient
+from .paginator import (
+    DescribeMappedResourceConfigurationPaginator,
+    ListSignalingChannelsPaginator,
+    ListStreamsPaginator,
+)
+
+Client = KinesisVideoClient
+
+
+__all__ = (
+    "Client",
+    "DescribeMappedResourceConfigurationPaginator",
+    "KinesisVideoClient",
+    "ListSignalingChannelsPaginator",
+    "ListStreamsPaginator",
+)
