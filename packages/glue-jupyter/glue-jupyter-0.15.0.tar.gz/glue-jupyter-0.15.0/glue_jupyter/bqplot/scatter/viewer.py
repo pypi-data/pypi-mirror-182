531b121b2c9fcf11fef95ce4e281cff14ffc23f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:30 +0000

    Add package glue-jupyter/0.15.0/glue-jupyter-0.15.0.tar.gz

---
+++ packages/glue-jupyter/glue-jupyter-0.15.0.tar.gz/glue-jupyter-0.15.0/glue_jupyter/bqplot/scatter/viewer.py
@@ -0,0 +1,29 @@
+from glue.viewers.scatter.state import ScatterViewerState
+
+from ..common.viewer import BqplotBaseView
+
+from .layer_artist import BqplotScatterLayerArtist
+
+from glue_jupyter.common.state_widgets.layer_scatter import ScatterLayerStateWidget
+from glue_jupyter.common.state_widgets.viewer_scatter import ScatterViewerStateWidget
+
+from glue_jupyter.registries import viewer_registry
+
+__all__ = ['BqplotScatterView']
+
+
+@viewer_registry("scatter")
+class BqplotScatterView(BqplotBaseView):
+
+    allow_duplicate_data = False
+    allow_duplicate_subset = False
+    large_data_size = 1e7
+
+    _state_cls = ScatterViewerState
+    _options_cls = ScatterViewerStateWidget
+    _data_artist_cls = BqplotScatterLayerArtist
+    _subset_artist_cls = BqplotScatterLayerArtist
+    _layer_style_widget_cls = ScatterLayerStateWidget
+
+    tools = ['bqplot:home', 'bqplot:panzoom', 'bqplot:rectangle', 'bqplot:circle',
+             'bqplot:xrange', 'bqplot:yrange']
