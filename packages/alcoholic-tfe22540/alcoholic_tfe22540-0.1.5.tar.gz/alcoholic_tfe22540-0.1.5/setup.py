af78c57b7e0ba65bbf2cafdbbe170af2948c80df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:47 +0000

    Add package alcoholic-tfe22540/0.1.5/alcoholic_tfe22540-0.1.5.tar.gz

---
+++ packages/alcoholic-tfe22540/alcoholic_tfe22540-0.1.5.tar.gz/alcoholic_tfe22540-0.1.5/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 16 11:19:19 2022
+
+@author: manou
+"""
+
+from setuptools import setup
+
+setup(
+    name='alcoholic_tfe22540',
+    version='0.1.5',    
+    description='Framework of my master thesis on the effect of withdrawal on the white matter of alcoholic patients using dMRI data.',
+    url='https://github.com/mdausort/TFE22-540',
+    author='Manon Dausort',
+    author_email='manon.dausort@uclouvain.be',
+    license='BSD 2-clause',
+    packages=['tfe22540'],
+    install_requires=['numpy',
+                      'nibabel',     
+                      'xlsxwriter',
+                      'pandas',
+                      'matplotlib',
+                      'sklearn',
+                      'seaborn',
+                      'dipy',
+                      'scikit-image',
+                      'scipy',
+                      'openpyxl'
+                      ],
+
+    classifiers=[
+        'Intended Audience :: Science/Research',    
+        'Programming Language :: Python',
+    ],
+)
---
+++ packages/alcoholic-tfe22540/alcoholic_tfe22540-0.1.5.tar.gz/alcoholic_tfe22540-0.1.5/tfe22540/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 19 15:12:26 2022
+
+@author: manou
+"""
+
