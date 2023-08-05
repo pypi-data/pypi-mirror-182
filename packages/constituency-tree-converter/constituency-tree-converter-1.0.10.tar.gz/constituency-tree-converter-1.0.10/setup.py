f1aeecbaba89fe4600df122b286bdf3754d6f275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:34 +0000

    Add package constituency-tree-converter/1.0.10/constituency-tree-converter-1.0.10.tar.gz

---
+++ packages/constituency-tree-converter/constituency-tree-converter-1.0.10.tar.gz/constituency-tree-converter-1.0.10/setup.py
@@ -0,0 +1,34 @@
+from os import path
+
+from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='constituency-tree-converter',
+    version='1.0.10',
+    packages=['con_tree_converter', ],
+    url='https://github.com/geasyheart/constituency-tree-converter',
+    license='MIT',
+    author='yuzhang',
+    author_email='geasyheart@163.com',
+    description='constituency tree converter',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    install_requires=[
+        'nltk==3.6.5'
+    ],
+    extras_require={
+        'full': [
+            'graphviz>=0.18.2'
+        ]
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
