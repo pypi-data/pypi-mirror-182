9ed3b3800999c5f552ef912667ade4ab8c707940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:51 +0000

    Add package ucb-client/22.7.12/ucb-client-22.7.12.tar.gz

---
+++ packages/ucb-client/ucb-client-22.7.12.tar.gz/ucb-client-22.7.12/ucb_client/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ucb_client.api.billing_api import BillingApi
+from ucb_client.api.builds_api import BuildsApi
+from ucb_client.api.buildtargets_api import BuildtargetsApi
+from ucb_client.api.config_api import ConfigApi
+from ucb_client.api.credentials_api import CredentialsApi
+from ucb_client.api.orgs_api import OrgsApi
+from ucb_client.api.projects_api import ProjectsApi
+from ucb_client.api.public_api import PublicApi
+from ucb_client.api.shares_api import SharesApi
+from ucb_client.api.status_api import StatusApi
+from ucb_client.api.userdevices_api import UserdevicesApi
+from ucb_client.api.users_api import UsersApi
+from ucb_client.api.webhooks_api import WebhooksApi
