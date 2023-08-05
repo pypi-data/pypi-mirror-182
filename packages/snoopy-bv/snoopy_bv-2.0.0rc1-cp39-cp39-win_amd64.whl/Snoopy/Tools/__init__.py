0bfafe132508bacb9e96a0d669e902129788bb1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:12 +0000

    Add package snoopy-bv/2.0.0rc1/snoopy_bv-2.0.0rc1-cp39-cp39-win_amd64.whl

---
+++ packages/snoopy-bv/snoopy_bv-2.0.0rc1-cp39-cp39-win_amd64.whl/Snoopy/Tools/__init__.py
@@ -0,0 +1,2 @@
+from .cleancommentlines import *
+from ._deprecated import renamed_function, deprecated_alias, renamed_class, moved_function, deprecated
---
+++ packages/snoopy-bv/snoopy_bv-2.0.0rc1-cp39-cp39-win_amd64.whl/Snoopy/version.py
@@ -0,0 +1,5 @@
+#Tags with hash (if version is not exactly the hash)
+snoopy_tag = "v2.0.0RC1-5-gb934950c"
+
+#Tags only
+snoopy_tag_only = "v2.0.0RC1"
