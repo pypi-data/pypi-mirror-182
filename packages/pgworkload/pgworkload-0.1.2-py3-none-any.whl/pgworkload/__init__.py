2c930fcea02461e501f678e076cdc1afb4e96be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:05 +0000

    Add package pgworkload/0.1.2/pgworkload-0.1.2-py3-none-any.whl

---
+++ packages/pgworkload/pgworkload-0.1.2-py3-none-any.whl/pgworkload/__init__.py
@@ -0,0 +1,15 @@
+import logging
+
+from importlib import metadata
+
+__version__ = metadata.version(__package__)
+
+del metadata  # optional, avoids polluting the results of dir(__package__)
+
+logger = logging.getLogger(__package__)
+logger.setLevel(logging.INFO)
+sh = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s [%(levelname)s] (%(processName)s %(process)d %(threadName)s) %(module)s: - %(message)s')
+sh.setFormatter(formatter)
+logger.addHandler(sh)
