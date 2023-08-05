e9ac53c917892d56f8990f231b44d3016554bc62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:13 +0000

    Add package asix-Sergio-Belmonte/0.1/asix_Sergio_Belmonte-0.1.tar.gz

---
+++ packages/asix-Sergio-Belmonte/asix_Sergio_Belmonte-0.1.tar.gz/asix_Sergio_Belmonte-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+
+setup(
+    name='asix_Sergio_Belmonte',
+    packages=['asix_Sergio_Belmonte'],  # this must be the same as the name above
+    version='0.1',
+    description='Sube paquete',
+    author='Belmonte' 
+)
