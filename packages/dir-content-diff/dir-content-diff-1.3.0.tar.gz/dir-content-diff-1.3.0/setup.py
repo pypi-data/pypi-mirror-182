e0f765cccac9cc669b4da934e3d9572dc29f5494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:20 +0000

    Add package dir-content-diff/1.3.0/dir-content-diff-1.3.0.tar.gz

---
+++ packages/dir-content-diff/dir-content-diff-1.3.0.tar.gz/dir-content-diff-1.3.0/setup.py
@@ -0,0 +1,70 @@
+"""Setup for the dir-content-diff package."""
+from pathlib import Path
+
+from setuptools import find_namespace_packages
+from setuptools import setup
+
+reqs = [
+    "dictdiffer",
+    "diff_pdf_visually>=1.5.1",
+    "PyYaml",
+]
+doc_reqs = [
+    "m2r2",
+    "mistune<2",
+    "sphinx",
+    "sphinx-bluebrain-theme",
+]
+pandas_reqs = [
+    "pandas",
+    "tables",
+]
+test_reqs = [
+    "coverage",
+    "dicttoxml<=1.7.12",
+    "matplotlib",
+    "rst2pdf>=0.99",
+    "pytest",
+    "pytest-html",
+]
+
+setup(
+    name="dir-content-diff",
+    author="Blue Brain Project, EPFL",
+    description="Simple tool to compare directory contents.",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url="https://dir-content-diff.readthedocs.io",
+    project_urls={
+        "Tracker": "https://github.com/BlueBrain/dir-content-diff/issues",
+        "Source": "https://github.com/BlueBrain/dir-content-diff",
+    },
+    license="Apache License 2.0",
+    packages=find_namespace_packages(include=["dir_content_diff*"]),
+    python_requires=">=3.8",
+    use_scm_version=True,
+    setup_requires=[
+        "setuptools_scm",
+    ],
+    install_requires=reqs,
+    extras_require={
+        "pandas": pandas_reqs,
+        "docs": doc_reqs + pandas_reqs,
+        "test": test_reqs + pandas_reqs,
+    },
+    entry_points={
+        "pytest11": ["dir-content-diff = dir_content_diff.pytest_plugin"],
+    },
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
