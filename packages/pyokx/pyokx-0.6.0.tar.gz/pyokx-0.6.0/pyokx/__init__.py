10faed3503b73d8f8ced73336038e09509ba1a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:43 +0000

    Add package pyokx/0.6.0/pyokx-0.6.0.tar.gz

---
+++ packages/pyokx/pyokx-0.6.0.tar.gz/pyokx-0.6.0/pyokx/__init__.py
@@ -0,0 +1,43 @@
+from loguru import logger
+import sys
+from .Account import Account
+from .Blocktrading import Blocktrading
+from .Convert import Convert
+from .Earn import Earn
+from .Funding import Funding
+from .Gridtrading import Gridtrading
+from .Marketdata import Marketdata
+from .Publicdata import Publicdata
+from .Status import Status
+from .SubAccount import SubAccount
+from .Trade import Trade
+from .Tradingdata import Tradingdata
+from .base import OKXClient
+
+
+__version__ = "0.4.0"
+
+
+def change_log_level(level: str = "INFO"):
+    logger.remove()
+    logger.add(sys.stdout, level=level)
+
+
+change_log_level()
+
+__all__ = [
+    "Account",
+    "Blocktrading",
+    "Convert",
+    "Earn",
+    "Funding",
+    "Gridtrading",
+    "Marketdata",
+    "Publicdata",
+    "Status",
+    "SubAccount",
+    "Trade",
+    "Tradingdata",
+    "change_log_level",
+    "OKXClient",
+]
