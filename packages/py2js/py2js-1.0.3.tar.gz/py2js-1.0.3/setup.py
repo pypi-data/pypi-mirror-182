d92b9309190dacfa293605b921dac1e441501ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:29 +0000

    Add package py2js/1.0.3/py2js-1.0.3.tar.gz

---
+++ packages/py2js/py2js-1.0.3.tar.gz/py2js-1.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages, find_namespace_packages
+
+with open('README.md', mode='r', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt', mode='r', encoding='utf-8') as f:
+    requires = map(str.strip, f.readlines())
+
+author = 'am230'
+name = 'py2js'
+py_modules = [name]
+
+setup(
+    name=name,
+    version="1.0.3",
+    keywords=("javascript", "convert"),
+    description="Write javascript in python with python syntax",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="MIT Licence",
+    url=f"https://github.com/{author}/{name}",
+    author=author,
+    author_email="am.230@outlook.jp",
+    py_modules=py_modules,
+    platforms="any",
+    packages=find_packages(),
+    requires=requires
+)
