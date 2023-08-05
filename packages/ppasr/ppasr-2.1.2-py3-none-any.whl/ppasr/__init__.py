d8fb7ca6e8b8edfc532a68b1f65190d3ff809fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:47 +0000

    Add package ppasr/2.1.2/ppasr-2.1.2-py3-none-any.whl

---
+++ packages/ppasr/ppasr-2.1.2-py3-none-any.whl/ppasr/__init__.py
@@ -0,0 +1,4 @@
+# 项目支持的模型
+SUPPORT_MODEL = ['squeezeformer_online', 'squeezeformer_offline',
+                 'conformer_online', 'conformer_offline',
+                 'deepspeech2_online', 'deepspeech2_offline']
