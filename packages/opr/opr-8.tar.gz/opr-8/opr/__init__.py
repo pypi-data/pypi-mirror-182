f32d3be2dbd1381a709c6945ef11a9699ef792f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:12 +0000

    Add package opr/8/opr-8.tar.gz

---
+++ packages/opr/opr-8.tar.gz/opr-8/opr/__init__.py
@@ -0,0 +1,14 @@
+# This file is placed in the Public Domain.
+# pylint: disable=W0622
+
+
+"object programming runtime"
+
+
+from opr import handler, objects, running, threads
+
+
+from opr.handler import *
+from opr.objects import *
+from opr.running import *
+from opr.threads import *
---
+++ packages/opr/opr-8.tar.gz/opr-8/opr/running.py
@@ -0,0 +1,110 @@
+# This file is placed in the Public Domain.
+# pylint: disable=C0115,C0116,C0411,C0413,W0212,R0903,W0201,E0402,W0613
+
+
+"runtime"
+
+
+import os
+import sys
+import time
+
+
+from .handler import scan
+from .message import Event, Parsed
+from .objects import Default, spl, update
+
+
+def __dir__():
+    return (
+            "Cfg",
+            "Console",
+            "boot",
+            "command",
+            "parse",
+            'scanner',
+            'scandir',
+            "wait"
+           )
+
+
+class Config(Default):
+
+    pass
+
+
+def boot():
+    prs = parse()
+    if "c" in prs.opts:
+        Cfg.console = True
+    if "d" in prs.opts:
+        Cfg.daemon= True
+    if "v" in prs.opts:
+        Cfg.verbose = True
+    if "w" in prs.opts:
+        Cfg.wait = True
+    update(Cfg.prs, prs)
+    update(Cfg, prs.sets)
+
+
+def command(cli, txt, event=None):
+    evt = (event() if event else Event())
+    evt.parse(txt)
+    evt.orig = repr(cli)
+    cli.handle(evt)
+    evt.wait()
+    return evt
+
+
+def include(name, namelist):
+    for nme in namelist:
+        if nme in name:
+            return True
+    return False
+
+
+def listmod(path):
+    res = []
+    if not os.path.exists(path):
+        return res
+    for fnm in os.listdir(path):
+        if fnm.endswith("~") or fnm.startswith("__"):
+            continue
+        res.append(fnm.split(os.sep)[-1][:-3])
+    return res
+
+
+def parse(txt=None):
+    if txt is None:
+        txt = " ".join(sys.argv[1:])
+    prs = Parsed()
+    prs.parse(txt)
+    update(Cfg.prs, prs)
+    return prs
+
+
+def scanner(pkg, importer, mods=None):
+    path = pkg.__path__[0]
+    name = pkg.__name__
+    scandir(path, importer, name, mods)
+
+
+def scandir(path, importer, pname, mods=None):
+    for modname in listmod(path):
+        if mods and not include(modname, spl(mods)):
+            continue
+        mname = "%s.%s" % (pname, modname)
+        mod = importer(mname, path)
+        scan(mod)
+
+
+def wait():
+    while 1:
+        time.sleep(1.0)
+
+
+## runtime
+
+
+Cfg = Config()
+Cfg.prs = Parsed()
