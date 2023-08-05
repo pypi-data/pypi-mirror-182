0154db7028f4dee09f187b35ebffe26defdfc985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:51 +0000

    Add package abricot/0.0.4a2/abricot-0.0.4a2.tar.gz

---
+++ packages/abricot/abricot-0.0.4a2.tar.gz/abricot-0.0.4a2/abricot/abr_random/abr_string.py
@@ -0,0 +1,22 @@
+from random import Random
+
+def generate_random_string(string_len=16, s_symbol=False)-> str:
+    """ To generate a random string of certain length.
+
+    Args:
+        string_len (int, optional): The length of string. Defaults to 16.
+
+    Returns:
+        str: A random string.
+    """
+    str = ''
+    special_symbol = '@#$%^&*!~?>:;'
+    chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789'
+    if s_symbol:
+        chars += special_symbol
+
+    length = len(chars) - 1
+    random = Random()
+    for _ in range(string_len):
+        str+=chars[random.randint(0,length)]
+    return str
