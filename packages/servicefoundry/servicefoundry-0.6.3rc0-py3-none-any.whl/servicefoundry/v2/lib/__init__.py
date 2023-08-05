3239679a865e60aad4f0c4191959b9aa49ab75bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:28 +0000

    Add package servicefoundry/0.6.3rc0/servicefoundry-0.6.3rc0-py3-none-any.whl

---
+++ packages/servicefoundry/servicefoundry-0.6.3rc0-py3-none-any.whl/servicefoundry/v2/lib/__init__.py
@@ -0,0 +1,28 @@
+# NOTE: Later all of these can be under a `models` or some other module
+# I am not entirely sure about the structure of `lib` module yet
+# This can go through another round of refactoring
+from servicefoundry.v2.lib.deployble_patched_models import (
+    Application,
+    Job,
+    ModelDeployment,
+    Notebook,
+    Service,
+)
+from servicefoundry.v2.lib.patched_models import (
+    BasicAuthCreds,
+    Build,
+    DockerFileBuild,
+    GitSource,
+    HealthProbe,
+    HttpProbe,
+    HuggingfaceModelHub,
+    Image,
+    LocalSource,
+    Manual,
+    Port,
+    PythonBuild,
+    RemoteSource,
+    Resources,
+    Schedule,
+    TruefoundryModelRegistry,
+)
