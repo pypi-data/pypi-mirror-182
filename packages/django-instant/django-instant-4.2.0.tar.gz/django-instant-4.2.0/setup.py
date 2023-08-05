fa48985934ce2bf7235a962e8f29b8294078696d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:40 +0000

    Add package django-instant/4.2.0/django-instant-4.2.0.tar.gz

---
+++ packages/django-instant/django-instant-4.2.0.tar.gz/django-instant-4.2.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+version = __import__("instant").__version__
+
+setup(
+    name="django-instant",
+    packages=find_packages(),
+    include_package_data=True,
+    version=version,
+    description="Websockets for Django with Centrifugo ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="synw",
+    author_email="synwe@yahoo.com",
+    url="https://github.com/synw/django-instant",
+    download_url="https://github.com/synw/django-instant/releases/tag/" + version,
+    keywords=["django", "websockets", "centrifugo"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Django :: 4.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+    ],
+    install_requires=["cent", "PyJWT"],
+    zip_safe=False,
+)
