770658f8d29530532f4059284283d0ba1cffaec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:33 +0000

    Add package commitizen-cz-base/0.1.0/commitizen-cz-base-0.1.0.tar.gz

---
+++ packages/commitizen-cz-base/commitizen-cz-base-0.1.0.tar.gz/commitizen-cz-base-0.1.0/README.md
@@ -0,0 +1,1 @@
+# commitizen-cz-base
---
+++ packages/commitizen-cz-base/commitizen-cz-base-0.1.0.tar.gz/commitizen-cz-base-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="commitizen-cz-base",
+    version="0.1.0",
+    py_modules=["cz_base"],
+    license="Apache Software License 2.0",
+    long_description="Commitizen configuration for base Angewandte projects",
+    install_requires=["commitizen"],
+)
