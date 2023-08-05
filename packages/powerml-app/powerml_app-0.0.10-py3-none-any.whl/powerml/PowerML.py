6ed6831eeb9114527aece806af681b74521f1f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:11 +0000

    Add package powerml-app/0.0.10/powerml_app-0.0.10-py3-none-any.whl

---
+++ packages/powerml-app/powerml_app-0.0.10-py3-none-any.whl/powerml/PowerML.py
@@ -0,0 +1,56 @@
+import io
+import requests
+import jsonlines
+from powerml.utils.run_ai import run_ai
+from powerml.utils.config import get_config
+
+
+class PowerML:
+    def __init__(self, config={}):
+        self.config = get_config(config)
+        self.current_model = ""
+
+    def predict(self,
+                prompt: str,
+                model: str = "",
+                stop: str = "",
+                max_tokens: int = 128,
+                temperature: int = 0,
+                ) -> str:
+        if model == "":
+            model = "llama" if self.current_model == "" else self.current_model
+        print("model: ", model)
+        # if the model is one of our models, then hit our api
+        return run_ai(prompt,
+                      max_tokens=max_tokens,
+                      api="powerml",
+                      model=model,
+                      stop=stop,
+                      temperature=temperature,
+                      key=self.config["powerml.key"],
+                      )
+
+    def fit(self,
+            data: list[str],
+            model: str = "llama"):
+        # Upload filtered data to train api
+        headers = {
+            "Authorization": "Bearer " + self.config["powerml.key"],
+            "Content-Type": "application/json", }
+        response = requests.post(
+            headers=headers,
+            url="https://api.staging.powerml.co/v1/train",
+            json={
+                "dataset": self.__make_dataset_string(data),
+                "model": model
+            })
+        model_details = response.json()['model']
+        self.current_model = model_details["model_name"]
+        return model_details
+
+    def __make_dataset_string(self, training_data):
+        string = io.StringIO()
+        with jsonlines.Writer(string) as writer:
+            for item in training_data:
+                writer.write({"prompt": item})
+        return string.getvalue()
