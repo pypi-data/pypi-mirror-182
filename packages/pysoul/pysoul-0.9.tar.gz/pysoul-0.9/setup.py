4c38224b31743135d51bed669608ee386470eb7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:09 +0000

    Add package pysoul/0.9/pysoul-0.9.tar.gz

---
+++ packages/pysoul/pysoul-0.9.tar.gz/pysoul-0.9/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+def readfile(filename):
+    with open(filename, 'r+') as f:
+        return f.read()
+      
+      
+setup(
+    name='pysoul',
+    version='0.9',
+    license='MIT',
+    author="Ahmad Salameh",
+    author_email='ahmad.majdi96@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/ahmadmajdi96/py_sal',
+    keywords='pysoul',
+    install_requires=[
+      ],
+    entry_points={
+        'console_scripts': [
+            'pysoul = caller:main'
+        ]
+    },
+
+)
