45c65a73d1ca073f216325d354e10727a5933bcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:53 +0000

    Add package commonsharedclasses/0.0.10/commonsharedclasses-0.0.10.tar.gz

---
+++ packages/commonsharedclasses/commonsharedclasses-0.0.10.tar.gz/commonsharedclasses-0.0.10/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='commonsharedclasses',
+    version='0.0.10',
+    description='This package will contain all common classes, version 0.0.10 updates:- logging added in cosmos_utility.py',
+    author='Ankit Kumar',
+    packages=['commonsharedclasses'],
+    zip_safe=False
+)
