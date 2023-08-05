0cfb15ab8bf5cb618a92355aebbdbc6aacdbb971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:59 +0000

    Add package twitchAPI/3.2.1/twitchAPI-3.2.1.tar.gz

---
+++ packages/twitchAPI/twitchAPI-3.2.1.tar.gz/twitchAPI-3.2.1/setup.py
@@ -0,0 +1,33 @@
+#  Copyright (c) 2020. Lena "Teekeks" During <info@teawork.de>
+
+from setuptools import setup, find_packages
+setup(
+    packages=find_packages(),
+    name="twitchAPI",
+    version="3.2.1",
+    url="https://github.com/Teekeks/pyTwitchAPI",
+    author="Lena \"Teekeks\" During",
+    author_email="info@teawork.de",
+    description="A Python 3.7+ implementation of the Twitch Helix API, PubSub, EventSub and Chat",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Communications",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11"
+    ],
+    package_data={
+        "twitchAPI": ["*.html"]
+    },
+    install_requires=['python-dateutil', 'aiohttp', 'typing_extensions']
+)
