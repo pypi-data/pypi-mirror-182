c9aafb7f1619b32dd879ae6ac9140338a53a12a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:02 +0000

    Add package xPDF50/1.0.7/xPDF50-1.0.7.tar.gz

---
+++ packages/xPDF50/xPDF50-1.0.7.tar.gz/xPDF50-1.0.7/README.md
@@ -0,0 +1,30 @@
+### xPDF50
+
+Given a URL of Harvard's CS50 web page containing a project, a problem, a lab or lecture notes xPDF50 outputs a clean PDF file of that project, problem, lab or notes.
+
+> Installation
+
+     $ pip install xPDF50
+
+Please note that following modules are 'required' for xPDF50 to work. 
+* Pillow
+* requests
+* xhtml2pdf
+
+
+> Usage
+
+     $ xPDF50 url
+
+Make sure to provide a URL of a cs50.harvard.edu page containing a project, a problem, a lab or lecture notes.
+
+> Upgrade
+
+     $ pip install xPDF50 --upgrade
+
+*** Important: Download the pdf file to the PC to view it 
+
+     
+
+
+
---
+++ packages/xPDF50/xPDF50-1.0.7.tar.gz/xPDF50-1.0.7/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="xPDF50",
+    version="1.0.7",
+    description="Downloads clean PDFs of CS50 course materials from cs50.harvard.edu web pages",
+    long_description = long_description, long_description_content_type=("text/markdown"),
+    py_modules=["__main__"],
+    packages=["xPDF50"],
+    entry_points={
+        "console_scripts":["xPDF50 = xPDF50.__main__:main"],
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    extras_require = {
+            "dev": ["pytest>=3.7"],
+    },
+    url="https://github.com/thaare87/projects/tree/master/xPDF50",
+    author="Thaare87",
+    author_email="thaare87@gmail.com",
+)
+
