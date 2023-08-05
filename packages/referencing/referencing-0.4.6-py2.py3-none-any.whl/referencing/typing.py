6dcdaa92270fd555a73ae62a3fd26d5986872905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:23 +0000

    Add package referencing/0.4.6/referencing-0.4.6-py2.py3-none-any.whl

---
+++ packages/referencing/referencing-0.4.6-py2.py3-none-any.whl/referencing/typing.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Iterable, Protocol, Union
+
+try:
+    from collections.abc import Mapping
+
+    Mapping[str, str]
+except TypeError:
+    from typing import Mapping
+
+if TYPE_CHECKING:
+    from referencing._core import Resolver
+
+
+#: A JSON Schema which is a JSON object
+ObjectSchema = Mapping[str, Any]
+
+#: A JSON Schema of any kind
+Schema = Union[bool, ObjectSchema]
+
+
+class Anchor(Protocol):
+    @property
+    def name(self) -> str:
+        ...
+
+    def resolve(self, resolver: Resolver, uri: str) -> tuple[Schema, str]:
+        pass
+
+
+class Specification(Protocol):
+    def id_of(self, resource: Schema) -> str | None:
+        """
+        The URI ID of the given resource.
+        """
+
+    def anchors_in(self, resource: ObjectSchema) -> Iterable[Anchor]:
+        """
+        All (non-recursively nested) anchors inside the given resource.
+        """
+
+    def subresources_of(self, resource: ObjectSchema) -> Iterable[Schema]:
+        """
+        All (non-recursively nested) resources inside the given resource.
+        """
