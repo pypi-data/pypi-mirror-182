083293c76c31b67988c85ef69df90b16a7bd76c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:27 +0000

    Add package felupe/6.2.2/felupe-6.2.2-py3-none-any.whl

---
+++ packages/felupe/felupe-6.2.2-py3-none-any.whl/felupe/tools/__init__.py
@@ -0,0 +1,17 @@
+from ._check import check
+from ._solve import solve
+from ._post import (
+    force,
+    moment,
+    curve,
+)
+from ._newton import (
+    newtonrhapson,
+    fun_items as fun,
+    jac_items as jac,
+)
+from ._project import (
+    topoints,
+    project,
+)
+from ._save import save
