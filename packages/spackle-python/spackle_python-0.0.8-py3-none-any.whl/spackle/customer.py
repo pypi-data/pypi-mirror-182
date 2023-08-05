7af0ed019ef87f4a5c1a07e6d855c9deed2c5f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:23 +0000

    Add package spackle-python/0.0.8/spackle_python-0.0.8-py3-none-any.whl

---
+++ packages/spackle-python/spackle_python-0.0.8-py3-none-any.whl/spackle/customer.py
@@ -0,0 +1,38 @@
+import json
+import time
+
+from spackle import dynamodb
+from spackle.exceptions import SpackleException
+
+
+class Customer:
+    def __init__(self, data):
+        self.data = data
+
+    @staticmethod
+    def retrieve(customer_id):
+        dynamodb_client = dynamodb.get_client()
+        response = dynamodb_client.get_item(
+            TableName=dynamodb.table_name,
+            Key={
+                "AccountId": {"S": dynamodb.identity_id},
+                "CustomerId": {"S": customer_id},
+            },
+        )
+
+        data = json.loads(response["Item"]["State"]["S"])
+        return Customer(data)
+
+    def enabled(self, key):
+        for feature in self.data["features"]:
+            if feature["key"] == key:
+                return feature["value_flag"]
+
+        return False
+
+    def limit(self, key):
+        for feature in self.data["features"]:
+            if feature["key"] == key:
+                return feature["value_limit"]
+
+        return 0
