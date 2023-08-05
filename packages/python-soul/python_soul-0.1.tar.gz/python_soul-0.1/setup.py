bf8f5531c4057333a8cfa536873d75c86f15b20f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:20 +0000

    Add package python-soul/0.1/python_soul-0.1.tar.gz

---
+++ packages/python-soul/python_soul-0.1.tar.gz/python_soul-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='python_soul',
+    version='0.1',
+    license='MIT',
+    author="Ahmad Salameh",
+    author_email='ahmad.majdi96@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords='python soul',
+    install_requires=[
+      ],
+
+)
---
+++ packages/python-soul/python_soul-0.1.tar.gz/python_soul-0.1/src/python_soul/__init__.py
@@ -0,0 +1,5 @@
+
+import command_manager
+import sys
+if __name__ == "__main__":
+    command_manager.main(sys.argv)
