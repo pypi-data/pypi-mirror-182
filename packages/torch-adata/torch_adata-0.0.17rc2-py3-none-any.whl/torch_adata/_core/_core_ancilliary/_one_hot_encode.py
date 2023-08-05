52e2265a00ae05f027a6e326178e77271c2152bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:33 +0000

    Add package torch-adata/0.0.17rc2/torch_adata-0.0.17rc2-py3-none-any.whl

---
+++ packages/torch-adata/torch_adata-0.0.17rc2-py3-none-any.whl/torch_adata/_core/_core_ancilliary/_one_hot_encode.py
@@ -0,0 +1,42 @@
+
+__module_name__ = "_one_hot_encode.py"
+__doc__ = """sklearn.preprocessing.OneHotEncoder() wrapper module."""
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu"])
+
+
+# -- specify package version: ------------------------------------------------------------
+__version__ = "0.0.17"
+
+
+# -- import packages: --------------------------------------------------------------------
+import sklearn.preprocessing
+import torch
+import numpy
+
+
+# -- sole function: ----------------------------------------------------------------------
+def one_hot_encode(y_data: numpy.ndarray) -> (sklearn.preprocessing.OneHotEncoder, torch.Tensor):
+
+    """
+    One-hot encode a y_data vector of shape y to a matrix of shape: y x n_categories.
+
+    Parameters:
+    -----------
+    y_data
+        numpy.ndarray
+
+    Returns:
+    --------
+    encoder
+        type: sklearn.preprocessing.OneHotEncoder
+
+    y_one_hot
+        type: torch.Tensor
+
+    """
+
+    encoder = sklearn.preprocessing.OneHotEncoder()
+    y_one_hot = encoder.fit_transform(y_data).toarray()
+
+    return encoder, torch.Tensor(y_one_hot)
