fc2e17f324085dc4d3346889ea9a85fd1615e561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:58 +0000

    Add package powerml-app/0.0.6/powerml_app-0.0.6-py3-none-any.whl

---
+++ packages/powerml-app/powerml_app-0.0.6-py3-none-any.whl/src/powerml/utils/config.py
@@ -0,0 +1,17 @@
+import config
+import os
+
+
+def get_config(dictionary={}):
+    global_config = setup_config(dictionary)
+    return global_config
+
+
+def setup_config(dictionary):
+    home = os.path.expanduser("~")
+    home_config_path = os.path.join(home, ".powerml/configure.yaml")
+    print(home_config_path)
+    return config.ConfigurationSet(
+        config.config_from_dict(dictionary),
+        config.config_from_yaml(home_config_path, read_from_file=True),
+    )
