c1a3cf74a199723970d0b36b0c66f18bc3ddf798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:01 +0000

    Add package pokemonCardLogger/0.2.11/pokemonCardLogger-0.2.11.tar.gz

---
+++ packages/pokemonCardLogger/pokemonCardLogger-0.2.11.tar.gz/pokemonCardLogger-0.2.11/pokemonCardLogger/__init__.py
@@ -0,0 +1,11 @@
+"""
+Description:
+    A Module to log and store information about a Pokémon tcg collection
+Usage:
+    To run as a program "python3 main.py"
+    To use as a library:
+        "from pokemonCardLogger import clss as pcl" for json storage
+        "from pokemonCardLogger import clss_alt as pcl" for sqlite storage (pre-alpha experimental)
+    To get just the packs and their ids, run "python3 packRef.py"
+"""
+from pokemonCardLogger import *
