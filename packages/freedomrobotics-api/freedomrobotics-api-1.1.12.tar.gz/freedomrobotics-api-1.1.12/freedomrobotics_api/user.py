3c53086fc9d76187b80c9b3c3b0a9575a3679d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:55 +0000

    Add package freedomrobotics-api/1.1.12/freedomrobotics-api-1.1.12.tar.gz

---
+++ packages/freedomrobotics-api/freedomrobotics-api-1.1.12.tar.gz/freedomrobotics-api-1.1.12/freedomrobotics_api/user.py
@@ -0,0 +1,38 @@
+from .account import Account
+from .entity import Entity
+
+
+class User(Entity):
+    """A user within the Freedom Robotics API. Not intended to be manually instantiated by the user of the API.
+
+    Args:
+        _id (str): User ID (e-mail address).
+        _data (`dict`): User data.
+    """
+    pk = 'user'
+
+    def __repr__(self):
+        return "<User: %s %s %s>" % (
+            self._id,
+            self._data.get("first_name", ""),
+            self._data.get("last_name",  ""),
+        )
+
+    @property
+    def url_base(self):
+        return '/users'
+
+    @property
+    def first_name(self):
+        return self._data.get("first_name")
+
+    @property
+    def role(self):
+        return self._data.get("role")
+
+    @property
+    def accounts(self, **kwargs):
+        return [
+            Account(account_id, None, api=self.api, **kwargs)
+            for account_id in self._data["accounts"]
+        ]
