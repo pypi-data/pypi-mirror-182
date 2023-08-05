db6f2bf4374ad17f3486357a4b9584078b651df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:35 +0000

    Add package tibber-aws/0.16.0/tibber_aws-0.16.0.tar.gz

---
+++ packages/tibber-aws/tibber_aws-0.16.0.tar.gz/tibber_aws-0.16.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+requirements = []
+with open("requirements.txt") as f:
+    for line in f.read().splitlines():
+        if line.startswith("#"):
+            continue
+        requirements.append(line)
+
+setup(
+    name="tibber_aws",
+    packages=["tibber_aws"],
+    install_requires=requirements,
+    version="0.16.0",
+    description="A python3 library to communicate with Aws",
+    python_requires=">=3.7.0",
+    author="Tibber",
+    author_email="drdaniel@tibber.com",
+    url="https://github.com/tibber/tibber-pyAws",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
---
+++ packages/tibber-aws/tibber_aws-0.16.0.tar.gz/tibber_aws-0.16.0/tibber_aws/aws_base.py
@@ -0,0 +1,40 @@
+import asyncio
+import logging
+from contextlib import AsyncExitStack
+
+from aiobotocore.session import get_session, AioSession
+from aiobotocore.config import AioConfig
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def get_aiosession() -> AioSession:
+    return get_session()
+
+
+class AwsBase:
+    def __init__(self, service_name, region_name="eu-west-1", max_pool_connections=10) -> None:
+        self._client = None
+        self._context_stack = AsyncExitStack()
+        self._region_name = region_name
+        self._service_name = service_name
+        self._init_lock = asyncio.Lock()
+        self._max_pool_connections = max_pool_connections
+
+    async def close(self) -> None:
+        await self._context_stack.aclose()
+        self._client = None
+
+    async def init_client_if_required(self, session=None) -> None:
+        async with self._init_lock:
+            if self._client is not None:
+                return
+
+            config = AioConfig(
+                max_pool_connections=self._max_pool_connections,
+            )
+
+            session = session or get_aiosession()
+            self._client = await self._context_stack.enter_async_context(
+                session.create_client(self._service_name, region_name=self._region_name, config=config)
+            )
