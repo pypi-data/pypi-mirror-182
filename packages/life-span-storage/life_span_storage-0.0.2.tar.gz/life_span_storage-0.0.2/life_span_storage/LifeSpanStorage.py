42ac0a246f193501c49b66caf0dfd3e7c112e1ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:49 +0000

    Add package life-span-storage/0.0.2/life_span_storage-0.0.2.tar.gz

---
+++ packages/life-span-storage/life_span_storage-0.0.2.tar.gz/life_span_storage-0.0.2/life_span_storage/LifeSpanStorage.py
@@ -0,0 +1,39 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class LifeSpanStorage(Component):
+    """A LifeSpanStorage component.
+ExampleComponent is an example component.
+It takes a property, `label`, and
+displays it.
+It renders an input with the property `value`
+which is editable by the user.
+
+Keyword arguments:
+
+- id (string; required)
+
+- data (string | number | list | dict; optional)"""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'life_span_storage'
+    _type = 'LifeSpanStorage'
+    @_explicitize_args
+    def __init__(self, id=Component.REQUIRED, data=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'data']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'data']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        for k in ['id']:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+
+        super(LifeSpanStorage, self).__init__(**args)
