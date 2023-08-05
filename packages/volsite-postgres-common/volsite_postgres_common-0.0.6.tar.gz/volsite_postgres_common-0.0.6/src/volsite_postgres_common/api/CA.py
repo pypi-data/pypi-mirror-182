65a564995c110f5358222bbbb03af31d9297230e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:51 +0000

    Add package volsite-postgres-common/0.0.6/volsite_postgres_common-0.0.6.tar.gz

---
+++ packages/volsite-postgres-common/volsite_postgres_common-0.0.6.tar.gz/volsite_postgres_common-0.0.6/src/volsite_postgres_common/api/CA.py
@@ -0,0 +1,40 @@
+from typing import Final
+
+
+# ====== CA: Common Attribute ======
+
+class CA:
+    Api: Final[str] = "api"
+    Auth: Final[str] = "aut"
+    Ascending: Final[str] = "asc"  # in SQL
+
+    Code: Final[str] = "cd"
+    Count: Final[str] = "cnt"
+
+    Data: Final[str] = "dt"
+    Descending: Final[str] = "desc"  # in SQL
+    DescriptionFormatted: Final[str] = "desf"
+    DescriptionId: Final[str] = "desid"
+    DescriptionPlain: Final[str] = "desp"
+
+    List: Final[str] = "lt"
+    Login: Final[str] = "lgn"
+
+    Name: Final[str] = "na"
+
+    Password: Final[str] = "pw"
+
+    Result: Final[str] = "ret"
+
+    Signup: Final[str] = "sgu"
+    SortBy: Final[str] = "stby"
+    SortDirection: Final[str] = "stdr"
+
+    Title: Final[str] = "tt"
+    TitleId: Final[str] = "ttid"
+
+    UserId: Final[str] = "uid"
+
+    Word: Final[str] = "w"
+
+    ___Error___: Final[str] = "err"
