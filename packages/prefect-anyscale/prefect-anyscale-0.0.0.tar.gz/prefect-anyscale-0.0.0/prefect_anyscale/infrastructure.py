46bdbd7a0347c7a0a00a8b14c1a24f15f253c4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:51 +0000

    Add package prefect-anyscale/0.0.0/prefect-anyscale-0.0.0.tar.gz

---
+++ packages/prefect-anyscale/prefect-anyscale-0.0.0.tar.gz/prefect-anyscale-0.0.0/prefect_anyscale/infrastructure.py
@@ -0,0 +1,37 @@
+from typing import Dict, Union
+from typing_extensions import Literal
+
+from prefect.infrastructure.base import Infrastructure
+from prefect.utilities.asyncutils import sync_compatible
+from pydantic import Field
+
+
+class AnyscaleJob(Infrastructure):
+
+    type: Literal["anyscale-job"] = Field(
+        default="anyscale-job", description="The type of infrastructure."
+    )
+
+    compute_config: Union[None, str, Dict[str, str]] = Field(
+        description="Compute config to use for the execution of the job.",
+        default=None,
+    )
+
+    cluster_env: Union[None, str, Dict[str, str]] = Field(
+        description="Cluster environment to use for the execution of the job."
+    )
+
+    _block_type_name = "Anyscale Job"
+
+    def preview(self):
+        return " \\\n".join(
+            "compute_config = " + str(self.compute_config),
+            "cluster_env = " + str(self.cluster_env),
+        )
+
+    @sync_compatible
+    async def run(
+        self,
+        task_status = None,
+    ):
+        pass
