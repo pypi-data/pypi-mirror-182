0b70c541fcac959636eb6bba7c35ba196823ea2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:38 +0000

    Add package lt-autosnap/0.3.0/lt-autosnap-0.3.0.tar.gz

---
+++ packages/lt-autosnap/lt-autosnap-0.3.0.tar.gz/lt-autosnap-0.3.0/lt_autosnap/exit.py
@@ -0,0 +1,17 @@
+import logging
+import subprocess
+from typing import Optional
+
+logger = logging.getLogger(__name__)
+
+
+def errexit(msg: str, code: int = 1, ex: Optional[Exception] = None):
+    if ex:
+        msg += f" [{ex.__class__.__name__}] {ex}"
+    logger.error(msg)
+    if ex:
+        if isinstance(ex, subprocess.CalledProcessError):
+            logger.info("[Process stdout] %s", ex.stdout.decode("utf-8"))
+            logger.error("[Process stderr] %s", ex.stderr.decode("utf-8"))
+        logger.debug("[Traceback]", exc_info=True)
+    exit(code)
