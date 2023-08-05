59a8e3e5bea89ad5cc4eed6d7f70d400385fa4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:33 +0000

    Add package nsj-rest-lib/0.1.1/nsj_rest_lib-0.1.1.tar.gz

---
+++ packages/nsj-rest-lib/nsj_rest_lib-0.1.1.tar.gz/nsj_rest_lib-0.1.1/src/nsj_rest_lib/settings.py
@@ -0,0 +1,13 @@
+import os
+
+# Lendo variáveis de ambiente
+DEFAULT_PAGE_SIZE = int(os.getenv('DEFAULT_PAGE_SIZE', 20))
+USE_SQL_RETURNING_CLAUSE = (
+    os.getenv('USE_SQL_RETURNING_CLAUSE', 'true').lower == 'true')
+
+DATABASE_HOST = os.getenv('DATABASE_HOST', '')
+DATABASE_PASS = os.getenv('DATABASE_PASS', '')
+DATABASE_PORT = os.getenv('DATABASE_PORT', '')
+DATABASE_NAME = os.getenv('DATABASE_NAME', '')
+DATABASE_USER = os.getenv('DATABASE_USER', '')
+DATABASE_DRIVER = os.getenv('DATABASE_DRIVER', 'POSTGRES')
