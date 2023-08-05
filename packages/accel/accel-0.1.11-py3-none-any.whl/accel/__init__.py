678dbdd17e424e91b04467cdd47ce7c7688d9ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:41 +0000

    Add package accel/0.1.11/accel-0.1.11-py3-none-any.whl

---
+++ packages/accel/accel-0.1.11-py3-none-any.whl/accel/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.11"
+
+from accel.base.atoms import Atom, Atoms
+from accel.base.box import Box
+
+# for backward compatibility
+from accel.base.mols import Mol, Mols
+from accel.base.systems import System, Systems
+
+__all__ = ["Box", "System", "Atom", "Systems", "Atoms", "Mols", "Mol"]
