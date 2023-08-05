639cdb8010390ccb3a78b1a6434482e4f9972d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:24 +0000

    Add package drafttopic/0.4.0/drafttopic-0.4.0-py3-none-any.whl

---
+++ packages/drafttopic/drafttopic-0.4.0-py3-none-any.whl/drafttopic/utilities/__init__.py
@@ -0,0 +1,34 @@
+"""
+This module implements a set of utilities for extracting topic labels from
+English Wikipedia using the WikiProject taxonomy.
+
+Draft Topic CLI
+===============
+
+drafttopic
+----------
+.. automodule:: drafttopic.drafttopic
+
+Sub-utilities
+=============
+
+extract_from_text
+-----------------
+.. automodule:: drafttopic.utilities.extract_from_text
+
+fetch_draft_text
+----------------
+.. automodule:: drafttopic.utilities.fetch_draft_text
+
+fetch_article_text
+------------------
+.. automodule:: drafttopic.utilities.fetch_article_text
+
+taxo_label
+----------
+.. automodule:: drafttopic.utilities.taxo_label
+
+write_labels
+------------
+.. automodule:: drafttopic.utilities.write_labels
+"""
