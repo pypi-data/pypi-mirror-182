0c33fe3ae4f9ef6a5371952beb55fb7f213b85ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:38 +0000

    Add package pysoul/0.6/pysoul-0.6.tar.gz

---
+++ packages/pysoul/pysoul-0.6.tar.gz/pysoul-0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name='pysoul',
+    version='0.6',
+    license='MIT',
+    author="Ahmad Salameh",
+    author_email='ahmad.majdi96@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/ahmadmajdi96/py_sal',
+    keywords='pysoul',
+    install_requires=[
+      ],
+    entry_points={
+        'console_scripts': [
+            'pysoul = caller:main'
+        ]
+    },
+
+)
