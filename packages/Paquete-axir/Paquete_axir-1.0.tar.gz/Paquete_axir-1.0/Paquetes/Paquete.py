e322048b5e8d55004c18961954da70a5fa781fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:10 +0000

    Add package Paquete-axir/1.0/Paquete_axir-1.0.tar.gz

---
+++ packages/Paquete-axir/Paquete_axir-1.0.tar.gz/Paquete_axir-1.0/Paquetes/Paquete.py
@@ -0,0 +1,14 @@
+def Suma (a,b):
+    return a+b
+def Restar(a,b):
+    return a-b
+def Multiplicar(a,b):
+    return a*b
+def Dividir(a,b):
+    return a/b
+def hfun():
+    print("Tengo las siguientes Funciones :")
+    print("Suma")
+    print("Restar")
+    print("Multiplicar")
+    print("Dividir")
---
+++ packages/Paquete-axir/Paquete_axir-1.0.tar.gz/Paquete_axir-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='Paquete_axir',
+   version='1.0',
+   description='A useful module',
+   author='DaniQB',
+   packages=['Paquetes'],  #same as name 
+)
