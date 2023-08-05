8e8f66eda273d5ca7a9deec6eb1e23cb1b5e0893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:10 +0000

    Add package machineconfig/0.7/machineconfig-0.7-py3-none-any.whl

---
+++ packages/machineconfig/machineconfig-0.7-py3-none-any.whl/machineconfig/jobs/python/python_ve_installer.py
@@ -0,0 +1,43 @@
+
+
+import crocodile.toolbox as tb
+import platform
+import machineconfig
+
+system = platform.system()
+lib_root = tb.P(machineconfig.__file__).parent
+
+
+def ve_setup():
+    print("\n\n")
+    print("Existing Python versions:\n=========================")
+    tb.P.get_env().Path.search("python.exe").reduce()[1:].print()
+    print(f"\nExisting virtual environments are:")
+    ves = tb.P.home().joinpath("venvs").search("*", files=False).apply(lambda a_ve: (a_ve.name, a_ve.joinpath("pyvenv.cfg").read_text()))
+    ves.apply(lambda a_ve: print(f"{a_ve[0]}\n{'-' * 10}\n{a_ve[1]}\n"))
+    dotted_py_version = input("Enter python version (3.11): ") or "3.11"
+    env_name = input("Enter virtual environment name (latest): ") or "latest"
+    repos = input("Install essential repos? ([y]/n): ") or "y"
+
+    env_path = tb.P.home().joinpath("venvs", env_name)
+    if env_path.exists(): env_path.delete(sure=True)
+
+    scripts = lib_root.joinpath(f"setup_{system.lower()}/ve.{'ps1' if system == 'Windows' else 'sh'}").read_text()
+    variable_prefix = "$" if system == "Windows" else ""
+    scripts = tb.modify_text(txt_raw=scripts, txt_search="ve_name=", txt_alt=f"{variable_prefix}ve_name='{env_name}'", replace_line=True)
+    scripts = tb.modify_text(txt_raw=scripts, txt_search="py_version=", txt_alt=f"{variable_prefix}py_version='{dotted_py_version.replace('.', '') if system == 'Windows' else dotted_py_version}'", replace_line=True)
+
+    if repos == "y":
+        text = lib_root.joinpath(f"setup_{system.lower()}/repos.{'ps1' if system == 'Windows' else 'sh'}").read_text()
+        text = tb.modify_text(txt_raw=text, txt_search="ve_name=", txt_alt=f"{variable_prefix}ve_name='{env_name}'", replace_line=True)
+        scripts += text
+
+    print("Script to create virtual environment...".center(80, "-"))
+    print(scripts)
+    print("".center(80, "-"))
+
+    return scripts
+
+
+if __name__ == '__main__':
+    pass
