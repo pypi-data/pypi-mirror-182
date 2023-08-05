6d4ea3f6eb51817e5cf17c6bc482d262f26218d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:49 +0000

    Add package forfun/0.6/forfun-0.6.tar.gz

---
+++ packages/forfun/forfun-0.6.tar.gz/forfun-0.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='forfun',
+    version='0.6',
+    author="Giorgos Myrianthous",
+    author_email='email@example.com',
+    packages=find_packages('for-fun'),
+    package_dir={'': 'for-fun'},
+    url='https://github.com/gmyrianthous/example-publish-pypi',
+    keywords='example project'
+
+)
