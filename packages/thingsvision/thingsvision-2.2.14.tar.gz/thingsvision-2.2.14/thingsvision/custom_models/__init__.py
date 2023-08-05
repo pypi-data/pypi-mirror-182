d88a370b609fff955e3957b39d18793bf2bd0843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:20:49 +0000

    Add package thingsvision/2.2.14/thingsvision-2.2.14.tar.gz

---
+++ packages/thingsvision/thingsvision-2.2.14.tar.gz/thingsvision-2.2.14/thingsvision/custom_models/__init__.py
@@ -0,0 +1,6 @@
+from .alexnet_ecoset import Alexnet_ecoset
+from .resnet50_ecoset import Resnet50_ecoset
+from .vgg16_ecoset import VGG16_ecoset
+from .openclip import OpenCLIP
+from .inception_ecoset import Inception_ecoset
+from .official_clip import clip
