2814013e1aa66e964d9633feb48b6560ef02cfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:00 +0000

    Add package microprediction/1.1.21/microprediction-1.1.21-py3-none-any.whl

---
+++ packages/microprediction/microprediction-1.1.21-py3-none-any.whl/microprediction/univariate/cdfvalues.py
@@ -0,0 +1,9 @@
+from microconventions.stats_conventions import is_discrete, evenly_spaced_percentiles, cdf_values,\
+    is_process, nudged, discrete_cdf, sign_changes, quantize, StatsConventions
+import numpy as np
+
+
+# Utilities to help with CDF's
+
+from microprediction.univariate.arrivals import approx_mode
+
