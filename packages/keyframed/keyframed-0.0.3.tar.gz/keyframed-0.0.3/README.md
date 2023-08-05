9bae5e069081f4f3cc08558450d9c1a22ec38faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:42 +0000

    Add package keyframed/0.0.3/keyframed-0.0.3.tar.gz

---
+++ packages/keyframed/keyframed-0.0.3.tar.gz/keyframed-0.0.3/README.md
@@ -0,0 +1,3 @@
+# Keyframed
+
+Data structures and convenience functions to facilitate working with keyframed parameters and interpolations
---
+++ packages/keyframed/keyframed-0.0.3.tar.gz/keyframed-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+README = None
+if Path('README.md').exists():
+    with open('README.md','r') as f:
+        README = f.read()
+
+setup(
+    name='keyframed',
+    version='0.0.3',
+    author='David Marx',
+    long_description=README,
+    install_requires=[
+        'traces',
+        'numexpr',
+        'loguru',
+        'scipy',
+    ],
+    extras_require={
+        'dev': [
+            'pytest'
+    ]},
+    packages=find_packages(
+        where='src',
+        include=['keyframed*'],
+    ),
+    package_dir = {"": "src"},
+    license='MIT',
+)
