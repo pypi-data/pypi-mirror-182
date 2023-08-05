571a8de5f78eb5853571c1d0c526f9b76d4e2e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:58 +0000

    Add package dbt-selectdb/1.3.1/dbt-selectdb-1.3.1.tar.gz

---
+++ packages/dbt-selectdb/dbt-selectdb-1.3.1.tar.gz/dbt-selectdb-1.3.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import find_namespace_packages, setup
+
+package_name = "dbt-selectdb"
+# make sure this always matches dbt/adapters/{adapter}/__version__.py
+package_version = "1.3.1"
+dbt_core_version = "1.3.1"
+description = """The doris/selectdb adapter plugin for dbt, Original code fork from Apache Doris"""
+
+setup(
+    name=package_name,
+    version=package_version,
+    description=description,
+    long_description=description,
+    author="long2ice,catpineapple,JNSimba",
+    author_email="1391869588@qq.com",
+    url="https://github.com/selectdb/dbt-selectdb.git",
+    packages=find_namespace_packages(include=["dbt", "dbt.*"]),
+    include_package_data=True,
+    install_requires=[
+        "dbt-core~={}".format(dbt_core_version),
+        "mysql-connector-python>=8.0.0,<8.1",
+    ],
+    python_requires=">=3.8,<=3.10",
+)
