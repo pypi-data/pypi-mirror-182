4c02f8871ba95d92610cec1f4ad7fe33203eb77f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:19 +0000

    Add package tibber-aws/0.16.2/tibber_aws-0.16.2-py3-none-any.whl

---
+++ packages/tibber-aws/tibber_aws-0.16.2-py3-none-any.whl/tibber_aws/sns.py
@@ -0,0 +1,24 @@
+import json
+
+import boto3
+
+from .aws_base import AwsBase
+
+
+class Topic(AwsBase):
+    def __init__(self, topic_name, region_name="eu-west-1"):
+        super().__init__("sns", region_name)
+        self._topic_arn = (
+            boto3.resource(self._service_name, region_name)
+            .create_topic(Name=topic_name)
+            .arn
+        )
+
+    async def publish(self, subject, message):
+        await self.init_client_if_required()
+        await self._client.publish(
+            TargetArn=self._topic_arn,
+            Message=json.dumps({"default": json.dumps(message)}),
+            Subject=subject,
+            MessageStructure="json",
+        )
