37fd4c9442348503640f1069869e0c38e2ecf560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:48 +0000

    Add package autotest-tools/2.0.5/autotest-tools-2.0.5.tar.gz

---
+++ packages/autotest-tools/autotest-tools-2.0.5.tar.gz/autotest-tools-2.0.5/autotest_tools/browser_tool/browser_option.py
@@ -0,0 +1,17 @@
+option = {
+    # 无头模式
+    "headless": "--headless",
+    # 禁用GPU加速
+    "disable-gpu": "--disable-gpu",
+    # 配置User-Agent
+    "user-agent": "--user-agent="
+                  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/80.0.3987.132 Safari/537.36",
+    # 设置浏览器分辨率
+    "window-size": "--window-size=1920,1080",
+    # 禁用浏览器正在被自动化程序控制的提示
+    "disable-infobars": "--disable-infobars",
+    # 隐藏滚动条
+    "hide-scrollbars": "--hide-scrollbars"
+}
