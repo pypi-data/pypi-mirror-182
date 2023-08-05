8bb59bcb85f3506c9a9c2acf9181bbd7e3f21c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:42 +0000

    Add package volsite-postgres-common/0.0.7/volsite_postgres_common-0.0.7.tar.gz

---
+++ packages/volsite-postgres-common/volsite_postgres_common-0.0.7.tar.gz/volsite_postgres_common-0.0.7/src/volsite_postgres_common/api/CA.py
@@ -0,0 +1,71 @@
+from typing import Final
+
+
+# ====== CA: Common Attribute ======
+
+class CA:
+    Api: Final[str] = "api"
+    Auth: Final[str] = "aut"
+    Ascending: Final[str] = "asc"  # in SQL
+
+    Check: Final[str] = "chk"
+    Click: Final[str] = "clk"
+    Code: Final[str] = "cd"
+    Collect: Final[str] = "clt"
+    Collected: Final[str] = "cltd"
+    Color: Final[str] = "col"
+    Count: Final[str] = "cnt"
+    Create: Final[str] = "ct"
+    CreateTime: Final[str] = "crt"
+
+    Data: Final[str] = "dt"
+    Descending: Final[str] = "desc"  # in SQL
+    Description: Final[str] = "des"
+    DescriptionFormatted: Final[str] = "desf"
+    DescriptionId: Final[str] = "desid"
+    DescriptionPlain: Final[str] = "desp"
+
+    Finished: Final[str] = "fied"
+
+    Head: Final[str] = "hd"
+    HeadVersion: Final[str] = "hv"
+
+    Id: Final[str] = "id"
+
+    List: Final[str] = "lt"
+    Login: Final[str] = "lgn"
+
+    Name: Final[str] = "na"
+    Note: Final[str] = "nt"
+
+    Password: Final[str] = "pw"
+    Public: Final[str] = "pb"
+
+    Result: Final[str] = "ret"
+
+    Signup: Final[str] = "sgu"
+    SortBy: Final[str] = "stby"
+    SortDirection: Final[str] = "stdr"
+    Stars: Final[str] = "strs"
+    Start: Final[str] = "str"
+    State: Final[str] = "ste"
+
+    Time: Final[str] = "tm"
+    TimeZone: Final[str] = "tmz"
+    Title: Final[str] = "tt"
+    TitleId: Final[str] = "ttid"
+
+    Update: Final[str] = "ud"
+    UpdateTime: Final[str] = "udt"
+    Used: Final[str] = "used"
+    UserId: Final[str] = "uid"
+
+    Version: Final[str] = "vn"
+    VersionCreate: Final[str] = "vct"
+    VersionId: Final[str] = "vid"
+    Visited: Final[str] = "vst"
+    Visiting: Final[str] = "vstng"
+
+    Word: Final[str] = "w"
+
+    ___Error___: Final[str] = "err"
