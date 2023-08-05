c4a29e6260f835fa61f185cf6c94e6a9f8fed92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:58 +0000

    Add package PyPtt/1.0.1/PyPtt-1.0.1.tar.gz

---
+++ packages/PyPtt/PyPtt-1.0.1.tar.gz/PyPtt-1.0.1/setup.py
@@ -0,0 +1,52 @@
+# Always prefer setuptools over distlib_utils
+
+from setuptools import setup
+
+# sync with PyPtt/__init__.py
+version = '1.0.1'
+
+setup(
+    name='PyPtt',  # Required
+    version=version,  # Required
+    description='PyPtt\ngithub: https://github.com/PyPtt/PyPtt',  # Required
+    long_description=open('README.md', encoding="utf-8").read(),  # Optional
+    long_description_content_type='text/markdown',
+    url='https://github.com/PyPtt/PyPtt',  # Optional
+
+    author='CodingMan',  # Optional
+    author_email='pttcodingman@gmail.com',  # Optional
+    # https://pypi.org/classifiers/
+    classifiers=[  # Optional
+
+        'Development Status :: 5 - Production/Stable',
+
+        'Operating System :: OS Independent',
+
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: BBS',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+
+        'Natural Language :: Chinese (Traditional)',
+        'Natural Language :: English',
+    ],
+    keywords=['PTT', 'crawler', 'bot', 'library', 'websockets'],  # Optional
+
+    python_requires='>=3.8',
+    packages=['PyPtt'],
+    install_requires=[
+        'progressbar2',
+        'websockets',
+        'uao',
+        'SingleLog',
+        'requests',
+        'AutoStrEnum'
+    ],
+)
