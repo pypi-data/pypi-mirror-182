81535eef86c27162d3823fd935b46fb3a2c83f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:30 +0000

    Add package welford-torch/0.1.0/welford-torch-0.1.0.tar.gz

---
+++ packages/welford-torch/welford-torch-0.1.0.tar.gz/welford-torch-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.rst") as f:
+    readme = f.read()
+
+kwargs = {
+    "name": "welford-torch",
+    "version": "0.1.0",
+    "description": "Python (Pytorch) implementation of Welford's algorithm.",
+    "author": "Nicky Pochinkov",
+    "author_email": "work@nicky.pro",
+    "url": "https://github.com/pesvut/welford-torch",
+    "license": "MIT",
+    "keywords": ["statistics", "online", "welford", "torch"],
+    "install_requires": ["torch"],
+    "packages": ["welford_torch"],
+    "long_description": readme,
+}
+
+setup(**kwargs)
---
+++ packages/welford-torch/welford-torch-0.1.0.tar.gz/welford-torch-0.1.0/welford_torch/__init__.py
@@ -0,0 +1,1 @@
+from .welford_torch import *
