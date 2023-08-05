3c8360d06eec6211b54f1c3a9064d32a5d862d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:01 +0000

    Add package felupe/6.2.1/felupe-6.2.1-py3-none-any.whl

---
+++ packages/felupe/felupe-6.2.1-py3-none-any.whl/felupe/__init__.py
@@ -0,0 +1,131 @@
+from .__about__ import __version__
+from . import math
+from . import mesh
+from . import quadrature
+from . import dof
+from . import element
+from . import tools
+from . import constitution
+from . import solve
+from . import region
+from . import mechanics
+
+from .region import (
+    Region,
+    RegionBoundary,
+    RegionQuad,
+    RegionHexahedron,
+    RegionQuadBoundary,
+    RegionHexahedronBoundary,
+    RegionConstantQuad,
+    RegionConstantHexahedron,
+    RegionQuadraticHexahedron,
+    RegionTriQuadraticHexahedron,
+    RegionTriangle,
+    RegionTetra,
+    RegionQuadraticTriangle,
+    RegionQuadraticTetra,
+    RegionTriangleMINI,
+    RegionTetraMINI,
+    RegionLagrange,
+)
+from ._assembly import (
+    # IntegralForm,
+    IntegralFormMixed as IntegralForm,
+    # IntegralFormAxisymmetric,
+    # LinearForm,
+    # BilinearForm,
+    # LinearFormMixed as LinearForm,
+    # BilinearFormMixed as BilinearForm,
+    # BaseForm,
+    Form,
+)
+from ._basis import (
+    BasisMixed as Basis,
+)
+from ._field import (
+    Field,
+    FieldAxisymmetric,
+    FieldPlaneStrain,
+    FieldContainer,
+    FieldsMixed,
+)
+from .dof import Boundary
+from .element import (
+    Line,
+    #
+    Quad,
+    ConstantQuad,
+    #
+    ArbitraryOrderLagrange as ArbitraryOrderLagrangeElement,
+    #
+    Hexahedron,
+    ConstantHexahedron,
+    QuadraticHexahedron,
+    TriQuadraticHexahedron,
+    #
+    Triangle,
+    TriangleMINI,
+    QuadraticTriangle,
+    #
+    Tetra,
+    TetraMINI,
+    QuadraticTetra,
+)
+from .quadrature import (
+    GaussLegendre,
+    GaussLegendreBoundary,
+    Triangle as TriangleQuadrature,
+    Tetrahedron as TetrahedronQuadrature,
+)
+from .mesh import (
+    Mesh,
+    MeshContainer,
+    Rectangle,
+    Cube,
+    Grid,
+)
+from .constitution import (
+    UserMaterial,
+    UserMaterialHyperelastic,
+    UserMaterialStrain,
+    NeoHooke,
+    OgdenRoxburgh,
+    LinearElastic,
+    LinearElasticPlaneStress,
+    LinearElasticPlaneStrain,
+    LinearElasticPlasticIsotropicHardening,
+    ThreeFieldVariation,
+    LineChange,
+    AreaChange,
+    VolumeChange,
+)
+from .tools import (
+    newtonrhapson,
+    save,
+    topoints,
+    project,
+)
+from .mechanics import (
+    SolidBody,
+    SolidBodyNearlyIncompressible,
+    StateNearlyIncompressible,
+    SolidBodyPressure,
+    SolidBodyGravity,
+    PointLoad,
+    Step,
+    Job,
+    CharacteristicCurve,
+)
+
+try:
+    from .mechanics import (
+        MultiPointConstraint,
+        MultiPointContact,
+    )
+except:
+    pass
+
+__all__ = [
+    "__version__",
+]
