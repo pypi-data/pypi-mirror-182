f2ba9ee5759193522993b1fa19767c72371b71d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:58 +0000

    Add package tibber-aws/0.16.1/tibber_aws-0.16.1.tar.gz

---
+++ packages/tibber-aws/tibber_aws-0.16.1.tar.gz/tibber_aws-0.16.1/README.md
@@ -0,0 +1,21 @@
+# tibber-pyAws
+AWS related packages.
+Built using GitHub Actions and releases created from GitHub and pushed to pypi.
+
+## Packages
+* [tibber_aws](https://pypi.org/project/tibber-aws/)
+
+## Create Release
+
+### CLI
+* Update version in setup.py
+* `gh release create <tag> -t <tag>`
+Example
+```sh
+gh release create 0.8.6 -t 0.8.6
+```
+
+### Manually
+* Update version in setup.py
+* Create a [Release](https://github.com/tibber/tibber-pyAws/releases)
+  This will push the package to pypi.
---
+++ packages/tibber-aws/tibber_aws-0.16.1.tar.gz/tibber_aws-0.16.1/tibber_aws/secret_manager.py
@@ -0,0 +1,35 @@
+import base64
+import json
+import logging
+
+import boto3
+from botocore.exceptions import ClientError, NoCredentialsError, UnauthorizedSSOTokenError
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_REGION_NAME = "eu-west-1"
+
+
+def get_secret_parser(secret_name, region_name=DEFAULT_REGION_NAME):
+    try:
+        secret = get_secret(secret_name, region_name)
+    except (ClientError, NoCredentialsError, UnauthorizedSSOTokenError):
+        _LOGGER.error("No credentials")
+        return {}
+    if secret is None:
+        return {}
+    return json.loads(secret)
+
+
+def get_secret(secret_name, region_name=DEFAULT_REGION_NAME):
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+
+    get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    # Decrypts secret using the associated KMS CMK.
+    # Depending on whether the secret is a string or binary, one of these fields will be populated.
+    if "SecretString" in get_secret_value_response:
+        return get_secret_value_response["SecretString"]
+    return base64.b64decode(get_secret_value_response["SecretBinary"])
