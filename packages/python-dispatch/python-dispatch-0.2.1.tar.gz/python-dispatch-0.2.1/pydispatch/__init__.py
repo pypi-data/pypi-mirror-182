9e410f197bfa0091b48cadc53b7d27e710b51af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:53 +0000

    Add package python-dispatch/0.2.1/python-dispatch-0.2.1.tar.gz

---
+++ packages/python-dispatch/python-dispatch-0.2.1.tar.gz/python-dispatch-0.2.1/pydispatch/__init__.py
@@ -0,0 +1,19 @@
+import sys
+import warnings
+try:
+    import importlib.metadata as importlib_metadata
+except ImportError:
+    import importlib_metadata
+
+try:
+    __version__ = importlib_metadata.version('python-dispatch')
+except: # pragma: no cover
+    __version__ = 'unknown'
+
+if sys.version_info < (3, 6): # pragma: no cover
+    warnings.warn('You are using `python-dispatch` with a deprecated Python version. '
+                  'After version 0.1.x, `python-dispatch` will only support Python 3.6 or greater.',
+                  UserWarning)
+
+from pydispatch.dispatch import Dispatcher, Event
+from pydispatch.properties import *
