30f80d45325acb47f78db635e7af39090696aac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:56 +0000

    Add package starwhale/0.3.4/starwhale-0.3.4-py3-none-any.whl

---
+++ packages/starwhale/starwhale-0.3.4-py3-none-any.whl/starwhale/api/_impl/dataset/__init__.py
@@ -0,0 +1,48 @@
+from starwhale.core.dataset.type import (
+    Link,
+    Text,
+    Audio,
+    Image,
+    Video,
+    Binary,
+    LinkAuth,
+    LinkType,
+    MIMEType,
+    ClassLabel,
+    S3LinkAuth,
+    BoundingBox,
+    GrayscaleImage,
+    LocalFSLinkAuth,
+    DefaultS3LinkAuth,
+    COCOObjectAnnotation,
+)
+
+from .model import Dataset
+from .loader import get_data_loader, SWDSBinDataLoader, UserRawDataLoader
+from .builder import BuildExecutor, SWDSBinBuildExecutor, UserRawBuildExecutor
+
+__all__ = [
+    "get_data_loader",
+    "Link",
+    "LinkAuth",
+    "DefaultS3LinkAuth",
+    "LocalFSLinkAuth",
+    "S3LinkAuth",
+    "MIMEType",
+    "LinkType",
+    "BuildExecutor",  # SWDSBinBuildExecutor alias
+    "UserRawBuildExecutor",
+    "SWDSBinBuildExecutor",
+    "SWDSBinDataLoader",
+    "UserRawDataLoader",
+    "Binary",
+    "Text",
+    "Audio",
+    "Video",
+    "Image",
+    "ClassLabel",
+    "BoundingBox",
+    "GrayscaleImage",
+    "COCOObjectAnnotation",
+    "Dataset",
+]
---
+++ packages/starwhale/starwhale-0.3.4-py3-none-any.whl/starwhale/api/service.py
@@ -0,0 +1,21 @@
+from ._impl.service import (
+    Api,
+    api,
+    Input,
+    Output,
+    Request,
+    Service,
+    Response,
+    JsonOutput,
+)
+
+__all__ = [
+    "Service",
+    "Request",
+    "JsonOutput",
+    "Api",
+    "api",
+    "Input",
+    "Output",
+    "Response",
+]
