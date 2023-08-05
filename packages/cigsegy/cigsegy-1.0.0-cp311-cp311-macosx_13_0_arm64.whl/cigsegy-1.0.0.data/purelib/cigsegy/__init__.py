ce3a89ae4ede364b77d2b5cc7e978dacbe3ea128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:20 +0000

    Add package cigsegy/1.0.0/cigsegy-1.0.0-cp311-cp311-macosx_13_0_arm64.whl

---
+++ packages/cigsegy/cigsegy-1.0.0-cp311-cp311-macosx_13_0_arm64.whl/cigsegy-1.0.0.data/purelib/cigsegy/__init__.py
@@ -0,0 +1,16 @@
+from .cigsegy import (Pysegy, fromfile, fromfile_ignore_header, tofile,
+                      tofile_ignore_header, collect)
+from .tools import (create, textual_header, metaInfo)
+
+
+__all__ = [
+    "Pysegy", 
+    "fromfile", 
+    "fromfile_ignore_header", 
+    "tofile",
+    "tofile_ignore_header", 
+    "create", 
+    "collect", 
+    "textual_header", 
+    "metaInfo"
+]
