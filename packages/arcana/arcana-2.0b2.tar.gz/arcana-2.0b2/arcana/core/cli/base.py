e75f42a3e595a526d084ecdf707efda9026dd3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:44 +0000

    Add package arcana/2.0b2/arcana-2.0b2.tar.gz

---
+++ packages/arcana/arcana-2.0b2.tar.gz/arcana-2.0b2/arcana/core/cli/base.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import pkgutil
+from importlib import import_module
+import click
+import arcana
+
+
+# Define the base CLI entrypoint
+@click.group()
+@click.version_option(version=arcana.__version__)
+def cli():
+    """Base command line group, installed as "arcana"."""
+
+
+@cli.group()
+def ext():
+    """Command-line group for extension hooks"""
+
+
+CLI_EXT_PKG = Path(arcana.__file__).parent / "cli"
+
+# Ensure that all sub-packages under CLI are loaded so they are added to the
+# base command
+for module in pkgutil.iter_modules([str(CLI_EXT_PKG)], prefix="arcana.cli."):
+    import_module(module.name)
