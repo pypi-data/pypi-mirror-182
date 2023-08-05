8d5a9748adc56b4e023be000203eb11d8c44ed8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:30 +0000

    Add package skysurvey/0.3.8/skysurvey-0.3.8-py3.9.egg

---
+++ packages/skysurvey/skysurvey-0.3.8-py3.9.egg/skysurvey/survey/ztf.py
@@ -0,0 +1,40 @@
+from .polygon import Survey
+from ztffields.fields import Fields
+
+__all__ = ["ZTF"]
+
+class ZTF( Survey ):
+
+    def __init__(self, data=None, level="focalplane", **kwargs):
+        """ """
+        fields = Fields.get_field_geometry(level=level)
+        return super().__init__(data=data, fields=fields)
+
+    @classmethod
+    def from_logs(cls, **kwargs):
+        """ """
+        from ztfquery.skyvision import get_summary_logs
+        logs = get_summary_logs(**kwargs)
+        return cls.from_pointings(data=logs, level="quadrant")
+        
+        
+    @classmethod
+    def from_pointings(cls, data, level="quadrant"):
+        """ """
+        if type(data) is dict:
+            data = pandas.DataFrame.from_dict(data)
+            
+        return cls(data=data, level=level)
+
+
+    @classmethod
+    def from_random(cls, size, bands, mjd_range, skynoise_range,
+                    level="focalplane", **kwargs):
+        """ 
+        fields
+        """
+        this = cls(level=level)
+        this.draw_random(size, bands,  
+                        mjd_range, skynoise_range, 
+                        inplace=True, **kwargs)
+        return this
