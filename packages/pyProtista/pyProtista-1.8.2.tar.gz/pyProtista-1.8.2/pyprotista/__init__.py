e514b9b3b7838ce2158025cbcbcc84753416c55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:56 +0000

    Add package pyProtista/1.8.2/pyProtista-1.8.2.tar.gz

---
+++ packages/pyProtista/pyProtista-1.8.2.tar.gz/pyProtista-1.8.2/pyprotista/__init__.py
@@ -0,0 +1,5 @@
+"""Init file."""
+import pyprotista.parsers
+import pyprotista.parsers.ident
+import pyprotista.parsers.quant
+from pyprotista.unify import Unify
---
+++ packages/pyProtista/pyProtista-1.8.2.tar.gz/pyProtista-1.8.2/tests/test_engine_parser_base_parser.py
@@ -0,0 +1,11 @@
+import pytest
+
+from pyprotista.parsers.base_parser import BaseParser
+
+
+def test_uninitialized_parser_compatiblity_is_false():
+    input_file = (
+        pytest._test_path / "data" / "test_Creinhardtii_QE_pH11_xtandem_alanine.xml"
+    )
+    compat = BaseParser.check_parser_compatibility(input_file)
+    assert compat is False
