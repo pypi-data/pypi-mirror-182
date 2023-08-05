708c4593633f705fadf98baa2f5834d196747ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:33 +0000

    Add package multivectors/1.2.1.post0/multivectors-1.2.1.post0.tar.gz

---
+++ packages/multivectors/multivectors-1.2.1.post0.tar.gz/multivectors-1.2.1.post0/setup.py
@@ -0,0 +1,13 @@
+import re
+from setuptools import setup
+
+with open('multivectors.py') as f:
+    text = f.read()
+match = re.search('"""(.+?)"""', text, re.S)
+if match is None:
+    raise ValueError('Could not find module docstring in multivectors.py')
+longdesc = match.group(1).strip()
+with open('README.rst', 'w') as f:
+    f.write(longdesc)
+
+setup()
