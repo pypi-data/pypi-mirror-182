d763d33687d9a06a3fc474e1fb2884f77e1f2308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:38 +0000

    Add package colin-qinglong/1.0.0/colin-qinglong-1.0.0.tar.gz

---
+++ packages/colin-qinglong/colin-qinglong-1.0.0.tar.gz/colin-qinglong-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+      name='colin-qinglong', #项目名称，取值随意
+      version='1.0.0', #版本号
+      py_modules=['myQLApi'], #对应模块文件的名称
+      author='colin',
+      author_email='colin@163.com',
+      url='https://github.com/LiuYang7/MyqinglongSDK',
+      description='个人使用'
+      )
