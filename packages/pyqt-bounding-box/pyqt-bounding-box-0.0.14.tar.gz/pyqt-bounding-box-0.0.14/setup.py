7f455cbc55e0c9a161694fc389a6c535ff1f00f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:20:56 +0000

    Add package pyqt-bounding-box/0.0.14/pyqt-bounding-box-0.0.14.tar.gz

---
+++ packages/pyqt-bounding-box/pyqt-bounding-box-0.0.14.tar.gz/pyqt-bounding-box-0.0.14/setup.py
@@ -0,0 +1,26 @@
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+
+setup(
+    name='pyqt-bounding-box',
+    version='0.0.14',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description='PyQt bounding box for graphic design software',
+    url='https://github.com/yjg30737/pyqt-bounding-box.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    install_requires=[
+        'PyQt5>=5.8'
+    ]
+)
