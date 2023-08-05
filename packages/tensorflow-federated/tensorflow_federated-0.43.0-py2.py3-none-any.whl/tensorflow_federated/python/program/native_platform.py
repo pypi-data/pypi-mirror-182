84b309dee00dc0d062636dc653996edb35038ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:16 +0000

    Add package tensorflow-federated/0.43.0/tensorflow_federated-0.43.0-py2.py3-none-any.whl

---
+++ packages/tensorflow-federated/tensorflow_federated-0.43.0-py2.py3-none-any.whl/tensorflow_federated/python/program/native_platform.py
@@ -0,0 +1,203 @@
+# Copyright 2022, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""A federated platform implemented using native TFF components."""
+
+import asyncio
+from collections.abc import Awaitable
+import inspect
+import typing
+from typing import Any
+
+from tensorflow_federated.python.common_libs import async_utils
+from tensorflow_federated.python.common_libs import py_typecheck
+from tensorflow_federated.python.common_libs import structure
+from tensorflow_federated.python.core.impl.computation import computation_base
+from tensorflow_federated.python.core.impl.context_stack import context_base
+from tensorflow_federated.python.core.impl.types import computation_types
+from tensorflow_federated.python.core.impl.types import placements
+from tensorflow_federated.python.core.impl.types import type_conversions
+from tensorflow_federated.python.program import federated_context
+from tensorflow_federated.python.program import value_reference
+
+
+class AwaitableValueReference(value_reference.MaterializableValueReference):
+  """A `tff.program.MaterializableValueReference` backed by an `Awaitable`."""
+
+  def __init__(self,
+               awaitable: Awaitable[value_reference.MaterializablePythonType],
+               type_signature: value_reference.MaterializableTffType):
+    """Returns an initialized `tff.program.AwaitableValueReference`.
+
+    Args:
+      awaitable: An `Awaitable` that returns the referenced value.
+      type_signature: The `tff.Type` of this object.
+    """
+    if not inspect.isawaitable(awaitable):
+      raise TypeError(f'Expected a `Awaitable`, found {type(awaitable)}')
+    py_typecheck.check_type(
+        type_signature, typing.get_args(value_reference.MaterializableTffType))
+
+    self._awaitable = awaitable
+    self._type_signature = type_signature
+    self._value = None
+
+  @property
+  def type_signature(self) -> value_reference.MaterializableTffType:
+    """The `tff.TensorType` of this object."""
+    return self._type_signature
+
+  async def get_value(self) -> value_reference.MaterializablePythonType:
+    """Returns the referenced value as a numpy scalar or array."""
+    if self._value is None:
+      self._value = await self._awaitable
+    return self._value
+
+  def __eq__(self, other: Any) -> bool:
+    if self is other:
+      return True
+    elif not isinstance(other, AwaitableValueReference):
+      return NotImplemented
+    return (self._type_signature == other._type_signature and
+            self._awaitable == other._awaitable)
+
+
+def _create_structure_of_awaitable_references(
+    awaitable: Awaitable[Any], type_signature: computation_types.Type) -> Any:
+  """Returns a structure of `tff.program.AwaitableValueReference`s."""
+  if not inspect.isawaitable(awaitable):
+    raise TypeError(f'Expected an `Awaitable`, found {type(awaitable)}')
+  py_typecheck.check_type(type_signature, computation_types.Type)
+
+  if type_signature.is_struct():
+
+    async def _to_structure(
+        awaitable: Awaitable[structure.Struct]) -> structure.Struct:
+      return structure.from_container(await awaitable)
+
+    awaitable = _to_structure(awaitable)
+    # A `async_utils.SharedAwaitable` is required to materialize structures of
+    # values sequentially or concurrently. This happens when `get_item` is
+    # invoked for each element.
+    shared_awaitable = async_utils.SharedAwaitable(awaitable)
+
+    async def _get_item(awaitable: Awaitable[structure.Struct],
+                        index: int) -> Any:
+      value = await awaitable
+      return value[index]
+
+    elements = []
+    element_types = structure.iter_elements(type_signature)
+    for index, (name, element_type) in enumerate(element_types):
+      element_awaitable = _get_item(shared_awaitable, index)
+      # A `async_utils.SharedAwaitable` is required to materialize structures of
+      # values multiple times. This happens when a value is released using
+      # multiple `tff.program.ReleaseManager`s.
+      element_shared_awaitable = async_utils.SharedAwaitable(element_awaitable)
+      element = _create_structure_of_awaitable_references(
+          element_shared_awaitable, element_type)
+      elements.append((name, element))
+    return structure.Struct(elements)
+  elif (type_signature.is_federated() and
+        type_signature.placement == placements.SERVER):
+    return _create_structure_of_awaitable_references(awaitable,
+                                                     type_signature.member)
+  elif type_signature.is_sequence():
+    return AwaitableValueReference(awaitable, type_signature)
+  elif type_signature.is_tensor():
+    return AwaitableValueReference(awaitable, type_signature)
+  else:
+    raise NotImplementedError(f'Unexpected type found: {type_signature}.')
+
+
+async def _materialize_structure_of_value_references(
+    value: Any, type_signature: computation_types.Type) -> Any:
+  """Returns a structure of materialized values."""
+  py_typecheck.check_type(type_signature, computation_types.Type)
+
+  async def _materialize(value: Any) -> Any:
+    if isinstance(value, value_reference.MaterializableValueReference):
+      return await value.get_value()
+    else:
+      return value
+
+  if type_signature.is_struct():
+    value = structure.from_container(value)
+    element_types = list(structure.iter_elements(type_signature))
+    element_awaitables = [
+        _materialize_structure_of_value_references(v, t)
+        for v, (_, t) in zip(value, element_types)
+    ]
+    elements = await asyncio.gather(*element_awaitables)
+    elements = [(n, v) for v, (n, _) in zip(elements, element_types)]
+    return structure.Struct(elements)
+  elif (type_signature.is_federated() and
+        type_signature.placement == placements.SERVER):
+    return await _materialize_structure_of_value_references(
+        value, type_signature.member)
+  elif type_signature.is_sequence():
+    return await _materialize(value)
+  elif type_signature.is_tensor():
+    return await _materialize(value)
+  else:
+    return value
+
+
+class NativeFederatedContext(federated_context.FederatedContext):
+  """A `tff.program.FederatedContext` backed by an `tff.framework.AsyncContext`.
+  """
+
+  def __init__(self, context: context_base.AsyncContext):
+    """Returns an initialized `tff.program.NativeFederatedContext`.
+
+    Args:
+      context: An `tff.framework.AsyncContext`.
+    """
+    py_typecheck.check_type(context, context_base.AsyncContext)
+
+    self._context = context
+
+  def invoke(self, comp: computation_base.Computation, arg: Any) -> Any:
+    """Invokes the `comp` with the argument `arg`.
+
+    Args:
+      comp: The `tff.Computation` being invoked.
+      arg: The optional argument of `comp`.
+
+    Returns:
+      The result of invocation, must contain only structures, server-placed
+      values, or tensors.
+
+    Raises:
+      ValueError: If the result type of the invoked comptuation does not contain
+      only structures, server-placed values, or tensors.
+    """
+    py_typecheck.check_type(comp, computation_base.Computation)
+    result_type = comp.type_signature.result
+    if not federated_context.contains_only_server_placed_data(result_type):
+      raise ValueError(
+          'Expected the result type of the invoked computation to contain only '
+          'structures, server-placed values, or tensors, found '
+          f'\'{result_type}\'.')
+
+    async def _invoke(context: context_base.AsyncContext,
+                      comp: computation_base.Computation, arg: Any) -> Any:
+      if comp.type_signature.parameter is not None:
+        arg = await _materialize_structure_of_value_references(
+            arg, comp.type_signature.parameter)
+      return await context.invoke(comp, arg)
+
+    result_coro = _invoke(self._context, comp, arg)
+    result = _create_structure_of_awaitable_references(result_coro, result_type)
+    result = type_conversions.type_to_py_container(result, result_type)
+    return result
---
+++ packages/tensorflow-federated/tensorflow_federated-0.43.0-py2.py3-none-any.whl/tensorflow_federated/python/program/release_manager.py
@@ -0,0 +1,190 @@
+# Copyright 2019, The TensorFlow Federated Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Utilities for releasing values from a federated program."""
+
+import abc
+import asyncio
+import collections
+from collections.abc import Callable, Sequence
+from typing import Any, Optional, Union
+
+import tree
+
+from tensorflow_federated.python.common_libs import py_typecheck
+from tensorflow_federated.python.common_libs import structure
+from tensorflow_federated.python.core.impl.types import computation_types
+
+
+class ReleaseManager(metaclass=abc.ABCMeta):
+  """An interface for releasing values from a federated program.
+
+  A `tff.program.ReleaseManager` is used to release values from platform storage
+  to customer storage in a federated program.
+  """
+
+  @abc.abstractmethod
+  async def release(self, value: Any, type_signature: computation_types.Type,
+                    key: Any) -> None:
+    """Releases `value` from a federated program.
+
+    An implementation of this interface should be specific about the types of
+    `value` and `key` for this method and should document how the `key` will be
+    used. This allows a federated program to understand how to create a `key`
+    for the `value` before it is released. For example, a
+    `tff.program.ReleaseManager` that releases metrics keyed by a strictly
+    increasing integer might specify a `value` type of `Mapping[str, Any]` and
+    a `key` type of `int`.
+
+    Args:
+      value: A materialized value, a value reference, or a structure of
+        materialized values and value references representing the value to
+        release. The exact structure of `value` is left up to the implementation
+        of `tff.program.ReleaseManager`.
+      type_signature: The `tff.Type` of `value`.
+      key: An optional value used to reference the released `value`, the exact
+        type and structure of `key` and how `key` is used is left up to the
+        implementation of `tff.program.ReleaseManager`.
+    """
+    raise NotImplementedError
+
+
+_FILTERED_SUBTREE = object()
+
+
+class FilteringReleaseManager(ReleaseManager):
+  """A `tff.program.ReleaseManager` that filters values before releasing them.
+
+  A `tff.program.FilteringReleaseManager` is a utility for filtering values
+  before releasing the values and is used to release values from platform
+  storage to customer storage in a federated program.
+
+  Values are filtered and released using the given `release_manager`.
+  """
+
+  def __init__(self, release_manager: ReleaseManager,
+               filter_fn: Callable[[tuple[Union[str, int], ...]], bool]):
+    """Returns an initialized `tff.program.FilteringReleaseManager`.
+
+    The `filter_fn` is a `Callable` that has a single parameter `path` and
+    returns a `bool`, and is used to filter values before they are released. A
+    `path` is a tuple of indices and/or keys which uniquely identifies the
+    position of the corresponding item in the `value`; `path` matches the
+    expectations of the `dm-tree` library. For example, this function could be
+    used to release a single metric with the name 'loss':
+
+    ```
+    def filter_fn(path) -> bool:
+      if path == ('loss', ):
+        return True
+      else:
+        return False
+    ```
+
+    Args:
+      release_manager: A `tff.program.ReleaseManager` used to release values to.
+      filter_fn: A `Callable` used to filter values before they are released,
+        this function has a single parameter `path` and returns a `bool`.
+    """
+    py_typecheck.check_type(release_manager, ReleaseManager)
+    py_typecheck.check_callable(filter_fn)
+
+    self._release_manager = release_manager
+    self._filter_fn = filter_fn
+
+  async def release(self, value: Any, type_signature: computation_types.Type,
+                    key: Any) -> None:
+    """Releases `value` from a federated program.
+
+    Args:
+      value: A materialized value, a value reference, or a structure of release.
+      type_signature: The `tff.Type` of `value`.
+      key: An optional value used to reference the released `value`.
+    """
+
+    def _fn(path: tuple[Union[str, int], ...],
+            subtree: tree.Structure) -> Optional[tree.Structure]:
+      if not tree.is_nested(subtree):
+        if self._filter_fn(path):
+          return None
+        else:
+          return _FILTERED_SUBTREE
+      else:
+        if isinstance(subtree, collections.OrderedDict):
+          return collections.OrderedDict([
+              (k, v) for k, v in subtree.items() if v is not _FILTERED_SUBTREE
+          ])
+        elif isinstance(subtree, dict):
+          items = sorted(subtree.items())
+          return {k: v for k, v in items if v is not _FILTERED_SUBTREE}
+        elif (isinstance(subtree, (list, tuple)) and
+              not py_typecheck.is_named_tuple(subtree)):
+          return [x for x in subtree if x is not _FILTERED_SUBTREE]
+        else:
+          raise NotImplementedError(f'Unexpected type found: {type(subtree)}.')
+
+    filtered_value = tree.traverse_with_path(_fn, value, top_down=False)
+
+    if type_signature.is_struct():
+      type_signature = structure.to_odict_or_tuple(type_signature)
+    filtered_type = tree.traverse_with_path(_fn, type_signature, top_down=False)
+    if tree.is_nested(filtered_type):
+      filtered_type = computation_types.to_type(filtered_type)
+
+    await self._release_manager.release(filtered_value, filtered_type, key)
+
+
+class GroupingReleaseManager(ReleaseManager):
+  """A `tff.program.ReleaseManager` that releases values to other release managers.
+
+  A `tff.program.GroupingReleaseManager` is a utility for release values from a
+  federated program to a collection of other release managers and is used to
+  release values from platform storage to customer storage in a federated
+  program.
+
+  Values are released using each of the `tff.program.ReleaseManager`s in the
+  given `release_managers`.
+  """
+
+  def __init__(self, release_managers: Sequence[ReleaseManager]):
+    """Returns an initialized `tff.program.GroupingReleaseManager`.
+
+    Args:
+      release_managers: A sequence of `tff.program.ReleaseManager` used to
+        release values to.
+
+    Raises:
+      ValueError: If `release_managers` is empty.
+    """
+    py_typecheck.check_type(release_managers, Sequence)
+    if not release_managers:
+      raise ValueError('Expected `release_managers` to not be empty.')
+    for release_manager in release_managers:
+      py_typecheck.check_type(release_manager, ReleaseManager)
+
+    self._release_managers = release_managers
+
+  async def release(self, value: Any, type_signature: computation_types.Type,
+                    key: Any) -> None:
+    """Releases `value` from a federated program.
+
+    Args:
+      value: A materialized value, a value reference, or a structure of
+        materialized values and value references representing the value to
+        release.
+      type_signature: The `tff.Type` of `value`.
+      key: An optional value used to reference the released `value`.
+    """
+    await asyncio.gather(
+        *
+        [m.release(value, type_signature, key) for m in self._release_managers])
