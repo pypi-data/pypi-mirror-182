3f70e13a819a9fcf4f9d2feefbf2dbc051f367de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:51 +0000

    Add package django-wm/4.0.1.dev4/django-wm-4.0.1.dev4.tar.gz

---
+++ packages/django-wm/django-wm-4.0.1.dev4.tar.gz/django-wm-4.0.1.dev4/mentions/helpers/resolution.py
@@ -0,0 +1,73 @@
+import logging
+from typing import Dict, Optional, Sequence, Set, Tuple, Type
+
+from mentions import contract
+from mentions.helpers.types import (
+    MentionableImpl,
+    ModelFilter,
+    ModelFilterMap,
+    UrlKwarg,
+)
+
+__all__ = [
+    "get_model_for_url_by_helper",
+]
+
+TypeSet = Set[Tuple[UrlKwarg, ModelFilter]]
+
+
+log = logging.getLogger(__name__)
+
+
+def get_model_for_url_by_helper(
+    model_class: Type[MentionableImpl],
+    urlpattern_args: Sequence,
+    urlpattern_kwargs: Dict,
+) -> Optional[MentionableImpl]:
+    """Resolve a model instance from urlpattern kwargs, as configured by
+    `mentions_path` or `mentions_re_path` helper functions.
+
+    Args:
+        model_class: A class that inherits MentionableMixin.
+        urlpattern_args: args from a `ResolverMatch`.
+        urlpattern_kwargs: kwargs from a `ResolverMatch`.
+
+    Returns:
+        An instance of `model_class`.
+
+    Raises:
+        KeyError: If the given urlpattern_kwargs does not contain helper values.
+    """
+    log.warning(
+        f"get_model_for_url_by_helper(model_class={model_class}, urlpattern_args={urlpattern_args} urlpattern_kwargs={urlpattern_kwargs})"
+    )
+
+    model_filter_map: ModelFilterMap = urlpattern_kwargs.pop(
+        contract.URLPATTERNS_MODEL_FILTER_MAP
+    )
+
+    mapping = unpack_model_filter_map(model_filter_map)
+    query = {value: urlpattern_kwargs[key] for key, value in mapping}
+
+    model_filters: Sequence = urlpattern_kwargs.pop(
+        contract.URLPATTERNS_MODEL_FILTERS, []
+    )
+    for key, value in zip(model_filters, urlpattern_args):
+        query[key] = value
+
+    return model_class.objects.get(**query)
+
+
+def unpack_model_filter_map(mapping: ModelFilterMap) -> TypeSet:
+    if isinstance(mapping, dict):
+        mapping = set(mapping.items())
+
+    def unpack(item):
+        # If mapping is a string, replace it with an 'identity' tuple of it: key == value.
+        if isinstance(item, str):
+            return item, item
+        return item
+
+    mapping = {unpack(x) for x in mapping}
+
+    return mapping
