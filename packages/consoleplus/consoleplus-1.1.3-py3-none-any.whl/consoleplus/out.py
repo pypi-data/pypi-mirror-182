4e9c8669126a714d24086e5c775ddae9aadda821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:01 +0000

    Add package consoleplus/1.1.3/consoleplus-1.1.3-py3-none-any.whl

---
+++ packages/consoleplus/consoleplus-1.1.3-py3-none-any.whl/consoleplus/out.py
@@ -0,0 +1,22 @@
+from sys import stdout
+from time import sleep
+import os
+class Output:
+
+    def write(self, text, time=0.01):
+        for char in text:
+            stdout.write(char)
+            stdout.flush()
+            sleep(time)
+        print()
+
+    def clear(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def write_centered(self, text, time=0.01):
+        text = text.center(os.get_terminal_size().columns)
+        if len(text.splitlines()) > 1:
+            for line in text.splitlines():
+                self.write_centered(line)
+        else:
+            self.write(text, time)
