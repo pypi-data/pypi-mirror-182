0b471de4ab6003f0e150b76f99271cd26dbc2825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:33 +0000

    Add package midi-utils/1.1.0/midi-utils-1.1.0.tar.gz

---
+++ packages/midi-utils/midi-utils-1.1.0.tar.gz/midi-utils-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+config = {
+    "name": "midi-utils",
+    "version": "1.1.0",
+    "packages": find_packages(),
+    "test_requires": [
+        "pytest",
+    ],
+    "author": "Brian Abelson",
+    "author_email": "hey@gltd.email",
+    "description": "midi-utils provides a couple of simple tools to generate midi notes in different scales",
+    "url": "https://gitlab.com/gltd/midi-utils",
+}
+
+setup(**config)
