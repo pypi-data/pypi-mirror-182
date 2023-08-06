b17412100506b5b0604bbf86261bc7c2535a0886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:20 +0000

    Add package myMilePackageShv/0.897/myMilePackageShv-0.897.tar.gz

---
+++ packages/myMilePackageShv/myMilePackageShv-0.897.tar.gz/myMilePackageShv-0.897/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myMilePackageShv',
+    version='0.897',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A test python package',
+	url='https://github.com/atevhs/MymileConv',
+    author='Shv',
+    author_email='atevhs@gmail.com'
+)
