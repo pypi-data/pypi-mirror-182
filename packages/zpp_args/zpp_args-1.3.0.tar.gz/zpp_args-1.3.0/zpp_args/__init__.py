45d57d0feed369284f670dafb2d7810cce84933c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:10 +0000

    Add package zpp_args/1.3.0/zpp_args-1.3.0.tar.gz

---
+++ packages/zpp_args/zpp_args-1.3.0.tar.gz/zpp_args-1.3.0/zpp_args/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.3.0'
+__author__ = 'ZephyrOff  (Alexandre Pajak)'
+__descriptor__ = "Module pour le traitement des arguments d'une ligne de commande"
+__lastupdate__ = '23/12/2022'
+__releasenote__ = 'Add category options'
+
+from .args import *
