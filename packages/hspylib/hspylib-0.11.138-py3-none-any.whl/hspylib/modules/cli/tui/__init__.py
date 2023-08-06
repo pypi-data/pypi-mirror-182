3cb2071ee62f06237674ffc1193430e75bf36c89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:12 +0000

    Add package hspylib/0.11.138/hspylib-0.11.138-py3-none-any.whl

---
+++ packages/hspylib/hspylib-0.11.138-py3-none-any.whl/hspylib/modules/cli/tui/__init__.py
@@ -0,0 +1,19 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.138
+#
+# Package: main.hspylib.modules.cli.tui
+"""Package initialization."""
+
+__all__ = [
+  'mchoose', 
+  'mdashboard', 
+  'menu', 
+  'minput', 
+  'mselect', 
+  'table', 
+  'tui_application', 
+  'tui_component', 
+  'tui_preferences'
+]
+__version__ = '0.11.138'
