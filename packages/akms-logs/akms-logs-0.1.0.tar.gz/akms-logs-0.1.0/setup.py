ddfbee967d98929a35e46fd6d72609cf1dfbd25b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:27 +0000

    Add package akms-logs/0.1.0/akms-logs-0.1.0.tar.gz

---
+++ packages/akms-logs/akms-logs-0.1.0.tar.gz/akms-logs-0.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="akms-logs",
+    version="0.1.0",
+    author="apinanyogaratnam",
+    author_email="apinanapinan@icloud.com",
+    description="A wrapper logger package for akms logging",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/apinanyogaratnam/akms-logs-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10.4",
+    install_requires=[],
+)
