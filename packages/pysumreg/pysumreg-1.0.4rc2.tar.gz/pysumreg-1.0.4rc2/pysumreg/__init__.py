12a84a289469edf07428d5f3dc6f72ccfaee74e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:48 +0000

    Add package pysumreg/1.0.4rc2/pysumreg-1.0.4rc2.tar.gz

---
+++ packages/pysumreg/pysumreg-1.0.4rc2.tar.gz/pysumreg-1.0.4rc2/pysumreg/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: iso-8859-1 -*-
+
+"""Configuration of pysumreg package."""
+
+from .sum_registers import SummationRegisters
+
+try:
+    from setuptools_scm import get_version
+    import pathlib
+
+    path = str(pathlib.Path(__file__).parent)
+    __version__ = get_version(root="..", relative_to=__file__)
+    del get_version
+except (LookupError, ModuleNotFoundError, Exception):
+    from importlib.metadata import version
+
+    __version__ = version("pysumreg")
+
+# -----------------------------------------------------------------------------
+# :author:    Pete R. Jemian
+# :email:     prjemian@gmail.com
+# :copyright: (c) 2014-2022, Pete R. Jemian
+#
+# Distributed under the terms of the Creative Commons Attribution 4.0 International Public License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+# -----------------------------------------------------------------------------
