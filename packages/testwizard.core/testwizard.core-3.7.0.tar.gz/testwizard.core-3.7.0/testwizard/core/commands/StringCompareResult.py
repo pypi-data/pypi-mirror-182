14ba439e73300d782dfbfb52d5f5903d63c08d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:10 +0000

    Add package testwizard.core/3.7.0/testwizard.core-3.7.0.tar.gz

---
+++ packages/testwizard.core/testwizard.core-3.7.0.tar.gz/testwizard.core-3.7.0/testwizard/core/commands/StringCompareResult.py
@@ -0,0 +1,23 @@
+import json
+import sys
+
+from testwizard.commands_core.ResultBase import ResultBase
+
+class StringCompareResult(ResultBase):
+    def __init__(self, result, successMessage, failMessage):
+        ResultBase.__init__(self, True, successMessage, failMessage)
+
+        if "distance" in result:
+            self.distance = result["distance"]
+            
+        if self.success is True:
+            return
+
+        if "message" in result:
+            self.message = result["message"]
+
+        if "errorMessage" in result:
+            self.message = result["errorMessage"]
+
+        if "errorCode" in result:
+            self.message = self.getMessageForErrorCode(self.message, result["errorCode"])
