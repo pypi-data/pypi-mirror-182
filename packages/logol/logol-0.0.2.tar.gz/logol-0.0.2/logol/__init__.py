e9c076feaf1303ff6e490790e6a65f0f9ab69bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:53 +0000

    Add package logol/0.0.2/logol-0.0.2.tar.gz

---
+++ packages/logol/logol-0.0.2.tar.gz/logol-0.0.2/logol/__init__.py
@@ -0,0 +1,1 @@
+from logol.logol import *
---
+++ packages/logol/logol-0.0.2.tar.gz/logol-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='logol',
+    packages=['logol'],
+    version='0.0.2',
+    author=['Joseíto Oliveira'],
+    python_requires='>3.8',
+    url='https://github.com/JoseitoOliveira/logol',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+    ]
+)
