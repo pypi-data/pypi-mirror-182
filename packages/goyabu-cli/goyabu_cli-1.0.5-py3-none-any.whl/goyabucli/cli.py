0fd4c58979b7d3b4e452203ba720895080d79e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:52 +0000

    Add package goyabu-cli/1.0.5/goyabu_cli-1.0.5-py3-none-any.whl

---
+++ packages/goyabu-cli/goyabu_cli-1.0.5-py3-none-any.whl/goyabucli/cli.py
@@ -0,0 +1,191 @@
+from .scraperManager import ScraperManager
+from .sessionManager import SessionManager
+from .playerManager import PlayerManager
+from .dropdown import interactiveTable
+from .translation import t, error
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import Dict, Union, List
+import termtables as tt
+from tqdm import tqdm
+
+def mainTUI(default_anime_name:str, default_player:str, episodes_range:Dict[str,Union[None,int]], default_root:str, always_yes:bool, default_scraper:List[str]):
+    manager = ScraperManager()
+    sessionmanager = SessionManager(scrapers=manager.scrapers, root=default_root)
+
+    session_item = sessionmanager.select(query=default_anime_name, maxListSize=10)
+
+    if isinstance(session_item,str):
+        animes = manager.search(session_item, default_scraper)
+
+        if not animes:
+            error(t("Nenhum anime encontrado com o nome '{}'", session_item))
+            exit()
+
+        anime_names = [['',anime.title, ','.join(anime.pageUrl.keys())] for anime in animes]
+
+        if not always_yes:
+            results = interactiveTable(
+                anime_names,
+                ['',t('Animes'), t('Fonte')],
+                'llc',
+                maxListSize=7,
+                highlightRange=(2,2),
+                width=20,
+                flexColumn=1
+            )
+
+            if results['selectedPos'] is None:
+                print(results)
+                exit()
+
+            anime = animes[results['selectedPos']]
+        else:
+            anime = animes[0]
+    else:
+        anime = session_item.anime
+
+
+    tt.print(
+        [[anime.title]],
+        header=[t("Anime Selecionado")],
+        style=tt.styles.rounded,
+        alignment="c",
+    )
+
+    def is_range_valid(episodes_range:Dict[str,Union[None,int]]):
+        return (
+            episodes_range['start'] is None or
+            episodes_range['end'] is None or
+            (episodes_range['end'] != episodes_range['start'])
+        )
+
+
+    session_anime = sessionmanager.find(anime)
+
+    if session_anime and not is_range_valid(episodes_range):
+        episodes_range['start'] = session_anime.lastEpisode-1
+        episodes_range['end'] = None
+
+    if len(anime.availableScrapers) > 1:
+        if not always_yes:
+            results = interactiveTable(
+                items=[['',scraperName] for scraperName in anime.availableScrapers],
+                header=['',t('Escolha uma fonte')],
+                alignment='ll',
+                behaviour='single',
+                maxListSize=10,
+                highlightRange=(0,1),
+                width=20,
+                flexColumn=1
+            )
+
+            if results['selectedItem'] is None:
+                raise Exception(t('Não foi possível selecionar uma fonte'))
+
+            anime.source = results['selectedItem'][1]
+        else:
+            anime.source = anime.availableScrapers[0]
+
+
+    episodes = anime.retrieveEpisodes()
+    episodes_names = [['', episode.title] for episode in episodes]
+
+    if not episodes:
+        error(t("Não foi possível acessar os episódios de '{}' usando '{}'", anime.title, anime.source))
+        exit()
+
+    if (not always_yes and not is_range_valid(episodes_range)) or not is_range_valid(episodes_range):
+        results = interactiveTable(
+            items=episodes_names,
+            header=['',t('Episodios')],
+            alignment='ll',
+            behaviour='multiSelectWithText',
+            maxListSize=13,
+            highlightRange=(1,1),
+            width=20,
+            flexColumn=1,
+            hintText=t('deseja assistir todos os episódios? [S/n]: ')
+        )
+
+
+        if results['text'] not in ['', 'S', 's']:
+            results = interactiveTable(
+                items=episodes_names,
+                header=['',t('Episodios')],
+                alignment='ll',
+                behaviour='multiSelect',
+                maxListSize=13,
+                highlightRange=(1,1),
+                width=20,
+                flexColumn=1
+            )
+
+            if results['items'] is None:
+                print(t("É possível selecionar os episódios usando 'c' ou a barra de espaço"))
+                exit()
+
+        if results['items']:
+            episodes_range['start'] = min(results['items'].keys())
+            episodes_range['end'] = max(results['items'].keys())
+
+            episodes = [episodes[i] for i in results['items']]
+    elif is_range_valid(episodes_range):
+        episodes = episodes[slice(episodes_range['start'], episodes_range['end'])]
+
+
+    with tqdm(total=len(episodes), postfix=t("Links carregados"), ascii=True, leave=False, bar_format='|{bar}| {n_fmt}/{total_fmt}{postfix}') as pbar:
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            futures = [executor.submit(episode.retrieveLinks,anime.source) for episode in episodes]
+            for _ in as_completed(futures):
+                pbar.update(1)
+
+    for episode in episodes:
+        if not episode.getLinksBySource(anime.source):
+            error(t('Não foi encontrado nenhum link válido para o episódio {}', episode.index+1))
+
+
+    player = PlayerManager(anime.title, anime.source, episodes, root=default_root)
+
+    playlist_file = player.generatePlaylistFile()
+
+    if not playlist_file:
+        error(t('Não foi possível gerar o arquivo m3u'))
+        exit()
+
+    print(t('Abrindo "{}"...', playlist_file))
+
+    if player.isMpvAvailable() and default_player == 'mpv':
+        start_pos = 0
+        last_watch_pos = 0
+        seek_time = 0
+
+        # restore previous session
+        ep_ids = [ep.index for ep in episodes]
+        if session_anime and session_anime.lastEpisode-1 in ep_ids:
+            last_watch_pos = session_anime.lastEpisode
+            seek_time = session_anime.watchTime
+            start_pos = ep_ids.index(last_watch_pos-1)
+
+            # jump to next episode if user stopped at the end of the episode of the previous session
+            time_until_ep_ends = session_anime.duration-seek_time
+            if time_until_ep_ends > 0 and time_until_ep_ends < 90:
+                start_pos += 1
+                seek_time = 0
+
+
+        results = player.playWithMPV(playlist_file, seek_time=seek_time, playlistPos=start_pos)
+    else:
+        results = player.play(playlist_file, default_player)
+
+
+    sessionmanager.add([anime])
+
+    sessionmanager.update(anime, results['lastEpisode'], results['watchTime'], results['duration'])
+
+    print(t('Atualizando o histórico...'))
+    sessionmanager.dump(verbose=True, number_to_update=10)
+
+
+
+
+
---
+++ packages/goyabu-cli/goyabu_cli-1.0.5-py3-none-any.whl/goyabucli/sessionManager.py
@@ -0,0 +1,215 @@
+from .scraper import Anime,Scraper
+from typing import List,Union
+from datetime import datetime, timezone
+from os import path, makedirs
+import json
+from .utils import getTotalEpisodesCount
+from .dropdown import interactiveTable,bcolors
+from .translation import t
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+class SessionItem():
+    def __init__(self, anime:Anime, date_utc:int, episodesInTotal:int, availableEpisodes:int, lastEpisode:int, lastSource:str, watchTime=0, duration=0):
+        self.anime = anime
+
+        self.date_utc = datetime.fromtimestamp(date_utc,timezone.utc)
+        self.episodesInTotal = episodesInTotal
+        self.availableEpisodes = availableEpisodes
+        self.watchTime = watchTime
+        self.lastEpisode = lastEpisode
+        self.lastSource = lastSource
+        self.duration = duration
+
+    @property
+    def id(self):
+        return self.anime.id
+
+    @property
+    def title(self):
+        return self.anime.title
+
+    @property
+    def status(self):
+        if self.lastEpisode == self.availableEpisodes and self.lastEpisode < self.episodesInTotal:
+            return 'insync'
+        elif self.lastEpisode < self.episodesInTotal:
+            return 'ongoing'
+        else:
+            return 'complete'
+
+
+class SessionManager():
+    def __init__(self, root='', scrapers:List[Scraper]=[]):
+        self.filename = '.lastsession.json'
+        self.scrapers = scrapers
+
+        if root != '':
+            self.root = root
+        else:
+            self.root = path.dirname(__file__)
+
+        if not path.isdir(root):
+            makedirs(root, exist_ok=True)
+
+        if not path.isfile(path.join(root,self.filename)):
+            with open(path.join(root,self.filename), 'w') as file:
+                json.dump({}, file)
+
+        self.session_items = []
+        self.session_items = self.load()
+
+    def find(self, anime:Anime) -> Union[SessionItem,None]:
+        found_anime = next((item for item in self.session_items if item.anime.id == anime.id), None)
+
+        return found_anime
+
+    def add(self, animes:List[Anime]):
+        all_ids = [item.id for item in self.session_items]
+        for anime in animes:
+            if anime.id in all_ids: 
+                right_sessionItem = self.session_items[all_ids.index(anime.id)]
+                right_sessionItem.lastSource = anime.source
+                right_sessionItem.availableEpisodes = len(anime.episodes)
+                right_sessionItem.anime = anime
+
+                # Move session item to the end of the list
+                self.session_items.remove(right_sessionItem)
+                self.session_items.append(right_sessionItem)
+            else:
+                self.session_items.append(
+                    SessionItem(
+                        anime,
+                        int(datetime.now().timestamp()),
+                        0,
+                        len(anime.episodes),
+                        0,
+                        anime.source
+                    )
+                )
+
+    def update(self, anime:Anime, lastEpisode:int, watchTime=0, duration=0):
+        right_sessionItem = next(item for item in self.session_items if item.id == anime.id)
+
+        if not right_sessionItem:
+            raise IndexError(f"Cannot find '{anime.title}' in session items")
+
+        right_sessionItem.lastEpisode = lastEpisode
+        right_sessionItem.watchTime = watchTime
+        right_sessionItem.duration = duration
+
+    def remove(self, id:str):
+        right_sessionItem = next(item for item in self.session_items if item.id == id)
+        self.session_items.remove(right_sessionItem)
+
+    def load(self) -> List[SessionItem]:
+        with open(path.join(self.root,self.filename)) as file:
+            self.content = content = json.load(file)
+
+        session_items = []
+
+        for id,json_anime in content.items():
+            anime = Anime(json_anime['title'], id, source=json_anime['lastSource'])
+            anime.pageUrl = json_anime['pageUrl']
+            anime.scrapers = self.scrapers
+
+            session_items.append(SessionItem(
+                anime,
+                json_anime['utc'],
+                json_anime['episodesInTotal'],
+                json_anime['availableEpisodes'],
+                json_anime['lastEpisode'],
+                json_anime['lastSource'],
+                watchTime=json_anime['watchTime'],
+                duration=json_anime['duration'],
+            ))
+        
+        return session_items
+
+    def dump(self, verbose=False, number_to_update=0):
+        content = {}
+
+        def updateSessionItem(i, session_item):
+            availableEpisodes = session_item.availableEpisodes
+            if number_to_update and i+1>len(self.session_items)-number_to_update:
+                availableEpisodes = len(session_item.anime.retrieveEpisodes())
+
+            content[session_item.id] = {
+                'title': session_item.title,
+                'pageUrl': session_item.anime.pageUrl,
+                'utc': int(session_item.date_utc.timestamp()),
+                'episodesInTotal': getTotalEpisodesCount(session_item.title) or availableEpisodes,
+                'availableEpisodes': availableEpisodes,
+                'lastEpisode': session_item.lastEpisode,
+                'lastSource': session_item.lastSource,
+                'watchTime': session_item.watchTime,
+                'duration': session_item.duration
+            }
+
+        pbar = None
+        if verbose:
+            from tqdm import tqdm
+            pbar = tqdm(total=len(self.session_items), postfix=t('Animes atualizados'), ascii=True, leave=False, bar_format='|{bar}| {n_fmt}/{total_fmt}{postfix}')
+
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            futures = [executor.submit(updateSessionItem,i, session_item) for i,session_item in enumerate(self.session_items)]
+            for _ in as_completed(futures):
+                if pbar:
+                    pbar.update(1)
+
+        if pbar:
+            pbar.close()
+
+        with open(path.join(self.root,self.filename), 'w') as file:
+            json.dump(content, file)
+
+    def select(self, hintText=t('Digite: '), maxListSize=5, width=100, query='') -> Union[SessionItem,str]:
+
+        if query:
+            if query.isdigit():
+                return self.session_items[len(self.session_items)-int(query)]
+            return query
+
+        def format_status(session_item:SessionItem) -> str:
+            status = t("Episodio {} [{}/{}]", session_item.lastEpisode, session_item.availableEpisodes, session_item.episodesInTotal)
+
+            if session_item.status == 'complete':
+                status = bcolors['green']+t("Completo")+bcolors['end']
+            elif session_item.status == 'insync':
+                status = bcolors['grey']+status+bcolors['end']
+
+            return status
+
+        if not self.session_items:
+            return str(input(hintText))
+
+        table_rows = [[str(len(self.session_items)-index),item.title, format_status(item)] for index,item in enumerate(self.session_items)]
+
+
+        results = interactiveTable(
+            table_rows,
+            ['' ,t("Sessoes anteriores"), t("Status")],
+            "ccc",
+            behaviour='multiSelectWithText',
+            maxListSize=maxListSize,
+            width=width,
+            flexColumn=1,
+            highlightRange=(2,2),
+            hintText=hintText
+        )
+
+        if results['text']:
+            if results['text'].isdigit():
+                return self.session_items[len(self.session_items)-int(results['text'])]
+            return results['text']
+
+        if results['selectedPos'] is None:
+            raise Exception('Invalid position')
+
+        return self.session_items[results['selectedPos']]
+
+
+
+
+
+
