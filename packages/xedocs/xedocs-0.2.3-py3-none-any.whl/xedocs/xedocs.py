9718fa0970a3a3b2736afe5da6398563a04c88ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:48 +0000

    Add package xedocs/0.2.3/xedocs-0.2.3-py3-none-any.whl

---
+++ packages/xedocs/xedocs-0.2.3-py3-none-any.whl/xedocs/xedocs.py
@@ -0,0 +1,100 @@
+"""Main module."""
+
+import pandas as pd
+
+from collections import defaultdict
+from typing import ClassVar, Dict, List, Type, Union
+
+
+from ._settings import settings
+from .schemas import XeDoc
+
+
+def find_docs(schema, datasource=None, **labels):
+    if isinstance(schema, str):
+        schema = find_schema(schema)
+    if not issubclass(schema, XeDoc):
+        raise TypeError(
+            "Schema must be a subclass of XeDoc" "or the name of a known schema."
+        )
+    """Find a document in a datasource."""
+    # labels, extra = schema.extract_labels(**kwargs)
+    return schema.find(datasource, **labels)
+
+
+def find_df(schema, datasource=None, **labels):
+    if isinstance(schema, str):
+        schema = find_schema(schema)
+    if not issubclass(schema, XeDoc):
+        raise TypeError(
+            "Schema must be a subclass of XeDoc or the name of a known schema."
+        )
+    """Find a document in a datasource."""
+    return schema.find_df(datasource, **labels)
+
+
+def find_one(schema, datasource=None, **kwargs):
+    if isinstance(schema, str):
+        schema = find_schema(schema)
+    if not issubclass(schema, XeDoc):
+        raise TypeError(
+            "Schema must be a subclass of XeDoc or the name of a known schema."
+        )
+    """Find a document in a datasource."""
+    labels, extra = schema.extract_labels(**kwargs)
+    return schema.find_one(datasource, **labels)
+
+
+def insert_docs(schema: str, docs: Union[list, dict, pd.DataFrame], datasource=None):
+    if isinstance(docs, pd.DataFrame):
+        docs = docs.reset_index().to_dict(orient="records")
+    if not isinstance(docs, list):
+        docs = [docs]
+    if isinstance(schema, str):
+        schema = find_schema(schema)
+
+    inserted = []
+    for data in docs:
+        doc = schema(**data)
+        doc.save(datasource)
+        inserted.append(doc)
+
+    return inserted
+
+
+def list_schemas() -> List[str]:
+    return list(XeDoc._XEDOCS)
+
+
+def all_schemas() -> Dict[str, Type[XeDoc]]:
+    return dict(XeDoc._XEDOCS)
+
+
+def schemas_by_category() -> Dict[str,Dict[str, Type[XeDoc]]]:
+    d = defaultdict(dict)
+    for name, schema in all_schemas().items():
+        d[schema._CATEGORY][name] = schema
+    return d
+
+
+def find_schema(name) -> Type[XeDoc]:
+    schema = XeDoc._XEDOCS.get(name, None)
+    if schema is not None:
+        return schema
+    for schema in XeDoc._XEDOCS.values():
+        if schema.__name__ == name:
+            return schema
+    raise KeyError(f"Correction with name {name} not found.")
+
+
+def help(schema):
+    if isinstance(schema, str):
+        schema = find_schema(schema)
+    return schema.help()
+
+
+def get_api_client(schema):
+    if isinstance(schema, str):
+        schema = find_schema(schema)
+    return settings.api_client(schema)
+
