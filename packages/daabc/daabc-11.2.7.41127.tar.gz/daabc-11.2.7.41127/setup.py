a25d26e8f9348a51331628f32b59abf0bcadb290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:52 +0000

    Add package daabc/11.2.7.41127/daabc-11.2.7.41127.tar.gz

---
+++ packages/daabc/daabc-11.2.7.41127.tar.gz/daabc-11.2.7.41127/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'daabc', 
+    version = '11.2.7.41127', 
+    description = 'DA2B C/CXX', 
+    author = 'Little Deer Travel Copyright 2000-2022', 
+    author_email = 'jzheng130@gmail.com',
+    url = 'https://da2bc.org', 
+    packages = ['pkgs'], 
+    data_files = [('lib', ['/usr/local/lib/libdaabc.so.11.2.7', '/usr/local/lib/libdaabcxx.so.11.2.7']), ('include', ['/usr/local/include/daab_c_api.h', '/usr/local/include/daab_cxx_api.h'])]
+)
