ff85f54cbe12815aa01cc38cb2d65b570ceee7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:35 +0000

    Add package nester-jungle/1.3.0/nester-jungle-1.3.0.tar.gz

---
+++ packages/nester-jungle/nester-jungle-1.3.0.tar.gz/nester-jungle-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'nester-jungle', 
+        version      = '1.3.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['nester_jungle'],
+        author       = 'jungle',
+        author_email = 'zhj_swjtu@126.com',
+        description  = 'A simple printer of nested lists',
+     )
