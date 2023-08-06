05a78c0e9811c2f0c457b9fb813e5c4bdc30d71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:59 +0000

    Add package SWAG-DNN/0.1.27/SWAG_DNN-0.1.27.tar.gz

---
+++ packages/SWAG-DNN/SWAG_DNN-0.1.27.tar.gz/SWAG_DNN-0.1.27/setup.py
@@ -0,0 +1,39 @@
+
+
+
+import setuptools
+
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SWAG_DNN", # Replace with your own username
+    version="0.1.27",
+    author="Saeid Safaei",
+    author_email="saeid.safaei79@gmail.com",
+    description="Implimentation of this paper The SWAG Algorithm; a Mathematical Approach that Outperforms Traditional Deep Learning. Theory and Implementation https://arxiv.org/pdf/1811.11813.pdf",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+     packages=['SWAG_DNN'],
+    install_requires=[            # I get to this in a second
+          'keras==2.6.0',
+          'tensorflow==2.6.4',
+
+      ],    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
+
+
+
+
+
+
+
