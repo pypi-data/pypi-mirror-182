f11808b4b56cbd8dc660ed341db057f7f82ec57e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:44 +0000

    Add package PyBitTorrent/0.5.4/PyBitTorrent-0.5.4-py3-none-any.whl

---
+++ packages/PyBitTorrent/PyBitTorrent-0.5.4-py3-none-any.whl/PyBitTorrent/__init__.py
@@ -0,0 +1,5 @@
+# Expose the more specific use relevant classes
+from PyBitTorrent.Bittorrent import TorrentClient
+from PyBitTorrent.TorrentFile import TorrentFile
+from PyBitTorrent.HTTPTracker import HTTPTracker
+from PyBitTorrent.UDPTracker import UDPTracker
