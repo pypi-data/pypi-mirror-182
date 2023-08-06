8e38619a07aeedfe50f184f80689945cf65cc986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:02 +0000

    Add package frequency/0.0.1rc0/frequency-0.0.1rc0.tar.gz

---
+++ packages/frequency/frequency-0.0.1rc0.tar.gz/frequency-0.0.1rc0/README.md
@@ -0,0 +1,4 @@
+# Frequency
+
+Frequency is a Python package for frequency analysis applied to particle accelerator data.
+It acts as a wrapper, providing a consistent unified interface to a number of different frequency analysis packages.
---
+++ packages/frequency/frequency-0.0.1rc0.tar.gz/frequency-0.0.1rc0/frequency/_version.py
@@ -0,0 +1,19 @@
+__version__ = "0.0.1.rc0"
+
+
+def version_info() -> str:
+    """Debug convenience function to give version, platform and runtime information."""
+    import platform
+    import sys
+    from pathlib import Path
+
+    info = {
+        "Package version": __version__,
+        "Install path": Path(__file__).resolve().parent,
+        "Python version": f"{sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}",
+        "Python implementation": sys.version,
+        "Platform": platform.platform(),
+    }
+    return "\n".join(
+        "{:>24} {}".format(k + ":", str(v).replace("\n", " ")) for k, v in info.items()
+    )
