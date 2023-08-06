2b087f056b03cd5940981719c218d23fb8f54c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:07 +0000

    Add package minuteinbox/0.1.0/minuteinbox-0.1.0.tar.gz

---
+++ packages/minuteinbox/minuteinbox-0.1.0.tar.gz/minuteinbox-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['minuteinbox']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytz>=2022.7,<2023.0', 'requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'minuteinbox',
+    'version': '0.1.0',
+    'description': 'Unofficial python wrapper for minuteinbox.com',
+    'long_description': ' # minuteinbox\n\n[![Version](https://img.shields.io/pypi/v/minuteinbox?logo=pypi)](https://pypi.org/project/minuteinbox)\n[![Quality Gate Status](https://img.shields.io/sonar/alert_status/fedecalendino_minuteinbox?logo=sonarcloud&server=https://sonarcloud.io)](https://sonarcloud.io/dashboard?id=fedecalendino_minuteinbox)\n[![CodeCoverage](https://img.shields.io/sonar/coverage/fedecalendino_minuteinbox?logo=sonarcloud&server=https://sonarcloud.io)](https://sonarcloud.io/dashboard?id=fedecalendino_minuteinbox)\n\nUnofficial python wrapper for minuteinbox.com\n',
+    'author': 'Fede Calendino',
+    'author_email': 'fede@calendino.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/fedecalendino/minuteinbox',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
