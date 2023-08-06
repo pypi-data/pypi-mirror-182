f494140f4e8697a55dc0a30a3042c552939f594c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:08 +0000

    Add package cegalprizm-pythontoolpro/2.2.2/cegalprizm_pythontoolpro-2.2.2-py3-none-any.whl

---
+++ packages/cegalprizm-pythontoolpro/cegalprizm_pythontoolpro-2.2.2-py3-none-any.whl/cegalprizm/pythontool/__init__.py
@@ -0,0 +1,47 @@
+# Copyright 2022 Cegal AS
+# All rights reserved.
+# Unauthorized copying of this file, via any medium is strictly prohibited.
+
+
+
+__version__ = '2.2.2'
+__git_hash__ = '660c4223'
+
+
+import sys
+
+from cegalprizm.pythontool.ooponly.ip_oop_transition import Point
+
+from cegalprizm.pythontool import _utils
+
+from cegalprizm.pythontool.petrelobject import PetrelObject
+
+from cegalprizm.pythontool.petrellink import PetrelLink
+from cegalprizm.pythontool.grid import Grid
+from cegalprizm.pythontool.gridproperty import GridProperty, GridDiscreteProperty
+from cegalprizm.pythontool.seismic import SeismicCube, SeismicLine
+from cegalprizm.pythontool.surface import Surface, SurfaceAttribute, SurfaceDiscreteAttribute, Surfaces, SurfaceAttributes
+from cegalprizm.pythontool.welllog import WellLog, DiscreteWellLog, LogSamples, LogSample, GlobalWellLog, DiscreteGlobalWellLog, Logs
+from cegalprizm.pythontool.borehole import Well
+from cegalprizm.pythontool.points import PointSet
+from cegalprizm.pythontool.polylines import PolylineSet, Polyline
+from cegalprizm.pythontool.horizoninterpretation import HorizonInterpretation, HorizonInterpretation3d, HorizonProperty3d
+from cegalprizm.pythontool.wellsurvey import WellSurvey
+from cegalprizm.pythontool.wavelet import Wavelet
+from cegalprizm.pythontool.workflow import Workflow
+from cegalprizm.pythontool.observeddata import ObservedData, ObservedDataSet
+from cegalprizm.pythontool.primitives import Extent, Indices, Annotation, CoordinatesExtent, AxisExtent
+from cegalprizm.pythontool.chunk import Chunk, Slice # Slice for backwards compatability
+from cegalprizm.pythontool.chunktype import ChunkType
+from cegalprizm.pythontool.exceptions import PythonToolException
+from cegalprizm.pythontool.observeddata import ObservedDataSet, ObservedDataSets
+from cegalprizm.pythontool.petrellink import DiscreteGlobalWellLogs, GlobalWellLogs
+from cegalprizm.pythontool.welllog import DiscreteWellLog, GlobalWellLog, WellLog
+from cegalprizm.pythontool.petrelconnection import PetrelConnection, make_connection
+
+import cegalprizm.pythontool.transactions
+
+
+
+
+    
