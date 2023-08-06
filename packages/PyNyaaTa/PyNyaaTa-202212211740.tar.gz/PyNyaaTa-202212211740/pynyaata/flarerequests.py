5530527fcf117b8162c0661e56c3104880e568a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:22 +0000

    Add package PyNyaaTa/202212211740/PyNyaaTa-202212211740.tar.gz

---
+++ packages/PyNyaaTa/PyNyaaTa-202212211740.tar.gz/PyNyaaTa-202212211740/pynyaata/flarerequests.py
@@ -0,0 +1,61 @@
+from urllib import parse
+from requests import RequestException, Response, Session, post
+from .config import CLOUDPROXY_ENDPOINT, REQUESTS_TIMEOUT
+
+
+class FlareRequests(Session):
+    def request(self, method, url, params=None, timeout=REQUESTS_TIMEOUT, **kwargs):
+        if not CLOUDPROXY_ENDPOINT:
+            return super().request(method, url, params, timeout=timeout, **kwargs)
+
+        sessions = post(CLOUDPROXY_ENDPOINT, json={"cmd": "sessions.list"}).json()
+
+        if "sessions" in sessions and len(sessions["sessions"]) > 0:
+            FLARESESSION = sessions["sessions"][0]
+        else:
+            response = post(CLOUDPROXY_ENDPOINT, json={"cmd": "sessions.create"})
+            session = response.json()
+
+            if "session" in session:
+                FLARESESSION = session["session"]
+            else:
+                raise RequestException(response)
+
+        post_data = {
+            "cmd": f"request.{method.lower()}",
+            "session": FLARESESSION,
+            "url": url,
+            "maxTimeout": timeout * 1000,
+        }
+
+        if params:
+            post_data["postData"] = parse.urlencode(params)
+
+        try:
+            response = post(
+                CLOUDPROXY_ENDPOINT,
+                json=post_data,
+            )
+
+            solution = response.json()
+
+            if "solution" in solution:
+                resolved = Response()
+
+                resolved.raw = solution["solution"]["response"]
+                resolved.status_code = solution["solution"]["status"]
+                resolved.headers = solution["solution"]["headers"]
+                resolved.url = url
+                resolved.reason = solution["status"]
+                resolved.cookies = solution["solution"]["cookies"]
+
+                return resolved
+
+            raise RequestException(response)
+        except RequestException:
+            session = post(
+                CLOUDPROXY_ENDPOINT,
+                json={"cmd": "sessions.destroy", "session": FLARESESSION},
+            )
+
+            raise RequestException(solution)
