333c902de260aaef139fbe0e80a09db700f5c784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:45 +0000

    Add package socketshelpone/0.0.1/socketshelpone-0.0.1.tar.gz

---
+++ packages/socketshelpone/socketshelpone-0.0.1.tar.gz/socketshelpone-0.0.1/socketshelpone/equal.py
@@ -0,0 +1,14 @@
+print("1")
+"""
+import numpy as np
+x= np.random.randint(0,2,10)
+print("array 1:")
+print(x)
+y=np.random.randint(0,2,10)
+print("array 2:")
+print(y)
+array_equal=np.allclose(x,y)
+print("test if two arrays are equal or not")
+print(array_equal)
+"""
+
---
+++ packages/socketshelpone/socketshelpone-0.0.1.tar.gz/socketshelpone-0.0.1/socketshelpone/max.py
@@ -0,0 +1,10 @@
+print("1")
+"""
+import numpy as np
+x=np.random.random((5,5))
+print("original values:")
+print(x)
+xmin,xmax=x.min(),x.max()
+print("minimum and maximun vales :")
+print(xmin,xmax)
+"""
