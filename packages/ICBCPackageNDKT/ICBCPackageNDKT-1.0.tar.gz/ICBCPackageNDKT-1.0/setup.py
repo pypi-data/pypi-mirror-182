376aad750208b7bf2c4bd3ae6b26af819a15fa5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:28 +0000

    Add package ICBCPackageNDKT/1.0/ICBCPackageNDKT-1.0.tar.gz

---
+++ packages/ICBCPackageNDKT/ICBCPackageNDKT-1.0.tar.gz/ICBCPackageNDKT-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ICBCPackageNDKT',
+    version='1.0',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A test ICBC python package',
+	url='https://github.com/ubco-mds-2022-labs/project-step-2-kcmtong.git',
+    author='Nowshaba Durrani & Kenny Tong',
+    author_email='nowshaba@gmail.com'
+)
