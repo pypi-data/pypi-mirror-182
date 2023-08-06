9e610eb16e523915cd56d9b9df4eb9d5c998961b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:12 +0000

    Add package ArseinRubika/4.6.1/ArseinRubika-4.6.1-py3-none-any.whl

---
+++ packages/ArseinRubika/ArseinRubika-4.6.1-py3-none-any.whl/libraryArsein/copyright.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2022 ArianBOT
+
+version = "4.6.1"
+copyright = " Arsein Copyright (C) 2022 arianabasi Team ArianBOT \n\n"
+Arian = "ArianBOT"
+
+
+class copyright_Arsein:
+    arsein = print(
+        f"\n\nArsein library version {version}\n{copyright}\n\nدر حال فعال شدن کمی صبور باشید...\n")
+    print(" ")
+    print(". . . . . . . . . . . ")
---
+++ packages/ArseinRubika/ArseinRubika-4.6.1-py3-none-any.whl/libraryArsein/methodjson.py
@@ -0,0 +1,11 @@
+from libraryArsein.encoder import encoderjson
+import json
+from json import dumps
+
+class meghdar:
+	def __init__(self,auth):
+		self.Auth = auth
+		self.enc = encoderjson(auth)
+
+	def return_json(self,Json):
+		return dumps({"api_version":"5","auth": self.Auth,"data_enc":self.enc.encrypt(dumps(Json))})
