08751c7fb81cfd5d4a9c261e866618cc798cec23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:50 +0000

    Add package osin/1.9.2/osin-1.9.2.tar.gz

---
+++ packages/osin/osin-1.9.2.tar.gz/osin-1.9.2/osin/types/__init__.py
@@ -0,0 +1,8 @@
+from osin.types.pyobject_type import (
+    PyObjectType,
+    PRIMITIVE_TYPES,
+    Number,
+)
+from osin.types.pyobject import PyObject, OTable, OImage, OAudio
+from osin.types.primitive_type import NestedPrimitiveOutput, NestedPrimitiveOutputSchema
+from osin.types.param_schema import ParamSchema
