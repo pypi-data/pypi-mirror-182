967aae9bd36b3149532ca2f30c815cca1172c6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:12 +0000

    Add package django-formset/0.13.1/django-formset-0.13.1.tar.gz

---
+++ packages/django-formset/django-formset-0.13.1.tar.gz/django-formset-0.13.1/formset/static/formset/js/SortableSelect-2UIVE5GA.js
@@ -0,0 +1,2 @@
+import{b as a,c as b}from"./chunk-JYPD4DR6.js";import"./chunk-SNXKFZ2Y.js";import"./chunk-XXGBPDQQ.js";import"./chunk-ON5OQYWL.js";export{b as SortableOptGroupElement,a as SortableSelectElement};
+//# sourceMappingURL=SortableSelect-2UIVE5GA.js.map
