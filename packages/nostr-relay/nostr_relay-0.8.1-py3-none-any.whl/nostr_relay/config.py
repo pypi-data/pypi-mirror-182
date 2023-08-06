e56172277f43c22812685159aea0589796f992fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:08 +0000

    Add package nostr-relay/0.8.1/nostr_relay-0.8.1-py3-none-any.whl

---
+++ packages/nostr-relay/nostr_relay-0.8.1-py3-none-any.whl/nostr_relay/config.py
@@ -0,0 +1,57 @@
+import yaml
+
+class ConfigClass:
+    DEBUG = True
+    max_event_size = 4096
+
+    def __init__(self):
+        self.gunicorn = {}
+        self.logging = {
+                'version': 1,
+                'formatters': {
+                    'simple': {
+                        'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+                    }
+                },
+                'handlers': {
+                    'console': {
+                        'class': 'logging.StreamHandler', 
+                        'level': 'DEBUG', 
+                        'formatter': 'simple', 
+                        'stream': 'ext://sys.stdout'
+                    }
+                }, 
+                'loggers': {
+                    'nostr_relay': {
+                        'level': 'INFO', 
+                        'handlers': ['console'], 
+                        'propagate': False
+                    }
+                }, 
+                'root': {
+                    'level': 'INFO', 
+                    'handlers': ['console']
+                }
+        } 
+
+    def __str__(self):
+        s = 'Config(\n'
+        for k, v in self.__dict__.items():
+            if k.startswith('_'):
+                continue
+            s += f'\t{k}={v} \n'
+        s += ')'
+        return s
+
+    def load(self, filename):
+        with open(filename, 'r') as fp:
+            conf = yaml.load(fp, yaml.FullLoader)
+
+        for k, v in conf.items():
+            setattr(self, k, v)
+
+    def __getattr__(self, attrname):
+        return None
+
+
+Config = ConfigClass()
