dcea88440e722f956e5471a71ccf553acec9952f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:38 +0000

    Add package gs-bn-probdist/1.1/gs_bn_probdist-1.1.tar.gz

---
+++ packages/gs-bn-probdist/gs_bn_probdist-1.1.tar.gz/gs_bn_probdist-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gs_bn_probdist',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['gs_bn_probdist'],
+      author='Himanshu Singh Sikarwar',
+      author_email='himanshusingh3639@gmail.com',
+      zip_safe=False)
