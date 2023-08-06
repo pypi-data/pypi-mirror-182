1f8cda828558f678a2890543dfd56d875adfe243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:01:11 +0000

    Add package exonutils/7.0/exonutils-7.0.tar.gz

---
+++ packages/exonutils/exonutils-7.0.tar.gz/exonutils-7.0/setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+setup(
+    name="exonutils",
+    install_requires=[],
+)
