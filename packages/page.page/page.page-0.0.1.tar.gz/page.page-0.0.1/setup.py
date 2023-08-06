ec038136bbe746908036468501dc9b1a680b1a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:57 +0000

    Add package page.page/0.0.1/page.page-0.0.1.tar.gz

---
+++ packages/page.page/page.page-0.0.1.tar.gz/page.page-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='page.page',
+  version='0.0.1',
+  description='desc.',
+  url='',  
+  author='a',
+  license='', 
+  classifiers=classifiers,
+  packages=find_packages(),
+)
