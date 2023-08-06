5392eb8008006a518a52a71391df9eab836855f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:18 +0000

    Add package czitools/0.1.3/czitools-0.1.3-py3-none-any.whl

---
+++ packages/czitools/czitools-0.1.3-py3-none-any.whl/czitools/_tests/test_md_general.py
@@ -0,0 +1,173 @@
+
+from czitools import pylibczirw_metadata as czimd
+from pylibCZIrw import czi as pyczi
+import os
+from pathlib import Path
+import numpy as np
+
+basedir = Path(__file__).resolve().parents[3]
+
+
+def test_pixeltypes():
+
+    # get the CZI filepath
+    filepath = os.path.join(basedir, r"data/Tumor_HE_RGB.czi")
+    md = czimd.CziMetadata(filepath)
+
+    print("PixelTypes: ", md.pixeltypes)
+    print("NumPy dtype: ", md.npdtype)
+    print("Max. Pixel Value: ", md.maxvalue)
+
+    assert (md.pixeltypes == {0: 'Bgr24'})
+    assert (md.isRGB == True)
+
+    # check the function to get npdtypes and maxvalues
+
+    pts = ["gray16",
+           "Gray16",
+           "gray8",
+           "Gray8",
+           "bgr48",
+           "Bgr48",
+           "bgr24",
+           "Bgr24",
+           "bgr96float",
+           "Bgr96Float",
+           "abc",
+           None]
+
+    dts = [np.dtype(np.uint16),
+           np.dtype(np.uint16),
+           np.dtype(np.uint8),
+           np.dtype(np.uint8),
+           np.dtype(np.uint16),
+           np.dtype(np.uint16),
+           np.dtype(np.uint8),
+           np.dtype(np.uint8),
+           np.dtype(np.uint16),
+           np.dtype(np.uint16),
+           None,
+           None]
+
+    mvs = [65535, 65535, 255, 255, 65535, 65535, 255, 255, 65535, 65535, None, None]
+
+    for pt, dt, mv in zip(pts, dts, mvs):
+
+        out = czimd.CziMetadata.get_dtype_fromstring(pt)
+        assert (out[0] == dt)
+        assert (out[1] == mv)
+
+
+def test_dimorder():
+
+    # get the CZI filepath
+    filepath = os.path.join(basedir, r"data/S=2_3x3_CH=2.czi")
+    md = czimd.CziMetadata(filepath)
+
+    assert(md.aics_dim_order == {'R': -1, 'I': -1, 'M': 5, 'H': 0, 'V': -1,
+           'B': -1, 'S': 1, 'T': 2, 'C': 3, 'Z': 4, 'Y': 6, 'X': 7, 'A': -1})
+    assert(md.aics_dim_index == [-1, -1, 5, 0, -1, -1, 1, 2, 3, 4, 6, 7, -1])
+    assert(md.aics_dim_valid == 8)
+
+
+def test_scene_shape():
+
+    files = [r"data/S=3_1Pos_2Mosaic_T=2=Z=3_CH=2_sm.czi",
+             r"data/CellDivision_T=3_Z=5_CH=2_X=240_Y=170.czi",
+             r"data/WP96_4Pos_B4-10_DAPI.czi"]
+
+    shapes = [False, True, True]
+
+    for file, sc in zip(files, shapes):
+
+        # get the CZI filepath
+        filepath = os.path.join(basedir, file)
+
+        # get the complete metadata at once as one big class
+        md = czimd.CziMetadata(filepath)
+
+        assert(md.scene_shape_is_consistent == sc)
+
+
+def test_reading_czi_fresh():
+
+    filepath = os.path.join(basedir, r"data/A01_segSD.czi")
+
+    # get the complete metadata at once as one big class
+    mdata = czimd.CziMetadata(filepath)
+
+    assert (mdata.sample.well_array_names == [])
+    assert (mdata.sample.well_indices == [])
+    assert (mdata.sample.well_position_names == [])
+    assert (mdata.sample.well_colID == [])
+    assert (mdata.sample.well_rowID == [])
+    assert (mdata.sample.well_counter == [])
+    assert (mdata.sample.scene_stageX == [])
+    assert (mdata.sample.scene_stageY == [])
+    assert (mdata.sample.image_stageX is None)
+    assert (mdata.sample.image_stageY is None)
+
+
+def test_get_image_dimensions():
+
+    test_dict = {"ImageDocument":
+                 {"Metadata":
+                  {"Information":
+                   {"Image":
+                    {"SizeX": 0,
+                     "SizeY": 300,
+                     "SizeS": 500,
+                     "SizeT": 3,
+                     "SizeZ": 20,
+                     "SizeC": 2,
+                     "SizeM": None,
+                     "SizeR": 0,
+                     "SizeH": -1,
+                     "SizeI": None,
+                     "SizeV": None,
+                     "SizeB": 1}}}}}
+
+    dimensions = ["SizeX",
+                  "SizeY",
+                  "SizeS",
+                  "SizeT",
+                  "SizeZ",
+                  "SizeC",
+                  "SizeM",
+                  "SizeR",
+                  "SizeH",
+                  "SizeI",
+                  "SizeV",
+                  "SizeB"]
+
+    results = [None, 300, 500, 3, 20, 2, None, None, None, None, None, 1]
+
+    dim_dict = czimd.CziDimensions.get_image_dimensions(test_dict)
+
+    for d, v in zip(dimensions, results):
+        print((d, v))
+        assert (dim_dict[d] == v)
+
+
+def test_scaling():
+
+    # get the CZI filepath
+    filepath = os.path.join(basedir, r"data/DAPI_GFP.czi")
+    md = czimd.CziMetadata(filepath)
+
+    assert(md.scale.X == 1.0)
+    assert(md.scale.Y == 1.0)
+    assert(md.scale.Z == 1.0)
+    assert(md.scale.ratio == {'xy': 1.0, 'zx': 1.0})
+
+    scaling = czimd.CziScaling(filepath, dim2none=True)
+    assert(scaling.X is None)
+    assert(scaling.Y is None)
+    assert(scaling.Z is None)
+    assert(scaling.ratio == {'xy': None, 'zx': None})
+
+    scaling = czimd.CziScaling(filepath, dim2none=False)
+    assert(scaling.X == 1.0)
+    assert(scaling.Y == 1.0)
+    assert(scaling.Z == 1.0)
+    assert(scaling.ratio == {'xy': 1.0, 'zx': 1.0})
---
+++ packages/czitools/czitools-0.1.3-py3-none-any.whl/czitools/_tests/test_writing.py
@@ -0,0 +1,180 @@
+from czitools import pylibczirw_tools, misc
+from czitools import pylibczirw_metadata as czimd
+from pylibCZIrw import czi as pyczi
+import os
+from pathlib import Path
+from tifffile import imread, TiffFile
+import numpy as np
+from tqdm.contrib import itertools as it
+from tqdm import tqdm
+
+basedir = Path(__file__).resolve().parents[3]
+
+# get some data to write
+filepath = os.path.join(basedir, r"data/z=16_ch=3.czi")
+mdarray, mdata, dimstring = pylibczirw_tools.read_6darray(filepath,
+                                                          output_dask=False,
+                                                          chunks_auto=False,
+                                                          output_order="STZCYX",
+                                                          remove_adim=True)
+numCH = mdata.image.SizeC
+numZ = mdata.image.SizeZ
+
+
+def test_write_1():
+
+    # get the CZI filepath
+    filepath1 = os.path.join(basedir, r"data/CH=1_16bit.tif")
+    filepath2 = os.path.join(basedir, r"data/Fluorescence_RGB.tif")
+
+    files = [filepath1, filepath2]
+    sps = [1, 3]
+
+    for file, sp in zip(files, sps):
+
+        czi_path = os.path.join(os.path.dirname(file), (misc.get_fname_woext(file) + ".czi"))
+
+        # remove the CZI if it already exits
+        if os.path.exists(czi_path):
+            os.remove(czi_path)
+
+        # read the TIF image
+        tif_image = imread(file)
+
+        tiff_tags = {}
+
+        with TiffFile(file) as tif:
+            for page in tif.pages:
+                for tag in page.tags:
+                    if not isinstance(tag.value, (bytes, bytearray)):
+                        tiff_tags[tag.name] = tag.value
+
+        # reverse RGB since ZEN is using BGR
+        if tiff_tags["SamplesPerPixel"] == 3:
+            print("Reversing RGB to BGR")
+            tif_image = tif_image[..., ::-1]
+
+        if tiff_tags["SamplesPerPixel"] == 1:
+            print("Adding new axis")
+            tif_image = tif_image[..., np.newaxis]
+
+        assert (tiff_tags["SamplesPerPixel"] == sp)
+
+        # open a new CZI and allow overwrite (!!!) to play around ...
+        with pyczi.create_czi(czi_path, exist_ok=True) as czidoc_w:
+
+            # write the plane
+            czidoc_w.write(data=tif_image)
+
+        # check if CZI was written
+        assert (os.path.exists(czi_path) is True)
+
+        # remove the files
+        os.remove(czi_path)
+
+
+def test_write_2():
+
+    # create the filename for the new CZI image file
+    newczi_4dstack = os.path.join(os.getcwd(), "z=16_ch=3.czi")
+
+    # open a new CZI and allow overwrite (!!!) to play around ...
+    with pyczi.create_czi(newczi_4dstack, exist_ok=True) as czidoc_w:
+
+        # loop over all z-planes and channels
+        for z, ch in it.product(range(numZ), range(numCH)):
+            # get the 2d array for the current plane and add axis to get (Y, X, 1) as shape
+            # write the plane with shape (Y, X, 1) to the new CZI file
+            czidoc_w.write(
+                data=mdarray[0, 0, z, ch, ...],
+                plane={"Z": z, "C": ch}
+            )
+
+    # get the complete metadata at once as one big class
+    mdata = czimd.CziMetadata(newczi_4dstack)
+
+    assert (mdata.pyczi_dims == {'T': (0, 1), 'Z': (0, 16),
+            'C': (0, 3), 'X': (0, 512), 'Y': (0, 512)})
+    assert (mdata.pixeltypes == {0: 'Gray16', 1: 'Gray16', 2: 'Gray16'})
+    assert (mdata.isRGB is False)
+    assert (mdata.ismosaic is False)
+
+    os.remove(newczi_4dstack)
+
+
+def test_write_3():
+
+    # create the filename for the new CZI image file
+    newczi_zloc = os.path.join(os.getcwd(), "newCZI_zloc.czi")
+    xstart = 0
+    ch = 0
+
+    with pyczi.create_czi(newczi_zloc, exist_ok=True) as czidoc_w:
+        # loop over all z-planes
+        for z in tqdm(range(numZ)):
+
+            # for fun - write the z-planes to different locations
+            czidoc_w.write(
+                data=mdarray[0, 0, z, ch, ...],
+                plane={"C": ch},
+                location=(xstart, 0)
+            )
+
+            # change the x-position for the next round to write "side-by-side"
+            xstart = xstart + 512
+
+    # get the complete metadata at once as one big class
+    mdata = czimd.CziMetadata(newczi_zloc)
+
+    assert (mdata.pyczi_dims == {'T': (0, 1), 'Z': (0, 1),
+            'C': (0, 1), 'X': (0, 8192), 'Y': (0, 512)})
+    assert (mdata.pixeltypes == {0: 'Gray16'})
+    assert (mdata.isRGB is False)
+    assert (mdata.ismosaic is True)
+    assert (mdata.bbox.total_bounding_box == {'T': (0, 1), 'Z': (
+        0, 1), 'C': (0, 1), 'X': (0, 8192), 'Y': (0, 512)})
+
+    os.remove(newczi_zloc)
+
+
+def test_write_4():
+
+    # first step is to create some kind of grid of locations
+    locx = []
+    locy = []
+    xystart = 0
+    offset = 700
+
+    # create the list for the 4x4 grid locations for the 16 planes
+    for x, y in it.product(range(4), range(4)):
+        locx.append(xystart + offset * x)
+        locy.append(xystart + offset * y)
+
+    # create the filename for the new CZI image file
+    newczi_zscenes = os.path.join(os.getcwd(), "newCZI_scenes.czi")
+    ch = 0
+
+    with pyczi.create_czi(newczi_zscenes, exist_ok=True) as czidoc_w:
+        # loop over all z-planes
+        for z in tqdm(range(numZ)):
+
+            # for "fun" - write the z-planes to different locations using the locations we just created
+            czidoc_w.write(
+                data=mdarray[0, 0, z, ch, ...],
+                plane={"C": ch},
+                scene=z,
+                location=(locx[z], locy[z])
+            )
+
+        # get the complete metadata at once as one big class
+    mdata = czimd.CziMetadata(newczi_zscenes)
+
+    assert (mdata.pyczi_dims == {'T': (0, 1), 'Z': (0, 1),
+            'C': (0, 1), 'X': (0, 2612), 'Y': (0, 2612)})
+    assert (mdata.pixeltypes == {0: 'Gray16'})
+    assert (mdata.isRGB is False)
+    assert (mdata.ismosaic is False)
+    assert (mdata.bbox.total_bounding_box == {'T': (0, 1), 'Z': (
+        0, 1), 'C': (0, 1), 'X': (0, 2612), 'Y': (0, 2612)})
+
+    os.remove(newczi_zscenes)
