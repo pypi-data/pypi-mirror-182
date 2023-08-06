937deadc9b070273986890cfc3d089067e00d8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:27 +0000

    Add package primul-pachet-Iustin-Muresanu/0.1.0/primul-pachet-Iustin-Muresanu-0.1.0.tar.gz

---
+++ packages/primul-pachet-Iustin-Muresanu/primul-pachet-Iustin-Muresanu-0.1.0.tar.gz/primul-pachet-Iustin-Muresanu-0.1.0/README.md
@@ -0,0 +1,8 @@
+
+# Primul meu pachet
+
+Acesta este primul pachet pe care l-am facut in pypi
+
+
+
+
---
+++ packages/primul-pachet-Iustin-Muresanu/primul-pachet-Iustin-Muresanu-0.1.0.tar.gz/primul-pachet-Iustin-Muresanu-0.1.0/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+setup (
+    name="primul-pachet-Iustin-Muresanu",
+    version="0.1.0",
+    author="Iustin Muresanu",
+    email="imuresanu@yahoo.com",
+    packages=["my_own_package"],
+    package_dir={"":"src\\"},
+    include_package_data=True,
+    description="primul meu pachet")
+
+
+
+
+
