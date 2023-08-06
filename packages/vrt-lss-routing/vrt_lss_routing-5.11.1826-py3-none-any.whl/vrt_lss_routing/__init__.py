b0d81c8aebfece09c6055581c334bada066e240d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:30:28 +0000

    Add package vrt-lss-routing/5.11.1826/vrt_lss_routing-5.11.1826-py3-none-any.whl

---
+++ packages/vrt-lss-routing/vrt_lss_routing-5.11.1826-py3-none-any.whl/vrt_lss_routing/__init__.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+
+"""
+    VRt.Routing [RO]
+
+    The version of the OpenAPI document: 5.11.1826
+    Contact: servicedesk@veeroute.com
+    Auto Generated
+"""
+
+
+__version__ = "5.11.1826"
+
+# import ApiClient
+from vrt_lss_routing.api_client import ApiClient
+
+# import Configuration
+from vrt_lss_routing.configuration import Configuration
+
+# import exceptions
+from vrt_lss_routing.exceptions import OpenApiException
+from vrt_lss_routing.exceptions import ApiAttributeError
+from vrt_lss_routing.exceptions import ApiTypeError
+from vrt_lss_routing.exceptions import ApiValueError
+from vrt_lss_routing.exceptions import ApiKeyError
+from vrt_lss_routing.exceptions import ApiException
