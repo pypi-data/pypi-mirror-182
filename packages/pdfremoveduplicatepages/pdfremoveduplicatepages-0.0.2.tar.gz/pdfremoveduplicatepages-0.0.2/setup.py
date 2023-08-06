fd054071f7cbe12edaea9a9d3b1f923698f58538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:10 +0000

    Add package pdfremoveduplicatepages/0.0.2/pdfremoveduplicatepages-0.0.2.tar.gz

---
+++ packages/pdfremoveduplicatepages/pdfremoveduplicatepages-0.0.2.tar.gz/pdfremoveduplicatepages-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pdfremoveduplicatepages",  # Replace with your own username
+    version="0.0.2",
+    author="Carsten Engelke",
+    author_email="carsten.engelke@gmail.com",
+    description="A simple tool to remove duplicate or near-duplicate PDF pages by comparing extracted text.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/carsten-engelke/pdf-remove-duplicate-pages",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'pdfremoveduplicatepages = pdfremoveduplicatepages:main',
+        ],
+    },
+    python_requires=">=3.6",
+    install_requires=["PyMuPDF>=1.21.1"]
+)
