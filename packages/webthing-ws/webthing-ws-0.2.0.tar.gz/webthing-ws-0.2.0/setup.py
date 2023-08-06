22b78e6119fb970eba7a7bce2181f72d350281a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:56:07 +0000

    Add package webthing-ws/0.2.0/webthing-ws-0.2.0.tar.gz

---
+++ packages/webthing-ws/webthing-ws-0.2.0.tar.gz/webthing-ws-0.2.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Setup file for WebThing WS."""
+import os
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as readme:
+    long_description = readme.read()
+
+setup(
+    name="webthing-ws",
+    version="0.2.0",
+    description="A WebThing WebSocket consumer and API client.",
+    long_description=long_description,
+    url="https://github.com/home-assistant-ecosystem/webthing-ws",
+    download_url="https://github.com/home-assistant-ecosystem/webthing-ws/releases",
+    author="Fabian Affolter",
+    author_email="fabian@affolter-engineering.ch",
+    license="MIT",
+    install_requires=[
+        "aiohttp>=3.8.0,<4",
+        "async_timeout",
+    ],
+    packages=["webthing_ws"],
+    python_requires=">=3.9",
+    zip_safe=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Utilities",
+    ],
+)
