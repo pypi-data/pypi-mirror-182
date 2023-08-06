130b629241a9e76659f5012eab30810c98ce3e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:27 +0000

    Add package my-movie-database/0.1.0/my-movie-database-0.1.0.tar.gz

---
+++ packages/my-movie-database/my-movie-database-0.1.0.tar.gz/my-movie-database-0.1.0/README.md
@@ -0,0 +1,2 @@
+# my-movie-database
+A command line tool to keep track of the movies you watched. Installable via pip.
---
+++ packages/my-movie-database/my-movie-database-0.1.0.tar.gz/my-movie-database-0.1.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+requirements = [
+    "SQLAlchemy==1.4.45",
+    "rich==12.6.0"
+]
+
+long_description = "A command line tool to keep track of the movies you watched. Installable via pip. " \
+                   "Uses SqlAlchemy. Built for Linux"
+
+setuptools.setup(
+    name="my-movie-database",
+    version="0.1.0",
+    author="Kumuthu Edirisinghe",
+    author_email="ekumuthu@gmail.com",
+    description="A command line tool to keep track of the movies you watched",
+    long_description=long_description,
+    url="https://github.com/kumuthu53/my-movie-database",
+    project_urls={
+        "Bug Tracker": "https://github.com/kumuthu53/my-movie-database/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.10",
+    install_requires=requirements,
+    scripts=["scripts/my-movie-database"]
+)
