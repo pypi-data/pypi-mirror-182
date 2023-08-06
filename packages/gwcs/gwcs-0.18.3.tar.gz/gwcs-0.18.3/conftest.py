6a06c1e57ab1f11ac0ace0f364b15a6d417e1c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:37 +0000

    Add package gwcs/0.18.3/gwcs-0.18.3.tar.gz

---
+++ packages/gwcs/gwcs-0.18.3.tar.gz/gwcs-0.18.3/conftest.py
@@ -0,0 +1,10 @@
+import os
+import pkg_resources
+
+
+entry_points = []
+for entry_point in pkg_resources.iter_entry_points('pytest11'):
+    entry_points.append(entry_point.name)
+
+if "asdf_schema_tester" not in entry_points:
+    pytest_plugins = ['asdf.tests.schema_tester']
