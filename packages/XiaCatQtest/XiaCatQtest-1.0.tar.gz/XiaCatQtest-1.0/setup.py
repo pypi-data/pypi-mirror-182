9470753a81cdf40f5480a5463c163e5789dfd257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:13 +0000

    Add package XiaCatQtest/1.0/XiaCatQtest-1.0.tar.gz

---
+++ packages/XiaCatQtest/XiaCatQtest-1.0.tar.gz/XiaCatQtest-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='XiaCatQtest',
+    version='1.0',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A test python package',
+	url='https://github.com/khalad-hasan/myMileConverter',
+    author='XiaCatQ',
+    author_email='XIACATQ@gmail.com'
+)
