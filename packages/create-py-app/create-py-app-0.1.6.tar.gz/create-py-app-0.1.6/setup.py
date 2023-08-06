ecfae51159c38105d8df94d0b364040d22e7c316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:01 +0000

    Add package create-py-app/0.1.6/create-py-app-0.1.6.tar.gz

---
+++ packages/create-py-app/create-py-app-0.1.6.tar.gz/create-py-app-0.1.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="create-py-app",
+    version="0.1.6",
+    author="Hideaki Yoshida",
+    author_email="",
+    description="A tool for creating a simple python framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hyoshida123/create-py-app",
+    packages=setuptools.find_packages(),
+    install_requires=["pyyaml"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": ["create-py-app=create_py_app.main:main"],
+    },
+)
