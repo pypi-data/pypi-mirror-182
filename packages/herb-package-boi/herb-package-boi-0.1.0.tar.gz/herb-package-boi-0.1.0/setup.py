08f1acb76d24b684ebe3b3ac528a1b5f348fd60d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:23 +0000

    Add package herb-package-boi/0.1.0/herb-package-boi-0.1.0.tar.gz

---
+++ packages/herb-package-boi/herb-package-boi-0.1.0.tar.gz/herb-package-boi-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name="herb-package-boi",
+    version="0.1.0",
+    author="Fetz Herbert",
+    author_email="fetz.martin.herbert.ovb@gmail.com",
+    packages=["my_own_package"],
+    package_dir={"":"src\\"},
+    include_package_data=True,
+    description="my_first_package"
+)
