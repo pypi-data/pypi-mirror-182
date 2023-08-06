a6293645857dfdbf9966ce765f421ec5de871d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:09 +0000

    Add package icon-registration/1.1.1/icon_registration-1.1.1-py3-none-any.whl

---
+++ packages/icon-registration/icon_registration-1.1.1-py3-none-any.whl/icon_registration/__init__.py
@@ -0,0 +1,18 @@
+from icon_registration.losses import (
+    LNCC,
+    LNCCOnlyInterpolated,
+    BlurredSSD,
+    GradientICON,
+    InverseConsistentNet,
+    gaussian_blur,
+    ssd_only_interpolated,
+    ssd,
+)
+from icon_registration.network_wrappers import (
+    DownsampleRegistration,
+    FunctionFromMatrix,
+    FunctionFromVectorField,
+    RegistrationModule,
+    TwoStepRegistration,
+)
+from icon_registration.train import train_batchfunction, train_datasets
