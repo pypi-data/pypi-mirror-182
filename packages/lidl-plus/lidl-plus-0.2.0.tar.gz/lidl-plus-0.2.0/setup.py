9d8213e8c1d972d84246e4ff76ecddd0a70c9e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:51 +0000

    Add package lidl-plus/0.2.0/lidl-plus-0.2.0.tar.gz

---
+++ packages/lidl-plus/lidl-plus-0.2.0.tar.gz/lidl-plus-0.2.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+Setup Lidl Plus api
+"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="lidl-plus",
+    version="0.2.0",
+    author="Andre Basche",
+    description="Fetch receipts and more from Lidl Plus",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Andre0512/lidl-plus",
+    license="MIT",
+    platforms="any",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=["requests"],
+    extras_require={"auth": ["selenium-wire", "webdriver-manager", "getuseragent", "oic"]},
+    entry_points={
+        "console_scripts": [
+            "lidl-plus = lidlplus.__main__:start",
+        ]
+    },
+)
