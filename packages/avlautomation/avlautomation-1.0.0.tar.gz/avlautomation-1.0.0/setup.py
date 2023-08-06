b983b1cc6ee6a0a8de9b8b09521ee92dfa328477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:09 +0000

    Add package avlautomation/1.0.0/avlautomation-1.0.0.tar.gz

---
+++ packages/avlautomation/avlautomation-1.0.0.tar.gz/avlautomation-1.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+VERSION = '1.0.0'
+DESCRIPTION = "Automation tools for vortex lattice method, AVL"
+LONG_DESCRIPTION = (Path(__file__).parent / "README.md").read_text()
+
+# setup
+setup(
+    name="avlautomation",
+    version=VERSION,
+    author="jj-foster",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Team-Peryton/AVL-automation",
+    packages=find_packages("avl_automation"),
+    install_requires=["numpy","scipy","pandas","matplotlib","tqdm"],
+    python_requires='>=3.9'
+)
