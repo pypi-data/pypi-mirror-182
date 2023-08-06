3cffb7686f3688cc5a7e1b74c4c73b90f40fb7fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:04 +0000

    Add package iman/0.0.65/iman-0.0.65.tar.gz

---
+++ packages/iman/iman-0.0.65.tar.gz/iman-0.0.65/iman/Image.py
@@ -0,0 +1,34 @@
+from iman import *
+
+
+def fun(fname , b):
+    ffmpeg_command =  '%s -y -i "%s" -vf "scale=w=%s:h=%s" -compression_level %s "%s"' %(b[3] ,fname , b[0],b[1] ,b[5], PJ(b[4],PN(fname)+b[2]) )
+    cmd(ffmpeg_command)
+   
+   
+ 
+def convert (fname_pattern ,ext ='jpg',ofolder=None , w=-1 , h=-1,level=100,  worker=4,ffmpeg_path='c:\\ffmpeg.exe'): 
+    
+    if (not ext.startswith('.')):
+       ext = '.' + ext    
+       
+    files = gf(fname_pattern) 
+    
+   
+    if (ofolder==None):
+       
+        ofolder=""
+    else:
+       PM(ofolder)
+     
+
+
+    worker=min(len(files) , worker)     
+
+    from multiprocessing import Pool
+    from functools import partial  
+    with Pool(processes=worker) as pool:
+         pool.map(partial(fun, b=[w , h,ext , ffmpeg_path,ofolder,level]), files)     
+
+
+ 
