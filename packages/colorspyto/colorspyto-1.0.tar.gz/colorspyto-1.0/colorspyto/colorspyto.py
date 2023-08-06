99af3c0a113c7d6108e6faf7c42aa8bbfb7c89aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:11 +0000

    Add package colorspyto/1.0/colorspyto-1.0.tar.gz

---
+++ packages/colorspyto/colorspyto-1.0.tar.gz/colorspyto-1.0/colorspyto/colorspyto.py
@@ -0,0 +1,5 @@
+import colorspyto
+
+
+def start():
+    return "Salut c'est le test qui te parle"
---
+++ packages/colorspyto/colorspyto-1.0.tar.gz/colorspyto-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='colorspyto',
+      version='1.0',
+      description='test',
+      packages=['colorspyto'],
+      author_email='nicolastecasselecrane@gmail.com',
+      zip_safe=False)
