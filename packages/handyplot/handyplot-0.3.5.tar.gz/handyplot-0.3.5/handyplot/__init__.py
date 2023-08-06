8b3b88441c438e0323c71ec98c913da5a2ac62ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:06 +0000

    Add package handyplot/0.3.5/handyplot-0.3.5.tar.gz

---
+++ packages/handyplot/handyplot-0.3.5.tar.gz/handyplot-0.3.5/handyplot/__init__.py
@@ -0,0 +1,2 @@
+from .color import color
+from .figure import Figure
---
+++ packages/handyplot/handyplot-0.3.5.tar.gz/handyplot-0.3.5/setup.py
@@ -0,0 +1,21 @@
+"""
+Type this in terminal to upgrade the package.
+Step-1
+    remove unuse directories
+Step-2
+    input command
+    >> python setup.py sdist bdist_wheel
+    >> twine upload dist/*
+"""
+from setuptools import find_packages, setup
+
+setup(name="handyplot",
+      version="0.3.5",
+      description="Test.",
+      author="Hong Peng",
+      python_requires=">=3.8.0",
+      url="https://github.com/minho-hong/handyplot.git",
+      package_data={},
+      packages=find_packages(),
+      install_requires=["matplotlib", "numpy", "vtk"],
+      license="GPL")
