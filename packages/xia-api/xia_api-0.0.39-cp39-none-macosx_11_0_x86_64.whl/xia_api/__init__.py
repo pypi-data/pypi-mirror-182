fbfa78493c44a9e726965c19ad84e456dc6b55ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:09 +0000

    Add package xia-api/0.0.39/xia_api-0.0.39-cp39-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-api/xia_api-0.0.39-cp39-none-macosx_11_0_x86_64.whl/xia_api/__init__.py
@@ -0,0 +1,16 @@
+from xia_api.rest import RestApi
+from xia_api.auth_client import AuthClient
+from xia_api.open_api import OpenApi
+from xia_api.message import XiaCollectionDeleteMsg, XiaDocumentDeleteMsg, XiaFileMsg
+from xia_api.message import XiaErrorMessage
+
+
+__all__ = [
+    "AuthClient",
+    "RestApi",
+    "OpenApi",
+    "XiaCollectionDeleteMsg", "XiaDocumentDeleteMsg", "XiaFileMsg",
+    "XiaErrorMessage",
+]
+
+__version__ = "0.0.39"
