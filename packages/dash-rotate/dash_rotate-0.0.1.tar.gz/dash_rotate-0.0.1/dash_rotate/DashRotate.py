3c052bc6fa39589cc49de88ac82eb1f693130c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:28 +0000

    Add package dash-rotate/0.0.1/dash_rotate-0.0.1.tar.gz

---
+++ packages/dash-rotate/dash_rotate-0.0.1.tar.gz/dash_rotate-0.0.1/dash_rotate/DashRotate.py
@@ -0,0 +1,41 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class DashRotate(Component):
+    """A DashRotate component.
+
+
+Keyword arguments:
+
+- id (string; optional)
+
+- delay (number; default 10)
+
+- message (a list of or a singular dash component, string or number; optional)
+
+- orientation (string; required)
+
+- reload (boolean; default True)"""
+    _children_props = ['message']
+    _base_nodes = ['message', 'children']
+    _namespace = 'dash_rotate'
+    _type = 'DashRotate'
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, orientation=Component.REQUIRED, message=Component.UNDEFINED, reload=Component.UNDEFINED, delay=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'delay', 'message', 'orientation', 'reload']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'delay', 'message', 'orientation', 'reload']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        for k in ['orientation']:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+
+        super(DashRotate, self).__init__(**args)
