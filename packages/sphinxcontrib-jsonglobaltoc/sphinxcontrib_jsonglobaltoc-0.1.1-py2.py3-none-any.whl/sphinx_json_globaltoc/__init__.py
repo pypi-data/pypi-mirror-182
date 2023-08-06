f5f5936c3edca429a73d187670ef019fbe81c9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:26 +0000

    Add package sphinxcontrib-jsonglobaltoc/0.1.1/sphinxcontrib_jsonglobaltoc-0.1.1-py2.py3-none-any.whl

---
+++ packages/sphinxcontrib-jsonglobaltoc/sphinxcontrib_jsonglobaltoc-0.1.1-py2.py3-none-any.whl/sphinx_json_globaltoc/__init__.py
@@ -0,0 +1,16 @@
+from typing import Dict, Any
+
+from sphinx.application import Sphinx
+
+from .builders import SphinxGlobalTOCJSONHTMLBuilder
+
+__version__ = '0.1.1'
+
+
+def setup(app: Sphinx) -> Dict[str, Any]:
+    app.add_builder(SphinxGlobalTOCJSONHTMLBuilder, override=True)
+
+    return {
+        'version': __version__,
+        'parallel_read_safe': True
+    }
