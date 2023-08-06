a603e6033a99f31ff46d108974e05e43b2bb17c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:56 +0000

    Add package isystem.connect/9.21.144.0/isystem.connect-9.21.144.0-cp38-none-manylinux1_x86_64.whl

---
+++ packages/isystem.connect/isystem.connect-9.21.144.0-cp38-none-manylinux1_x86_64.whl/isystem/icmetadata.py
@@ -0,0 +1,13 @@
+__version__ = '9.21.144.0'
+name = 'isystem.connect'
+package_data = {'isystem': ['isysCvrgToCobertura.xslt', 'isystemTestReport.xslt', 'blue.css', 'gray.css', '_connect.so']}
+long_description = """==============================================================================
+       isystem.connect for Python
+==============================================================================
+
+This package enables control of winIDEA and embedded targets from Python.
+Please find the detailed documentation with examples at
+http://www.isystem.com/downloads/winIDEA/SDK/iSYSTEM.Python.SDK/documentation/isystem-connect-api/index.html
+
+(c) iSYSTEM Labs d.o.o., 2020
+"""
