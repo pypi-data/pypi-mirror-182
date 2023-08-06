b870f7b5fdc1f5a295090d4bb18fa6e1ab5fec65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:05 +0000

    Add package ohmm/1.0.1/ohmm-1.0.1.tar.gz

---
+++ packages/ohmm/ohmm-1.0.1.tar.gz/ohmm-1.0.1/ohmm/ohmm_studio.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+import inspect
+from .GetData import gettheuserid,check_login_recursive
+from threading import Thread
+folder=inspect.getfile(gettheuserid).split("site-packages")[0]
+folder=folder+"site-packages\\ohmm\\"
+#print(folder)
+def start_studio():
+    try:
+        folderr=folder+"ohmm-driver.exe"
+        subprocess.run([folderr, "start"])
+        #os.system(folder+"DataKund.exe start")
+    except Exception as e:
+        print("Exception is",e)
+def main():
+    Thread(target = start_studio).start()
+    check_login_recursive()
