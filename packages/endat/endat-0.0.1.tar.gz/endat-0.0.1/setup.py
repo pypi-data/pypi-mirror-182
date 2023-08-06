4fe41dc68cf55062de5971b135bae75f9ce54a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:22 +0000

    Add package endat/0.0.1/endat-0.0.1.tar.gz

---
+++ packages/endat/endat-0.0.1.tar.gz/endat-0.0.1/setup.py
@@ -0,0 +1,24 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="endat",
+    version="0.0.1",
+    author="Aqouthe",
+    author_email="hoonie0929@gmail.com",
+    description="encryption module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Aqouthe",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
