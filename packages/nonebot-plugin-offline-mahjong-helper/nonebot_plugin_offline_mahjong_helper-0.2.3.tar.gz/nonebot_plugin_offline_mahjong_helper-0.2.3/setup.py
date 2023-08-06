15e151d65fccd7a8eecdf16d17086cf3981fab0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:27 +0000

    Add package nonebot-plugin-offline-mahjong-helper/0.2.3/nonebot_plugin_offline_mahjong_helper-0.2.3.tar.gz

---
+++ packages/nonebot-plugin-offline-mahjong-helper/nonebot_plugin_offline_mahjong_helper-0.2.3.tar.gz/nonebot_plugin_offline_mahjong_helper-0.2.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='nonebot_plugin_offline_mahjong_helper',
+    version='0.2.3',
+    keywords=["nonebot","mahjong"],
+    author='Nranphy',
+    author_email='3102002900@qq.com',
+    url='https://github.com/Nranphy/nonebot_plugin_offline_mahjong_helper',
+    description="基于nonebot2可约桌、算点、查询的面麻助手。",
+    long_description=u'基于nonebot2可约桌、算点、查询的面麻助手。',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "nonebot2",
+        "nonebot-adapter-onebot",
+        "nonebot_plugin_htmlrender",
+        "nonebot_plugin_apscheduler",
+        "httpx"
+        ],
+)
