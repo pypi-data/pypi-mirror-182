6c69aff6004a781fd541761857d676312e44721f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:41 +0000

    Add package odoo14-addon-l10n-ro-payment-to-statement/14.0.2.7.0/odoo14_addon_l10n_ro_payment_to_statement-14.0.2.7.0-py3-none-any.whl

---
+++ packages/odoo14-addon-l10n-ro-payment-to-statement/odoo14_addon_l10n_ro_payment_to_statement-14.0.2.7.0-py3-none-any.whl/odoo/addons/l10n_ro_payment_to_statement/models/account_move.py
@@ -0,0 +1,58 @@
+# Copyright (C) 2015-2020 Deltatech
+# Copyright (C) 2022 NextERP Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from odoo import models
+
+
+class AccountMove(models.Model):
+    _name = "account.move"
+    _inherit = ["account.move", "l10n.ro.mixin"]
+
+    def write(self, vals):
+        if "state" in vals and vals.get("state") == "posted":
+            for move in self:
+                if move.is_l10n_ro_record:
+                    if (
+                        not move.name or move.name == "/"
+                    ) and move.journal_id.l10n_ro_journal_sequence_id:
+                        new_number = (
+                            move.journal_id.l10n_ro_journal_sequence_id.next_by_id()
+                        )
+                        super(AccountMove, move).write({"name": new_number})
+
+        if "statement_line_id" in vals and vals.get("statement_line_id"):
+            statement_line = self.env["account.bank.statement.line"].browse(
+                vals.get("statement_line_id")
+            )
+            for move in self:
+                if (
+                    not move.name
+                    or move.name == "/"
+                    and statement_line.is_l10n_ro_payment_disposal
+                ):
+                    if (
+                        statement_line.amount >= 0.0
+                        and move.journal_id.l10n_ro_cash_in_sequence_id
+                    ):
+                        new_number = (
+                            move.journal_id.l10n_ro_cash_in_sequence_id.next_by_id()
+                        )
+                        super(AccountMove, move).write({"name": new_number})
+                    elif (
+                        statement_line.amount < 0.0
+                        and move.journal_id.l10n_ro_cash_out_sequence_id
+                    ):
+                        new_number = (
+                            move.journal_id.l10n_ro_cash_out_sequence_id.next_by_id()
+                        )
+                        super(AccountMove, move).write({"name": new_number})
+
+        return super(AccountMove, self).write(vals)
+
+    def _post(self, soft=True):
+        for move in self:
+            if move.payment_id and move.is_l10n_ro_record:
+                move.payment_id.l10n_ro_force_cash_sequence()
+        return super()._post(soft)
