0bb487cb979fa77fdc850999477827036f99bcd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:01:15 +0000

    Add package dnevniklib/1.0/dnevniklib-1.0.tar.gz

---
+++ packages/dnevniklib/dnevniklib-1.0.tar.gz/dnevniklib-1.0/dnevniklib/__init__.py
@@ -0,0 +1,12 @@
+"""
+DNEVNIK MOS LIB
+
+version 0.1 beta
+
+Work on Closed API DNEVNIK МЭШ (MOS)
+"""
+
+from dnevniklib.marks import *
+from dnevniklib.homeworks import *
+from dnevniklib.school import *
+from dnevniklib.user import *
