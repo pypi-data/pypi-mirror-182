88e95a1c60eef296dfbda8b3cd33803d85bf8f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:32 +0000

    Add package gpt-do/0.1.0/gpt_do-0.1.0.tar.gz

---
+++ packages/gpt-do/gpt_do-0.1.0.tar.gz/gpt_do-0.1.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gpt_do',
+ 'gpt_do.doers',
+ 'gpt_do.vendor.chatgpt_wrapper',
+ 'gpt_do.vendor.chatgpt_wrapper.chatgpt_wrapper']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.1.3,<9.0.0', 'openai>=0.25.0,<0.26.0', 'retry>=0.9.2,<0.10.0']
+
+entry_points = \
+{'console_scripts': ['do = do.cli:do']}
+
+setup_kwargs = {
+    'name': 'gpt-do',
+    'version': '0.1.0',
+    'description': 'GPT-powered bash commands.',
+    'long_description': '# `do`\n\nThis is a handy-dandy CLI for when you don\'t know wtf to do.\n\nInstead of furiously grepping through man pages, simply use `do`, and have GPT-3 do all the magic for you.\n\n## Usage\n\n```console\n$ export OPENAI_API_KEY=xxx # stick this in your bash_profile\n$ do amend the message of my last commit to "It works!"\ngit commit --amend -m \'It works!\'\nThis command will amend the message of the last commit to \'It works!\'.\nDo you want to continue? [y/N]: y\n[main 3e6a2f6] It works!!\n Date: Thu Dec 22 01:15:40 2022 -0800\n 5 files changed, 1088 insertions(+)\n create mode 100644 .gitignore\n create mode 100644 .gitmodules\n create mode 100644 README.md\n create mode 100644 poetry.lock\n create mode 100644 pyproject.toml\n```\xa0\xa0\n',
+    'author': 'Yasyf Mohamedali',
+    'author_email': 'yasyfm@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/yasyf/do',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
