930ef441a481600e0235d439885c24f79011e91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:41 +0000

    Add package radox/0.0.2/radox-0.0.2.tar.gz

---
+++ packages/radox/radox-0.0.2.tar.gz/radox-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'radox',
+  packages = find_packages(),
+  version = '0.0.2',
+  description = 'A WSGI based Python web framework inspired from flask',
+  author = 'Palani',
+  author_email = 'palanioffcl@gmail.com',
+  install_requires=['wsgi'],
+  keywords = ['web', 'python', 'framework', 'libraries', 'webapps'],
+  classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Internet :: WWW/HTTP"
+    ]
+)
