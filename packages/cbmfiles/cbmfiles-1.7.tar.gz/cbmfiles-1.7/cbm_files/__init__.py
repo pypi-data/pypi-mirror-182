24d6f3e43fc6fdcf42186a1da4536a57ace72b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:06 +0000

    Add package cbmfiles/1.7/cbmfiles-1.7.tar.gz

---
+++ packages/cbmfiles/cbmfiles-1.7.tar.gz/cbmfiles-1.7/cbm_files/__init__.py
@@ -0,0 +1,5 @@
+from .basic_file import BASICFile  # noqa: F401
+from .binary_file import BinaryFile  # noqa: F401
+from .data_file import DataFile  # noqa: F401
+from .pc64_file import PC64File  # noqa: F401
+from .program_file import ProgramFile  # noqa: F401
