be940b8542a0e9d4bbbded57a6f60d626088a010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:45 +0000

    Add package eskiz-sms/0.2.0/eskiz_sms-0.2.0.tar.gz

---
+++ packages/eskiz-sms/eskiz_sms-0.2.0.tar.gz/eskiz_sms-0.2.0/eskiz_sms/__init__.py
@@ -0,0 +1,12 @@
+from . import exceptions
+from . import types
+from .eskiz import EskizSMS
+
+__version__ = '0.2.0'
+
+__all__ = [
+    'EskizSMS',
+    "__version__",
+    "exceptions",
+    "types"
+]
