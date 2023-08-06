a0f4d41482ea6253a7c65462f8219cb86a0e07ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:20 +0000

    Add package potyk-doc/0.3.0/potyk_doc-0.3.0-py3-none-any.whl

---
+++ packages/potyk-doc/potyk_doc-0.3.0-py3-none-any.whl/potyk_doc/models.py
@@ -0,0 +1,39 @@
+import enum
+from typing import Tuple, NamedTuple
+
+
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+# https://en.wikipedia.org/wiki/Media_type
+class Mimetype(str, enum.Enum):
+    text = 'text/plain'
+    html = 'text/html'
+    csv = 'text/csv'
+    json = 'application/json'
+    pdf = 'application/pdf'
+    zip = 'application/zip'
+    any = 'application/octet-stream'
+    docx = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    xlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
+
+class DocumentType(str, enum.Enum):
+    pdf = 'pdf'
+    docx = 'docx'
+
+    @property
+    def mime_type(self):
+        """
+        >>> DocumentType.pdf.mime_type
+        'application/pdf'
+        """
+        return Mimetype.__members__[self.value].value
+
+
+FileData = bytes
+FileName = str
+
+
+class File(NamedTuple):
+    """Файл - тьюпл из байтов и названия файла"""
+    data: FileData
+    name: FileName
