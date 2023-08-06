fd620329de3fdd28a5414bcb8ce79dbdfd84cc5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:02 +0000

    Add package qtstrap/0.3.11/qtstrap-0.3.11-py2.py3-none-any.whl

---
+++ packages/qtstrap/qtstrap-0.3.11-py2.py3-none-any.whl/qtstrap/extras/style/__init__.py
@@ -0,0 +1,2 @@
+from .colors import colors, qcolors
+from .dark_palette import darkPalette, install_dark_palette
---
+++ packages/qtstrap/qtstrap-0.3.11-py2.py3-none-any.whl/qtstrap/qt.py
@@ -0,0 +1,5 @@
+import qtpy 
+from qtpy import QtCore, QtGui, QtWidgets
+from qtpy.QtCore import *
+from qtpy.QtGui import *
+from qtpy.QtWidgets import *
