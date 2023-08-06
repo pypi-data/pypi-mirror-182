0562c0f6bc92927cc4013513523e64f60cc2d32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:43 +0000

    Add package pysumreg/1.0.4rc1/pysumreg-1.0.4rc1.tar.gz

---
+++ packages/pysumreg/pysumreg-1.0.4rc1.tar.gz/pysumreg-1.0.4rc1/docs/source/conf.py
@@ -0,0 +1,58 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from importlib.metadata import version
+import configparser
+import pathlib
+import sys
+
+sys.path.insert(0, str(pathlib.Path().absolute().parent.parent))
+import pysumreg
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+root_path = pathlib.Path(__file__).parent.parent.parent
+parser = configparser.ConfigParser()
+parser.read(root_path / "setup.cfg")
+metadata = parser["metadata"]
+
+project = metadata["name"]
+copyright = metadata["copyright"]
+author = metadata["author"]
+description = metadata["description"]
+rst_prolog = f".. |author| replace:: {author}"
+
+# -- Special handling for version numbers ------------------------------------
+# https://github.com/pypa/setuptools_scm#usage-from-sphinx
+
+release = version(project)
+version = ".".join(release.split(".")[:2])
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = """
+    sphinx.ext.autodoc
+    sphinx.ext.autosummary
+    sphinx.ext.coverage
+    sphinx.ext.githubpages
+    sphinx.ext.inheritance_diagram
+    sphinx.ext.mathjax
+    sphinx.ext.todo
+    sphinx.ext.viewcode
+""".split()
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+today_fmt = "%Y-%m-%d %H:%M"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+# html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
