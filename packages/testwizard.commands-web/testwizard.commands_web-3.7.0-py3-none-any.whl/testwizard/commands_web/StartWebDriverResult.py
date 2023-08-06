6ed7a28a12c20838b770161106a3abd48e21e36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:12 +0000

    Add package testwizard.commands-web/3.7.0/testwizard.commands_web-3.7.0-py3-none-any.whl

---
+++ packages/testwizard.commands-web/testwizard.commands_web-3.7.0-py3-none-any.whl/testwizard/commands_web/StartWebDriverResult.py
@@ -0,0 +1,17 @@
+import json
+import sys
+
+from testwizard.commands_core.ResultBase import ResultBase
+
+
+class StartWebDriverResult(ResultBase):
+    def __init__(self, result, successMessage, failMessage):
+        ResultBase.__init__(self, result["ok"] is True, successMessage, failMessage)
+
+        self.executorUrl = result["executorUrl"]
+        self.sessionId = result["sessionId"]
+
+        if self.success is True:
+            return
+
+        self.message = self.getMessageForErrorCode(self.message, result["errorCode"])
