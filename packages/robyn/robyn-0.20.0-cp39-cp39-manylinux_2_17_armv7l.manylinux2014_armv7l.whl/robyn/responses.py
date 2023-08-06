fe147ec45345dba7de03a9d72c2f3354bdca7e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:51 +0000

    Add package robyn/0.20.0/robyn-0.20.0-cp39-cp39-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/robyn/robyn-0.20.0-cp39-cp39-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/robyn/responses.py
@@ -0,0 +1,28 @@
+import json
+from typing import Any, Dict
+
+
+def static_file(file_path: str) -> Dict[str, Any]:
+    """
+    This function will help in serving a static_file
+
+    :param file_path str: file path to serve as a response
+    """
+
+    return {
+        "type": "static_file",
+        "file_path": file_path,
+        # this is a hack for now
+        "body": "",
+        "status_code": 200,
+    }
+
+
+def jsonify(input_dict: dict) -> str:
+    """
+    This function serializes input dict to a json string
+
+    :param input_dict dict: response of the function
+    """
+
+    return json.dumps(input_dict)
