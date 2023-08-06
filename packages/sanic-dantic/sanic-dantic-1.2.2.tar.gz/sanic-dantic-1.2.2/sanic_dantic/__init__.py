9c0d6c04bbf640d201cb244663b541fa5b7dd990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:58 +0000

    Add package sanic-dantic/1.2.2/sanic-dantic-1.2.2.tar.gz

---
+++ packages/sanic-dantic/sanic-dantic-1.2.2.tar.gz/sanic-dantic-1.2.2/sanic_dantic/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import pydantic
+from pydantic import *
+
+from .basic_definition import ParsedArgsObj, DanticModelObj
+from .sanic_class_dantic import DanticView
+from .sanic_function_dantic import parse_params
+
+__author__ = "Connor Zhang"
+__copyright__ = "Copyright 2022, Connor Zhang"
+__license__ = "MIT"
+__version__ = "1.2.2"
+__all__ = pydantic.__all__ + [
+    "DanticView",
+    "parse_params",
+    "ParsedArgsObj",
+    "DanticModelObj",
+    "BaseModel",
+    "Field"
+]
---
+++ packages/sanic-dantic/sanic-dantic-1.2.2.tar.gz/sanic-dantic-1.2.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sanic-dantic",
+    version="1.2.2",
+    author="Connor Zhang",
+    author_email="chzhangyue@outlook.com",
+    description="""A request parameter checking and parsing library
+                based on pydantic under the sanic framework""",
+    install_requires=['pydantic>=1.10.2', 'sanic>=21.3.4'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/misss85246/sanic-dantic",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Utilities",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.8',
+)
