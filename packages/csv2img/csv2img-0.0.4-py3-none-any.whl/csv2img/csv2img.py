20ea4d06e9d64f012b2d117b5d210766cae7bae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:36 +0000

    Add package csv2img/0.0.4/csv2img-0.0.4-py3-none-any.whl

---
+++ packages/csv2img/csv2img-0.0.4-py3-none-any.whl/csv2img/csv2img.py
@@ -0,0 +1,17 @@
+from csv2pdf import convert
+import fitz
+def csv2img(a=None) -> None:
+    if a is not None:
+        name = str(a).replace(".csv",'')
+        pdf = str(name) + str(".pdf")
+        convert(a, pdf)
+        file_path = pdf
+        doc = fitz.open(file_path)
+        i = 0
+        for page in doc:
+            i += 1
+            pix = page.get_pixmap(dpi=300)  # render page to an image
+            output = str(name) + str(i) + ".png"
+            pix.save(output)
+        doc.close()
+        return "Done"
