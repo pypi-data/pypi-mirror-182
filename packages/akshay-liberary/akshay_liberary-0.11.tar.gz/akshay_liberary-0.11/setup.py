6c235ca9f8c0c7bbb8e6e8e70f16e2bb10dcfaf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:09 +0000

    Add package akshay-liberary/0.11/akshay_liberary-0.11.tar.gz

---
+++ packages/akshay-liberary/akshay_liberary-0.11.tar.gz/akshay_liberary-0.11/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='akshay_liberary',
+      version='0.11',
+      description='operation & greeting library',
+      packages=['akshay-library'],
+      author_email='adhomse99@gmail.com',
+      zip_safe=False)
