387e0efee53dce2c3627f9819f2725e08ac2db02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:50 +0000

    Add package serial-interface/2.3.1/serial_interface-2.3.1.tar.gz

---
+++ packages/serial-interface/serial_interface-2.3.1.tar.gz/serial_interface-2.3.1/serial_interface/__about__.py
@@ -0,0 +1,11 @@
+"""Extends pyserial to make serial device interfaces."""
+# This file is generated automatically from .metadata.org
+# File edits may be overwritten!
+
+__version__ = '2.3.1'
+__description__ = 'Extends pyserial to make serial device interfaces.'
+__license__ = 'BSD-3-Clause'
+__url__ = 'https://github.com/janelia-pypi/serial_interface_python'
+__author__ = 'Peter Polidoro'
+__email__ = 'peter@polidoro.io'
+__copyright__ = '2022 Howard Hughes Medical Institute'
