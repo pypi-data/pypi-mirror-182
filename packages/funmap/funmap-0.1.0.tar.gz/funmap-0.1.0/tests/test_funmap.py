af541f9d4029c730ec980214639e7e291be7bd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:00 +0000

    Add package funmap/0.1.0/funmap-0.1.0.tar.gz

---
+++ packages/funmap/funmap-0.1.0.tar.gz/funmap-0.1.0/tests/test_funmap.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `funmap` package."""
+
+
+import unittest
+
+from funmap import funmap
+
+
+class TestFunmap(unittest.TestCase):
+    """Tests for `funmap` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
