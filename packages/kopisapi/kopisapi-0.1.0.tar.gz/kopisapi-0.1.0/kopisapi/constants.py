aea655545326cbc11e9c6f9feb37f2fd3903fb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:24 +0000

    Add package kopisapi/0.1.0/kopisapi-0.1.0.tar.gz

---
+++ packages/kopisapi/kopisapi-0.1.0.tar.gz/kopisapi-0.1.0/kopisapi/constants.py
@@ -0,0 +1,30 @@
+GENRE_CODE = {
+    "AAAA": "act",
+    "AAAB": "musical",
+    "BBBA": "dance",
+    "CCCA": "classic",
+    "CCCB": "opera",
+    "CCCC": "korean_classical",
+    "EEEA": "complex"
+}
+
+
+AREA_CODE = {
+    "11": "seoul",
+    "26": "busan",  
+    "27": "daegu",
+    "28": "inchon",
+    "29": "gwangju",
+    "30": "daejeon",
+    "31": "ulsan",
+    "36": "sejong",
+    "41": "gyeonggi",
+    "42": "gangwon",
+    "43": "chungbuk",
+    "44": "chungnam",
+    "45": "jeonbuk",
+    "46": "jeonnam",
+    "47": "gyeongbuk",
+    "48": "gyeongnam",
+    "50": "jeju"
+}
