758d1046a7de36a752cc1c65ae8fa428b4675953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:55:11 +0000

    Add package heframework/0.4.0/heframework-0.4.0.tar.gz

---
+++ packages/heframework/heframework-0.4.0.tar.gz/heframework-0.4.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='heframework',
+      version='0.4.0',
+      description='本次更新: 1. 修复emmm提出的show传入标题的问题; 2. 新增choose框架',
+      author='heStudio',
+      author_email='hestudio@hestudio.org',
+      url='https://gitee.com/hestudio-framework/main-windows/',
+      packages=["heframework","heframework.src"],
+     )
+
