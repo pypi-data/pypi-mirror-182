27666d1605c77583b038cf5eafa18a34deb51cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:29 +0000

    Add package dbnd-redshift/1.0.11.1/dbnd-redshift-1.0.11.1.tar.gz

---
+++ packages/dbnd-redshift/dbnd-redshift-1.0.11.1.tar.gz/dbnd-redshift-1.0.11.1/setup.py
@@ -0,0 +1,19 @@
+# © Copyright Databand.ai, an IBM Company 2022
+
+import os
+
+import setuptools
+
+
+BASE_PATH = os.path.dirname(__file__)
+CFG_PATH = os.path.join(BASE_PATH, "setup.cfg")
+
+config = setuptools.config.read_configuration(CFG_PATH)
+version = config["metadata"]["version"]
+
+setuptools.setup(
+    name="dbnd-redshift",
+    package_dir={"": "src"},
+    install_requires=["psycopg2-binary", "dbnd==" + version, "redshift_connector"],
+    entry_points={},
+)
