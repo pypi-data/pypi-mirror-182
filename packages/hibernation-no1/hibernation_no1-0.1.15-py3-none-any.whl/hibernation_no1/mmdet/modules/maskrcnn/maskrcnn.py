e686572e1991219c4154481cbf87c03dc394ee9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:43 +0000

    Add package hibernation-no1/0.1.15/hibernation_no1-0.1.15-py3-none-any.whl

---
+++ packages/hibernation-no1/hibernation_no1-0.1.15-py3-none-any.whl/hibernation_no1/mmdet/modules/maskrcnn/maskrcnn.py
@@ -0,0 +1,18 @@
+
+from hibernation_no1.mmdet.modules.base.module import BaseModule
+from hibernation_no1.mmdet.modules.swin_transformer.swin_transformer import SwinTransformer
+
+class MaskRCNN(BaseModule):
+    def __init__(self,
+                 backbone,
+                 neck,
+                 rpn_head,
+                 roi_head=None,
+                 train_cfg=None,
+                 test_cfg=None,
+                 init_cfg=None):
+        super(MaskRCNN, self).__init__(init_cfg)
+        
+        # TODO: maneging with registry
+        self.backbone = SwinTransformer(**backbone)
+        print(f"self.backbone : \n{self.backbone}")        
