7acb98c969a3cd1ba3c7f317b72f594a660eecf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:17 +0000

    Add package apmath/0.0.2/apmath-0.0.2.tar.gz

---
+++ packages/apmath/apmath-0.0.2.tar.gz/apmath-0.0.2/README.md
@@ -0,0 +1,17 @@
+# Appmath
+
+## Sekcja 1
+To jest mój przykładowy projekt wykorzystujący moduł setuptools i Pythona.
+
+## Sekcja 2
+Przykładowy kod pętli `for`:
+
+```python
+for i in range(100):
+    if i % 2 == 0:
+        print(i)
+```
+
+## Sekcja 3
+To jest tekst **pogrubiony**. \
+To jest tekst *pochylony*.
---
+++ packages/apmath/apmath-0.0.2.tar.gz/apmath-0.0.2/apmath/apmath.py
@@ -0,0 +1,23 @@
+def sum_ab(a, b):
+    return a + b
+
+def subs_ab(a, b):
+    return a - b
+
+def mult_ab(a, b):
+    return a * b
+
+def div_ab_float(a, b):
+    return a / b
+
+def div_ab_int(a, b):
+    return a // b
+
+def modulo_ab(a, b):
+    return a % b
+
+def numberiseven(a):
+    if(a % 2 == 0):
+        return True
+    else:
+        return False
