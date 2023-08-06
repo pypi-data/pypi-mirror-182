fe8aa03cad021c14e5cc14f42dcd61b83a561ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:44 +0000

    Add package whitelist-roblox/0.1.5/whitelist_roblox-0.1.5.tar.gz

---
+++ packages/whitelist-roblox/whitelist_roblox-0.1.5.tar.gz/whitelist_roblox-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='whitelist_roblox',
+    version='0.1.5',    
+    description='Whitelist For Roblox Script',
+    author='MaGiXx#6964',
+    packages=['wl_magixx'],
+    install_requires=[
+        'requests',        
+        'disnake'             
+    ]
+)
