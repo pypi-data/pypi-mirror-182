5d25b6dcba7d7442e4e7dea216694d7289da4571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:35 +0000

    Add package sphinxcontrib-jsonglobaltoc/0.1.2/sphinxcontrib-jsonglobaltoc-0.1.2.tar.gz

---
+++ packages/sphinxcontrib-jsonglobaltoc/sphinxcontrib-jsonglobaltoc-0.1.2.tar.gz/sphinxcontrib-jsonglobaltoc-0.1.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="sphinxcontrib-jsonglobaltoc",
+    version="0.1.2",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "sphinx",
+        "sphinxcontrib-serializinghtml"
+    ],
+    author="Caltech IMSS ADS",
+    author_email="cmalek@caltech.edu",
+    url="https://github.com/caltechads/sphinxcontrib-jsonglobaltoc",
+    description="Sphinx extension to add a global table of contents to JSON output files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['documentation', 'sphinx', 'json'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        "Topic :: Documentation",
+        "Topic :: Software Development :: Documentation",
+    ],
+)
