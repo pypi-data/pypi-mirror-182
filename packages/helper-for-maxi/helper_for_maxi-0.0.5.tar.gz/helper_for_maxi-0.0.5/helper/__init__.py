1de744ba95f4f435772fe177c94fd7f49921ca2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:21 +0000

    Add package helper-for-maxi/0.0.5/helper_for_maxi-0.0.5.tar.gz

---
+++ packages/helper-for-maxi/helper_for_maxi-0.0.5.tar.gz/helper_for_maxi-0.0.5/helper/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Maxi's Functions
+~~~~~~~~~~~~~~~~
+
+Contains all Functions
+
+"""
+
+from .better_input import better_input as BetterInput
+from .better_print import better_print as BetterPrint
+from .output import output as Output
+from .terminal import Terminal as Terminal
