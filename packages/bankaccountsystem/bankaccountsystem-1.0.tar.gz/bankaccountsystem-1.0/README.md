7f17885a096b4a31e73df36b3c4396eebf64dc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:24 +0000

    Add package bankaccountsystem/1.0/bankaccountsystem-1.0.tar.gz

---
+++ packages/bankaccountsystem/bankaccountsystem-1.0.tar.gz/bankaccountsystem-1.0/README.md
@@ -0,0 +1,1 @@
+bankaccountsystem package
---
+++ packages/bankaccountsystem/bankaccountsystem-1.0.tar.gz/bankaccountsystem-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bankaccountsystem',
+    version='1.0',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A test python package',
+	url='https://github.com/Anakin2009/bankaccountsystem',
+    author='Ju, Zhijia',
+    author_email='kcjzj0007@gmail.com'
+)
