dbc4a1b2e43dcb036b65c9512b80829eaf1c9f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:13 +0000

    Add package pypeek/2.4.7/pypeek-2.4.7-py3-none-any.whl

---
+++ packages/pypeek/pypeek-2.4.7-py3-none-any.whl/pypeek/shortcut.py
@@ -0,0 +1,41 @@
+import os, subprocess, platform, static_ffmpeg, shutil
+
+static_ffmpeg.add_paths() # download ffmpeg if necessary
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+desktop_path = os.path.expanduser("~/Desktop")
+
+def win():
+    shortcut_name = "Peek"
+    script_path = r"pypeek-gui"
+    icon_path = f"{dir_path}/icon/peek.ico"
+    shortcut_path = os.path.join(desktop_path, shortcut_name + ".lnk")
+
+    script = f'''
+    $WshShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("{shortcut_path}")
+    $Shortcut.TargetPath = "{script_path}"
+    $Shortcut.IconLocation = "{icon_path}"
+    $Shortcut.Save()
+    '''
+    subprocess.run(["powershell", "-ExecutionPolicy", "Bypass", "-Command", script])
+
+def mac():
+
+    script = f'''
+        tell application "Terminal"
+            if not (exists window 1) then reopen
+            do script "pypeek &;" in window 1
+        end tell
+        '''
+
+    subprocess.run(["osacompile", "-o", f"{desktop_path}/Peek.app", "-e", script])
+    shutil.copy(f"{dir_path}/icon/peek.icns", f"{desktop_path}/peek.app/Contents/Resources/applet.icns")
+
+def create_shortcut():
+    if platform.system() == 'Windows':
+        win()
+    elif platform.system() == 'Darwin':
+        mac()
+    else:
+        pass
