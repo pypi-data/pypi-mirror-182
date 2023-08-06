4af9a49abc234971f7632eb38c55fadf4568dcca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:47 +0000

    Add package qt-command-palette/0.0.5/qt_command_palette-0.0.5-py3-none-any.whl

---
+++ packages/qt-command-palette/qt_command_palette-0.0.5-py3-none-any.whl/qt_command_palette/_storage.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+from typing import Any, Callable, TypeVar
+import inspect
+
+_R = TypeVar("_R")
+
+
+class Storage:
+    """The variable storage."""
+
+    _INSTANCES: dict[str, Storage] = {}
+
+    def __init__(self):
+        self._varmap: dict[str, Callable[[], Any]] = {}
+
+    def mark_getter(self, name: str, func: Callable[[], Any] | None = None):
+        def wrapper(f: Callable[[], Any]):
+            self._varmap[name] = f
+            return f
+
+        return wrapper if func is None else wrapper(func)
+
+    def mark_constant(self, name: str, value: Any):
+        self._varmap[name] = lambda: value
+
+    @classmethod
+    def instance(cls, name: str = "") -> Storage:
+        if name not in cls._INSTANCES:
+            cls._INSTANCES[name] = Storage()
+        return cls._INSTANCES[name]
+
+    def call(self, func: Callable[..., _R], parent=None) -> _R:
+        """Call a function with variables from the storage."""
+        args = []
+        for v in inspect.signature(func).parameters.keys():
+            if v == "self" and parent is not None:
+                args.append(parent)
+            elif getter := self._varmap.get(v, None):
+                args.append(getter())
+            else:
+                raise ValueError(f"Variable {v} not found in storage")
+        return func(*args)
+
+
+def get_storage(name: str = "") -> Storage:
+    """Get the name specific storage instance."""
+    return Storage.instance(name)
