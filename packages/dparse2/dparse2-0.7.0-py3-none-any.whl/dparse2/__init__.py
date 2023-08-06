88bd6963f347a03d23cfb167aa6a0465989bc41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:57 +0000

    Add package dparse2/0.7.0/dparse2-0.7.0-py3-none-any.whl

---
+++ packages/dparse2/dparse2-0.7.0-py3-none-any.whl/dparse2/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c)  Jannis Gebauer and others
+# Originally from https://github.com/pyupio/dparse/
+# Now maintained at https://github.com/nexB/dparse2
+
+# Originally from
+# __author__ = """Jannis Gebauer"""
+# __email__ = 'support@pyup.io'
+
+from dparse2.parser import parse  # noqa
