3e6c9bc4d7dd342469608fe72b574f910e2cd0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:23 +0000

    Add package mt-aws-utils/0.0.4/mt_aws_utils-0.0.4.tar.gz

---
+++ packages/mt-aws-utils/mt_aws_utils-0.0.4.tar.gz/mt_aws_utils-0.0.4/mt_aws_utils/awsutils.py
@@ -0,0 +1,98 @@
+'''Mark This database utility module.'''
+
+from mt_aws_utils.dbutil import Db
+
+
+class AWSMarkThis(Db):
+    '''AWS MarkThis database access layer'''
+
+    def sheet_insert(self, rows):
+        '''Insert sheets.
+        rows is a list of tuples.  Each row has the following items in order
+        sheetid, owner, title, style, answers_csv '''
+
+        qry = '''INSERT into sheet
+                 (sheetid, owner, title, style, answers_csv, created_at)
+                 VALUES(%s, %s, %s, %s, %s, NOW())'''
+
+        return self.executemany(qry, rows)
+
+    def sheet_get(self, sheetid):
+        '''gets sheet info for sheetid'''
+        qry = 'SELECT * FROM sheet WHERE sheetid=%s'
+
+        for row in self.namedselect(qry, (sheetid,)):
+            return row
+
+        return None
+
+    def page_insert(self, rows):
+        '''Insert pages from list of rows (tuples).
+        Tuples in following order.
+        stackid, page_number, original'''
+
+        qry = '''INSERT into page
+                 (stackid, page_number, queued_at, original)
+                 VALUES(%s, %s, NOW(), %s)'''
+
+        return self.executemany(qry, rows)
+
+    def page_get(self, stackid, page_number):
+        '''gets a page based on stackid and page_number'''
+
+        qry = 'SELECT * FROM page WHERE stackid=%s AND page_number=%s'
+
+        for row in self.namedselect(qry, (sheetid,)):
+            return row
+
+        return None
+
+    def page_get_all(self, stackid):
+        '''gets all pages with specific stackid'''
+        qry = 'SELECT * FROM page WHERE stackid=%s'
+
+        for row in self.namedselect(qry, (sheetid,)):
+            return row
+
+        return None
+
+
+    def stack_insert(self, stack):
+        '''Insert stack'''
+
+        arglist = ()
+        columns = []
+
+        for key, val in stack.items():
+            columns += [key]
+            arglist += (val,)
+
+        qry = 'INSERT into stack (' + ', '.join(columns) + ') ' +\
+              'VALUES(' + ', '.join(['%s'] * len(columns)) + ')'
+
+        return self.singlequery(qry, arglist)
+
+    def stack_get(self, stackid):
+        '''gets stack from stackid'''
+        qry = 'SELECT * FROM stack WHERE stackid=%s'
+
+        for row in self.namedselect(qry, (stackid,)):
+            return row
+
+        return None
+
+    def stack_processed_update(self, stackid, status='done'):
+        '''marks stack as processsed'''
+
+        qry = '''UPDATE stack
+                 SET processed_at=NOW(), status_message=%s
+                 WHERE id=%s'''
+
+        return self.singlequery(qry, (status, stackid))
+
+
+
+
+
+
+
