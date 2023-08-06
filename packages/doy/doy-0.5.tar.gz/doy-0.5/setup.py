d2d9c8c0844d4d3d340095147f288e16c85db8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:25 +0000

    Add package doy/0.5/doy-0.5.tar.gz

---
+++ packages/doy/doy-0.5.tar.gz/doy-0.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['doy', 'doy..ipynb_checkpoints']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.6.2,<4.0.0', 'numpy>=1.24.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'doy',
+    'version': '0.5',
+    'description': '',
+    'long_description': '# Doy\n\nSimple utility package\n',
+    'author': 'Dominik Schmidt',
+    'author_email': 'schmidtdominik30@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
