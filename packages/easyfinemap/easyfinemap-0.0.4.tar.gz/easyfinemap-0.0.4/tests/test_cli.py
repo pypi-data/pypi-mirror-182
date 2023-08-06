4fb32a430055f746f4074362f8ec6cb3f98f69e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:00 +0000

    Add package easyfinemap/0.0.4/easyfinemap-0.0.4.tar.gz

---
+++ packages/easyfinemap/easyfinemap-0.0.4.tar.gz/easyfinemap-0.0.4/tests/test_cli.py
@@ -0,0 +1,18 @@
+"""Tests for the cli module."""
+
+from typer.testing import CliRunner
+
+from easyfinemap.cli import app
+
+runner = CliRunner()
+
+def test_main():
+    """Test the main entrypoint."""
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+
+def test_validate_ldref():
+    """Test the validate_ldref command."""
+    result = runner.invoke(app, ["validate-ldref", "--help"])
+    assert result.exit_code == 0
+    assert "validate-ldref" in result.stdout
