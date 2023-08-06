9ded6568f1f2db61ac6e6e3e7cfc1b92c01e147e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:31 +0000

    Add package signal-cli/0.0.4/signal_cli-0.0.4.tar.gz

---
+++ packages/signal-cli/signal_cli-0.0.4.tar.gz/signal_cli-0.0.4/signal_cli/__init__.py
@@ -0,0 +1,12 @@
+from .General import General
+from .Devices import Devices
+from .Groups import Groups
+from .Messages import Messages
+from .Attachments import Attachments
+from .Profiles import Profiles
+from .Identities import Identities
+from .Reactions import Reactions
+from .Search import Search
+from .Contacts import Contacts
+
+__all__ = ['General', 'Devices', 'Groups', 'Messages', 'Attachments', 'Profiles', 'Identities', 'Reactions', 'Search', 'Contacts']
