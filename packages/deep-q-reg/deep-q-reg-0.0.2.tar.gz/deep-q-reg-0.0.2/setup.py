6292e5d3aa4b0a7edcb24c34e8d346247e55e1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:39 +0000

    Add package deep-q-reg/0.0.2/deep-q-reg-0.0.2.tar.gz

---
+++ packages/deep-q-reg/deep-q-reg-0.0.2.tar.gz/deep-q-reg-0.0.2/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup
+# 公開用パッケージの作成 [ezpip]
+import ezpip
+
+# 公開用パッケージの作成 [ezpip]
+with ezpip.packager(develop_dir = "./_develop_deep_q_reg/") as p:
+	setup(
+		name = "deep-q-reg",
+		version = "0.0.2",
+		description = "Tool to easily perform quantile regression using deep learning (pytorch).",
+		author = "bib_inf",
+		author_email = "contact.bibinf@gmail.com",
+		url = "https://github.co.jp/",
+		packages = p.packages,
+		install_requires = ["ezpip", "sout>=1.2.1", "relpath", "fies", "tqdm", "eznorm"],
+		long_description = p.long_description,
+		long_description_content_type = "text/markdown",
+		license = "CC0 v1.0",
+		package_dir = {"deep-q-reg": "deep_q_reg"},
+		package_data = {"deep-q-reg": ["parts/default_params.yml"]},
+		classifiers = [
+			"Programming Language :: Python :: 3",
+			"License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
+		],
+	)
