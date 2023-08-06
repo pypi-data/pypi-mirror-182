437274cbd3577251783c5f5b75eefa12daff33a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:00 +0000

    Add package tpdp/0.0.1/tpdp-0.0.1-py3-none-any.whl

---
+++ packages/tpdp/tpdp-0.0.1-py3-none-any.whl/tpdp/__init__.py
@@ -0,0 +1,16 @@
+from .__info__ import __author__, __email__, __license__, __maintainer__
+from .__version__ import __version__
+from .pipeline import Pipeline, PipelineResult, State, Step, StepResult
+
+__all__ = [
+    "__version__",
+    "__email__",
+    "__author__",
+    "__license__",
+    "__maintainer__",
+    "Pipeline",
+    "PipelineResult",
+    "State",
+    "Step",
+    "StepResult",
+]
