42c2881229bf7155e336f07440d25f2991941db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:58:18 +0000

    Add package termgame/0.0.2/termgame-0.0.2-py3-none-any.whl

---
+++ packages/termgame/termgame-0.0.2-py3-none-any.whl/termgame/settings.py
@@ -0,0 +1,19 @@
+"""Settings for the project."""
+
+import os
+from dataclasses import dataclass
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@dataclass(frozen=True)
+class LogSettings:
+    level: str = os.getenv("LOG_LEVEL", "INFO")
+    file: str = os.getenv("LOG_FILE", "logs/game.log")
+
+
+@dataclass(frozen=True)
+class Settings:
+    log_settings: LogSettings = LogSettings()
