96df0eafd2b772d9dbb12da40671766d002cdd25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:20 +0000

    Add package visual-novel-toolkit/1.2.0/visual_novel_toolkit-1.2.0-py3-none-any.whl

---
+++ packages/visual-novel-toolkit/visual_novel_toolkit-1.2.0-py3-none-any.whl/visual_novel_toolkit/cli.py
@@ -0,0 +1,27 @@
+# mypy: disable-error-code = misc
+from typer import Exit
+from typer import Typer
+
+from .find_unused_words import find_unused_words
+from .make_draft import make_draft
+from .sort_words import sort_words
+
+
+cli = Typer()
+
+
+@cli.command()
+def sort() -> None:
+    if sort_words():
+        raise Exit(code=1)
+
+
+@cli.command()
+def unused() -> None:
+    if find_unused_words():
+        raise Exit(code=1)
+
+
+@cli.command()
+def draft() -> None:
+    make_draft()
