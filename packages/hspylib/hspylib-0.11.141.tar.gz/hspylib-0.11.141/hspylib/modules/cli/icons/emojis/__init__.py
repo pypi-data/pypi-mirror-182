e2afb400695cd982b53c9adf6a6837fd34053c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:41 +0000

    Add package hspylib/0.11.141/hspylib-0.11.141.tar.gz

---
+++ packages/hspylib/hspylib-0.11.141.tar.gz/hspylib-0.11.141/hspylib/modules/cli/icons/emojis/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.141
+#
+# Package: main.hspylib.modules.cli.icons.emojis
+"""Package initialization."""
+
+__all__ = [
+  'emojis', 
+  'face_smiling'
+]
+__version__ = '0.11.141'
---
+++ packages/hspylib/hspylib-0.11.141.tar.gz/hspylib-0.11.141/hspylib/modules/cli/tui/mdashboard/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.141
+#
+# Package: main.hspylib.modules.cli.tui.mdashboard
+"""Package initialization."""
+
+__all__ = [
+  'dashboard_builder', 
+  'dashboard_item', 
+  'mdashboard'
+]
+__version__ = '0.11.141'
