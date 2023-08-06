074bac10369f8613e2356e993b976cd03d8a953d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:52 +0000

    Add package simpleandfunnycalculator/0.0.1/simpleandfunnycalculator-0.0.1.tar.gz

---
+++ packages/simpleandfunnycalculator/simpleandfunnycalculator-0.0.1.tar.gz/simpleandfunnycalculator-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Time series classification using Deep Learning'
+
+
+# Setting up
+setup(
+    name="simpleandfunnycalculator",
+    version=VERSION,
+    author="JavidanAbdullayev (Javidan Abdullayev)",
+    author_email="<javidan.abdullayev@uha.fr>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
