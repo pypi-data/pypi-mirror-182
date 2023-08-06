6b508c721788c1da3c46042c0961b2254f9534e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:50 +0000

    Add package nonebot-plugin-maze/0.2.4/nonebot_plugin_maze-0.2.4.tar.gz

---
+++ packages/nonebot-plugin-maze/nonebot_plugin_maze-0.2.4.tar.gz/nonebot_plugin_maze-0.2.4/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nonebot_plugin_maze",
+    version="0.2.4",
+    author="EtherLeaF",
+    author_email="thetapilla@gmail.com",
+    keywords=["pip", "nonebot2", "nonebot", "nonebot_plugin"],
+    description="""NoneBot2交互式走迷宫插件""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EtherLeaF/nonebot_plugin_maze",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        'nonebot-adapter-onebot>=2.1.0',
+        'nonebot2>=2.0.0-beta.2',
+        'numpy>=1.24.0',
+        'pillow>=9.0.0',
+        'asyncer>=0.0.1'
+    ],
+    python_requires=">=3.8"
+)
