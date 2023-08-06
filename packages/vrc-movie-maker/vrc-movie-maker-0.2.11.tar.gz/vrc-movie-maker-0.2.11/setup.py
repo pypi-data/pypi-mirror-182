8376b0bdf54082688dd77b12f3c3a15bcbcc87ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:31:18 +0000

    Add package vrc-movie-maker/0.2.11/vrc-movie-maker-0.2.11.tar.gz

---
+++ packages/vrc-movie-maker/vrc-movie-maker-0.2.11.tar.gz/vrc-movie-maker-0.2.11/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vrc-movie-maker",
+    version="0.2.11",
+    license='MIT',
+    author="Noricha",
+    author_email="noricha.vr@gmail.com",
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    install_requires=[
+        'selenium>=4.5.0',
+        'webdriver-manager>=3.8.4',
+        'source-converter>=0.1.17',
+        'Pillow>=9.3.0',
+    ],
+    url='https://github.com/noricha-vr/MovieMaker',
+    extras_require={
+        "develop": ['pytest']
+    },
+)
