3ddd4d90877d8446a0860e9e5fd43161b94354b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:11 +0000

    Add package perceval-quandela/0.7.3/perceval_quandela-0.7.3-py3-none-any.whl

---
+++ packages/perceval-quandela/perceval_quandela-0.7.3-py3-none-any.whl/perceval/__init__.py
@@ -0,0 +1,46 @@
+# MIT License
+#
+# Copyright (c) 2022 Quandela
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+Through a simple object-oriented Python API, Perceval provides tools for composing photonic circuits from linear
+optical components like beamsplitters and phase shifters, defining single-photon sources, manipulating Fock states,
+and running simulations."""
+
+from pkg_resources import get_distribution
+import importlib
+
+__version__ = get_distribution("perceval-quandela").version
+
+from .components import *
+from .backends import *
+from .utils import *
+from .rendering import *
+from .runtime import *
+
+
+def register_plugin(name, silent=False):
+    try:
+        plugin = importlib.import_module(name)
+        assert plugin.register(silent) is True
+    except Exception as e:
+        raise RuntimeError("cannot import %s: %s" % (name, str(e)))
+    return True
