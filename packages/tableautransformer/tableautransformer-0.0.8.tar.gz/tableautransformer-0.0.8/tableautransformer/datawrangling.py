0e3b52af39b83de7caa40046acee08f9c2bbdd93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:47 +0000

    Add package tableautransformer/0.0.8/tableautransformer-0.0.8.tar.gz

---
+++ packages/tableautransformer/tableautransformer-0.0.8.tar.gz/tableautransformer-0.0.8/tableautransformer/datawrangling.py
@@ -0,0 +1,23 @@
+# This module has more granulated functions which aid in the manipulation of data
+import pandas as pd
+import numpy as np
+
+def basic_table(read_type: str, sheet_name: str, read_path: str, columns_to_keep, columns_rename):
+    
+    # read in the file
+    if read_type == 'csv':
+        df_basic_table = pd.read_csv(read_path)
+    elif read_type == 'excel':
+        df_basic_table = pd.read_excel(read_path, sheet_name=sheet_name)
+    else:
+        print('read_type must be either "csv" or "excel"')
+    
+    # columns to keep 
+    df_basic_table = df_basic_table[[columns_to_keep]]
+
+    # rename columns
+    df_basic_table.columns = [columns_rename]
+
+    return df_basic_table
+
+
