ffe6cb0d71994135098caeb8fdcdbf7dc9daab4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:24 +0000

    Add package objectprogramming/104/objectprogramming-104-py3-none-any.whl

---
+++ packages/objectprogramming/objectprogramming-104-py3-none-any.whl/op/__init__.py
@@ -0,0 +1,17 @@
+# This file is placed in the Public Domain.
+# pylint: disable=W0622
+
+
+"object programming (op)"
+
+
+from op import evt, hdl, obj, run, thr, usr, ver
+
+
+from op.evt import *
+from op.hdl import *
+from op.obj import *
+from op.run import *
+from op.thr import *
+from op.usr import *
+from op.ver import __version__
