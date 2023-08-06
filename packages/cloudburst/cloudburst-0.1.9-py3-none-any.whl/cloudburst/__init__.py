11e28694d44204d37e1b8560e9c2cf5a59eda100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:27 +0000

    Add package cloudburst/0.1.9/cloudburst-0.1.9-py3-none-any.whl

---
+++ packages/cloudburst/cloudburst-0.1.9-py3-none-any.whl/cloudburst/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2022-present William W. Marx <m94@marx.sh>
+#
+# SPDX-License-Identifier: Apache 2.0
+"""cloudburst"""
+
+from .twitter import TwitterSession
