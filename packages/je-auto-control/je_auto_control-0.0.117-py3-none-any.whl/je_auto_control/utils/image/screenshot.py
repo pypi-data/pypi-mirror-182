a2a59240b4ca8feb56fe671f89c513e053531c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:25 +0000

    Add package je-auto-control/0.0.117/je_auto_control-0.0.117-py3-none-any.whl

---
+++ packages/je-auto-control/je_auto_control-0.0.117-py3-none-any.whl/je_auto_control/utils/image/screenshot.py
@@ -0,0 +1,16 @@
+from PIL import ImageGrab, Image
+
+
+def pil_screenshot(file_path: str = None, region: list = None) -> Image:
+    """
+    use pil to make a screenshot
+    :param file_path save screenshot path (None is no save)
+    :param region screenshot region (screenshot region on screen)
+    """
+    if region is not None:
+        image = ImageGrab.grab(bbox=region)
+    else:
+        image = ImageGrab.grab()
+    if file_path is not None:
+        image.save(file_path)
+    return image
