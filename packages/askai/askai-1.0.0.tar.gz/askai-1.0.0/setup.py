1254f2226ba542137a5c45a8c9d48fba10a66b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:32 +0000

    Add package askai/1.0.0/askai-1.0.0.tar.gz

---
+++ packages/askai/askai-1.0.0.tar.gz/askai-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup  # type: ignore
+
+
+setup(
+    name="askai",
+    version="1.0.0",
+    author="Max Fischer",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/maxvfischer/askai",
+    download_url="https://github.com/maxvfischer/askai/archive/refs/tags/v1.0.0.tar.gz",
+    py_modules=["entrypoint_askai"],
+    install_requires=[
+        "click==8.1.3",
+        "openai==0.25.0",
+        "PyYAML==6.0",
+    ],
+    entry_points="""
+        [console_scripts]
+        askai=entrypoint_askai:askai
+    """
+)
