08d1d30c63b4572b5065297152ea5b1f46338090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:14 +0000

    Add package anicli-api/0.2.1/anicli_api-0.2.1.tar.gz

---
+++ packages/anicli-api/anicli_api-0.2.1.tar.gz/anicli_api-0.2.1/anicli_api/decoders/__init__.py
@@ -0,0 +1,5 @@
+from anicli_api.decoders.aniboom import Aniboom
+from anicli_api.decoders.kodik import Kodik
+from anicli_api.decoders.sibnet import Sibnet
+
+ALL_DECODERS = [Kodik, Aniboom, Sibnet]
