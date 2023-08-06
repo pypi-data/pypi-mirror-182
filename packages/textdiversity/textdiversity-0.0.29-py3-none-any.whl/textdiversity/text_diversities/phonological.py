29e4cffe59752c2b1624437b6c0f2505e153e41d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:18 +0000

    Add package textdiversity/0.0.29/textdiversity-0.0.29-py3-none-any.whl

---
+++ packages/textdiversity/textdiversity-0.0.29-py3-none-any.whl/textdiversity/text_diversities/phonological.py
@@ -0,0 +1,250 @@
+# TextDiversity pkgs
+import spacy
+import numpy as np
+import pandas as pd
+from functools import partial
+import logging
+logging.basicConfig(level=logging.CRITICAL)
+
+from Bio import Align
+
+# rhythmic diversity
+import cadence as cd
+import string
+
+# phonemic diversity
+from phonemizer import phonemize
+from phonemizer.backend import EspeakBackend
+from phonemizer.punctuation import Punctuation
+from phonemizer.separator import Separator
+
+import os
+import sys
+from collections import Counter
+
+# locals
+from ..metric import TextDiversity
+from ..utils import *
+
+# ==================================================================================
+
+class Rhythmic(TextDiversity):
+
+    default_config = {
+        # TextDiversity configs
+        'q': 1,
+        'normalize': False,
+        'remove_stopwords': False, 
+        'verbose': False,
+        # Rhythmic configs
+        'pad_to_max_len': False,
+        'split_sentences': False,
+    }
+
+    def __init__(self, config={}):
+        config = {**self.default_config, **config} 
+        super().__init__(config)
+        self.model = spacy.load("en_core_web_sm")
+        self.aligner = Align.PairwiseAligner()
+
+    def align_and_score(self, seq1, seq2):
+        return self.aligner.align(seq1, seq2).score
+
+    def extract_features(self, corpus, return_ids=False):
+
+        # clean corpus
+        corpus = clean_text(corpus)
+
+        # split sentences
+        if self.config['split_sentences']:
+            corpus, text_ids, sentence_ids = split_sentences(corpus, return_ids=True)
+        else:
+            ids = list(range(len(corpus)))
+            text_ids, sentence_ids = ids, ids
+
+        # strip punctuation
+        corpus = [d.translate(str.maketrans('', '', string.punctuation)) for d in corpus]
+
+        # # remove any blanks...
+        # corpus = [d for d in corpus if len(d.strip()) > 0]
+
+        # extracts rhythms (sequences of [un]weighted [un]stressed syllables)
+        rhythms = []
+        for s in corpus:
+            prose = cd.Prose(s)
+            df = prose.sylls().reset_index()
+            if all([c in df.columns for c in ['syll_stress', 'syll_weight']]):
+                rhythm = df[df['word_ipa_i'] == 1][['syll_stress', 'syll_weight']].values
+                rhythm = ["".join(row) for row in rhythm]
+            else:
+                rhythm = [""]
+            rhythms.append(rhythm)
+
+        # compute max seq length for padding / normalization later
+        self.max_len = find_max_list(rhythms)
+
+        # pad to max sentence doc length
+        if self.config['pad_to_max_len']:
+            rhythms = np.array([r + ['N'] * (self.max_len - len(r)) for r in rhythms])
+
+        if return_ids:
+            return rhythms, corpus, text_ids, sentence_ids 
+        return rhythms, corpus
+
+    def calculate_similarities(self, features):
+        """
+        Uses biopython.Bio.Align.PairwiseAligner() to align and score stands of
+        phonological DNA (i.e. sequences of [un]weighted [un]stressed syllables)
+        """
+
+        if is_list_of_lists(features):
+            # convert rhythm tags to alphas
+            features = tag2alpha(features)
+            features = ["".join(rhythm) for rhythm in features]
+
+        # compute pairwise alignment + scoring
+        Z = compute_Z_pairwise(
+            features, 
+            self.align_and_score, 
+            self.max_len, 
+            verbose=self.config['verbose'])
+
+        # normalize the similarities by max_len
+        Z = Z / self.max_len
+
+        return Z
+
+
+    def calculate_similarity_vector(self, q_feat, c_feat):
+
+        features = [q_feat] + c_feat
+
+        if is_list_of_lists(features):
+            # convert rhythm tags to alphas
+            features = tag2alpha(features)
+            features = ["".join(pos) for pos in features]
+
+        q_feat = features[0]
+        c_feat = features[1:]
+
+        q_len = len(q_feat)
+        scores = []
+        for f in c_feat:
+            score = self.align_and_score(q_feat, f)
+            score /= max(len(f), q_len)
+            scores.append(score)
+
+        z = np.array(scores)
+
+        return z
+
+    def calculate_abundance(self, species):
+        num_species = len(species)
+        p = np.full(num_species, 1 / num_species)
+        return p
+
+    def __call__(self, corpus): 
+        return super().__call__(corpus)
+
+
+class Phonemic(TextDiversity):
+
+    default_config = {
+        # TextDiversity configs
+        'q': 1,
+        'normalize': False,
+        'remove_stopwords': False, 
+        'verbose': False,
+    }
+
+    def __init__(self, config={}):
+        config = {**self.default_config, **config} 
+        super().__init__(config)
+
+        # load pre-computed phonemic similarity matrix Z
+        file_dir = os.path.dirname(__file__)
+        csv_path = os.path.join(file_dir, '../similarities/phoneme_similarities.csv')
+        self.Z = pd.read_csv(csv_path, index_col=0)
+
+        # phonemizer
+        logger = logging.getLogger()
+        logger.disabled = True
+        self.backend = EspeakBackend('en-us', words_mismatch='ignore', logger=logger)
+        self.separator = Separator(phone=' ', word=None)
+
+    def get_phonemes(self, corpus, return_counts=True):
+        # remove all the punctuation from the text, considering only the specified
+        # punctuation marks
+        text = Punctuation(';:,.!"?()-/><').remove(corpus)
+
+        # build the set of all the words in the text
+        words = {w.lower() for line in text for w in line.strip().split(' ') if w}
+
+        # build the lexicon by phonemizing each word one by one. The backend.phonemize
+        # function expect a list as input and outputs a list.
+        lexicon = {
+            word : self.backend.phonemize([word], separator=self.separator, strip=True)[0]
+            for word in words} 
+
+        if return_counts:
+            phoneme_counts = Counter(" ".join(lexicon.values()).split(" "))
+            del phoneme_counts[" "]
+            return lexicon, phoneme_counts
+        
+        return lexicon 
+
+    def extract_features(self, corpus):
+
+        # get phonemes
+        lexicon, phoneme_counts = self.get_phonemes(corpus)
+
+        # TODO: Think of a more elegant way of doing this...
+        #       The phonemic diversity calculation doesn't follow
+        #       the same flow as other diversities because we
+        #       precomputed the Z matrix and the main thing 
+        #       needed now is the phoneme_counts.
+        return phoneme_counts, phoneme_counts
+
+    def calculate_similarities(self, features):
+
+        unwanted = set(features) - set(self.Z)
+        for unwanted_key in unwanted: del features[unwanted_key]
+        phoneme_list = list(features.keys())
+        Z = self.Z.loc[phoneme_list][phoneme_list].to_numpy()
+
+        return Z
+
+    def calculate_similarity_vector(self, q_feat, c_feat):
+        raise Exception("Ranking requires metrics that operate on the document level. Try Rhythmic instead.")
+
+    def calculate_abundance(self, species):
+        p = np.array(list(species.values()), dtype=np.float16)
+        p /= p.sum()
+        return p
+
+    def __call__(self, corpus): 
+        return super().__call__(corpus)
+
+
+if __name__ == '__main__':
+
+    # TEST
+    lo_div = ['one massive earth', 'an enormous globe', 'the colossal world']
+    hi_div = ['basic human right', 'you were right', 'make a right']
+
+    # diversities
+    print("diversities")
+    print_div_metric(Rhythmic, lo_div, hi_div)
+    print_div_metric(Phonemic, lo_div, hi_div)
+
+    # similarities
+    print("similarities")
+    print_sim_metric(Rhythmic, lo_div, hi_div)
+    print_sim_metric(Phonemic, lo_div, hi_div)
+
+    # rank similarities
+    print("rankings")
+    print_ranking(Rhythmic, ["I was wrong"], lo_div + hi_div)
+    # print_ranking(Phonemic, "burn big planets", lo_div + hi_div)
+
+    # (textdiv) ~\GitHub\TextDiversity\src>python -m textdiversity.text_diversities.phonological
