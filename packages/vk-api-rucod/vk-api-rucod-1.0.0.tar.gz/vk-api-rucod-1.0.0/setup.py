19500893513a50d35e31c6736810783beb551e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:00 +0000

    Add package vk-api-rucod/1.0.0/vk-api-rucod-1.0.0.tar.gz

---
+++ packages/vk-api-rucod/vk-api-rucod-1.0.0.tar.gz/vk-api-rucod-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vk-api-rucod',
+    version='1.0.0',
+    packages=['vk-api-rucod'],
+    author='RuCodinger',
+    author_email='rucodinger@gmail.com',
+    description='Library for create vk bots on Python',
+    zip_safe=False
+)
