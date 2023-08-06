d2a0e32747587dbc324618841d949c666f496368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:29 +0000

    Add package rextk/0.0.0/rextk-0.0.0.tar.gz

---
+++ packages/rextk/rextk-0.0.0.tar.gz/rextk-0.0.0/setup.py
@@ -0,0 +1,24 @@
+
+"""
+@author: Chris Lockhart <clockha2@gmu.edu>
+"""
+
+from setuptools import setup
+
+
+setup(
+    name='rextk',
+    version='0.0.0',
+    author='Chris Lockhart',
+    author_email='clockha2@gmu.edu',
+    description='',
+    long_description='',
+    url="https://www.lockhartlab.org/",
+    packages=[
+        'rextk',
+    ],
+    install_requires=None,
+    include_package_data=True,
+    zip_safe=True,
+)
+
