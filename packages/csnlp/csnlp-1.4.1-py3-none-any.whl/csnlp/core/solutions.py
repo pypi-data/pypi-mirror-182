70780d428b72082d879f97441b48e0f3fe2551cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:16 +0000

    Add package csnlp/1.4.1/csnlp-1.4.1-py3-none-any.whl

---
+++ packages/csnlp/csnlp-1.4.1-py3-none-any.whl/csnlp/core/solutions.py
@@ -0,0 +1,168 @@
+from dataclasses import dataclass
+from itertools import product
+from typing import Any, Callable, Dict, Generic, Iterable, TypeVar, Union
+
+import casadi as cs
+import numpy as np
+from casadi.tools.structure3 import CasadiStructured, DMStruct
+
+from csnlp.core.data import array2cs, cs2array
+
+T = TypeVar("T", cs.SX, cs.MX)
+
+
+@dataclass(frozen=True)
+class Solution(Generic[T]):
+    """Class containing information on the solution of an NLP solver's run."""
+
+    f: float
+    vars: Dict[str, T]
+    vals: Dict[str, cs.DM]
+    stats: Dict[str, Any]
+    _get_value: Callable[[T, bool], cs.DM]
+
+    @property
+    def status(self) -> str:
+        """Gets the status of the solver at this solution."""
+        return self.stats["return_status"]
+
+    @property
+    def success(self) -> bool:
+        """Gets whether the solver's run was successful."""
+        return self.stats["success"]
+
+    @property
+    def barrier_parameter(self) -> float:
+        """Gets the IPOPT barrier parameter at the optimal solution"""
+        return self.stats["iterations"]["mu"][-1]
+
+    def value(self, x: T, eval: bool = True) -> Union[T, cs.DM]:
+        """Computes the value of the expression substituting the values of this
+        solution in the expression.
+
+        Parameters
+        ----------
+        x : casadi.SX or MX
+            The symbolic expression to be evaluated at the solution's values.
+        eval : bool, optional
+            Evaluates numerically the new expression. By default, `True`. See
+            `csnlp.solutions.subsevalf` for more details.
+
+        Returns
+        -------
+        casadi.SX or MX or DM
+            The expression evaluated with the solution's values.
+
+        Raises
+        ------
+        RuntimeError
+            Raises if `eval=True` but there are symbolic variables that are
+            still free since they are outside the solution's variables.
+        """
+        return self._get_value(x, eval=eval)  # type: ignore
+
+
+def _internal_subsevalf_cs(
+    expr: T,
+    old: Union[T, Dict[str, T], Iterable[T]],
+    new: Union[T, Dict[str, T], Iterable[T]],
+    eval: bool,
+) -> Union[T, cs.DM]:
+    """Internal utility for substituting and evaluting casadi objects."""
+    if isinstance(expr, (cs.DM, DMStruct)):
+        return expr
+
+    if isinstance(old, dict):
+        for name, o in old.items():
+            expr = cs.substitute(expr, o, new[name])  # type: ignore
+    elif isinstance(old, Iterable) and not isinstance(
+        old, (cs.SX, cs.MX, CasadiStructured)
+    ):
+        for o, n in zip(old, new):
+            expr = cs.substitute(expr, o, n)
+    else:
+        expr = cs.substitute(expr, old, new)
+
+    if eval:
+        expr = cs.evalf(expr)
+    return expr
+
+
+def _internal_subsevalf_np(
+    expr: np.ndarray,
+    old: Union[T, Dict[str, T], Iterable[T]],
+    new: Union[T, Dict[str, T], Iterable[T]],
+    eval: bool,
+) -> Union[T, np.ndarray, cs.DM]:
+    """Internal utility for substituting and evaluting arrays of casadi objects."""
+    # if not symbolic, return it
+    if expr.dtype != object:
+        return expr
+
+    # up to 2D, we can get away with only one substitution
+    if expr.ndim <= 2:
+        return _internal_subsevalf_cs(array2cs(expr), old, new, eval)
+
+    # for tensors, check if the right end or the left is bigger, then loop over the
+    # rest while substituing in the right/left end of the array
+    shape = expr.shape
+    transposed = (shape[0] * shape[1]) > (shape[-2] * shape[-1])
+    if transposed:
+        shape = shape[::-1]
+        expr = expr.transpose()
+    out = np.empty(shape, dtype=object)
+    shape_iter, shape_cs = shape[:-2], shape[-2:]
+    for i in product(*map(range, shape_iter)):
+        out[i] = cs2array(
+            _internal_subsevalf_cs(array2cs(expr[i]), old, new, eval)
+        ).reshape(shape_cs)
+    if transposed:
+        out = out.transpose()
+        expr = expr.transpose()
+
+    if eval:
+        out = out.astype(float)
+    return out
+
+
+def subsevalf(
+    expr: Union[T, np.ndarray],
+    old: Union[T, Dict[str, T], Iterable[T]],
+    new: Union[T, Dict[str, T], Iterable[T]],
+    eval: bool = True,
+) -> Union[T, cs.DM, np.ndarray]:
+    """
+    Substitutes the old variables with the new ones in the symbolic expression,
+    and evaluates it, if required.
+
+    Parameters
+    ----------
+    expr : casadi.SX, MX or an array of these
+        Expression for substitution and, possibly, evaluation.
+    old : casadi.SX, MX (or struct, dict, iterable of)
+        Old variable to be substituted.
+    new : numpy.array or casadi.SX, MX, DM (or struct, dict, iterable of)
+        New variable that substitutes the old one. If a collection, it is
+        assumed the type is the same of `old` (so, old and new should share
+        collection type).
+    eval : bool, optional
+        Evaluates numerically the new expression. By default, `True`.
+
+    Returns
+    -------
+    new_expr : casadi.SX, MX, DM or an array of these
+        New expression after substitution (SX, MX) and, possibly, evaluation
+        (DM).
+
+    Raises
+    ------
+    Exception
+        Raises if `old` and `new` are not compatible with `casadi.substitute`; or if
+        `eval=True` but there are symbolic variables that are still free, i.e., the
+        expression cannot be evaluated numerically since it is still (partially)
+        symbolic.
+    """
+    if isinstance(expr, np.ndarray):
+        return _internal_subsevalf_np(expr, old, new, eval)
+    else:
+        return _internal_subsevalf_cs(expr, old, new, eval)
---
+++ packages/csnlp/csnlp-1.4.1-py3-none-any.whl/csnlp/util/math.py
@@ -0,0 +1,195 @@
+from itertools import combinations
+from math import sqrt
+from typing import Optional, Union
+
+import casadi as cs
+import numpy as np
+import numpy.typing as npt
+from scipy.special import comb
+
+SQRT2 = sqrt(2)
+
+
+def log(
+    x: Union[cs.SX, cs.MX, cs.DM], base: Union[None, cs.SX, cs.MX, cs.DM] = None
+) -> Union[cs.SX, cs.MX, cs.DM]:
+    """Logarithm. With one argument, return the natural logarithm of `x` (to base `e`).
+    With two arguments, return the logarithm of x to the given base, calculated as
+    `log(x) / log(base)`.
+
+    Parameters
+    ----------
+    x : Union[cs.SX, cs.MX, cs.DM]
+        Value to compute the logarithm of.
+    base : Union[None, cs.SX, cs.MX, cs.DM], optional
+        Base of the logarithm, by default None
+
+    Returns
+    -------
+    Union[cs.SX, cs.MX, cs.DM]
+        The logarithm. of `x` with base `base`.
+    """
+    return cs.log(x) if base is None else cs.log(x) / cs.log(base)
+
+
+def prod(
+    x: Union[cs.SX, cs.MX, cs.DM], axis: Optional[int] = None
+) -> Union[cs.SX, cs.MX, cs.DM]:
+    """Computes the product of all the elements in `x` (CasADi version of `numpy.prod`).
+
+    Parameters
+    ----------
+    x : Union[cs.SX, cs.MX, cs.DM]
+        The variable whose entries must be multiplied together.
+    axis : {0, 1, None}
+        Axis or along which a product is performed. The default, `axis=None`, will
+        calculate the product of all the elements in the matrix. If axis is negative it
+        counts from the last to the first axis.
+
+    Returns
+    -------
+    Union[cs.SX, cs.MX, cs.DM]
+        Product of the elements.
+    """
+    if axis is None:
+        x = cs.vec(x)
+        axis = 0
+    elif axis < 0:
+        axis = 2 + axis
+    sum_ = cs.sum1 if axis == 0 else cs.sum2
+    n_negatives = sum_(x < 0)
+    p = cs.exp(sum_(cs.log(cs.fabs(x))))
+    return cs.if_else(cs.mod(n_negatives, 2) == 0.0, 1, -1) * p
+
+
+def quad_form(
+    A: Union[cs.SX, cs.MX, cs.DM], x: Union[cs.SX, cs.MX, cs.DM]
+) -> Union[cs.SX, cs.MX, cs.DM]:
+    """Calculates quadratic form `x.T*A*x`.
+
+    Parameters
+    ----------
+    A : Union[cs.SX, cs.MX, cs.DM]
+        The matrix of weights in the quadratic form. If a vector, a matrix with the
+        vector as diagonal is used instead.
+    x : Union[cs.SX, cs.MX, cs.DM]
+        The vector in the quadratic form.
+
+    Returns
+    -------
+    Union[cs.SX, cs.MX, cs.DM]
+        The result of the quadratic form.
+
+    Raises
+    ------
+    RuntimeError
+        Raises if
+         - `A` is not squared
+         - `A` and `x` cannot be multiplied together
+         - `x` is not a vector.
+    """
+    if A.is_vector():
+        A = cs.diag(A)
+    return cs.bilin(A, x, x)
+
+
+def norm_cdf(
+    x: Union[cs.SX, cs.MX, cs.DM],
+    loc: Union[cs.SX, cs.MX, cs.DM] = cs.DM(0),
+    scale: Union[cs.SX, cs.MX, cs.DM] = cs.DM(1),
+) -> Union[cs.SX, cs.MX, cs.DM]:
+    """Computes the cdf of a normal distribution. See `scipy.stats.norm.cdf`.
+
+    Parameters
+    ----------
+    x : Union[cs.SX, cs.MX, cs.DM]
+        The value at which the cdf is computed.
+    loc : Union[cs.SX, cs.MX, cs.DM], optional
+        Mean of the normal distribution. By default, `loc=0`.
+    scale : Union[cs.SX, cs.MX, cs.DM], optional
+        Standard deviation of the normal distribution. By default, `scale=0`.
+
+    Returns
+    -------
+    Union[cs.SX, cs.MX, cs.DM]
+        The cdf of the normal distribution.
+    """
+    return 0.5 * (1 + cs.erf((x - loc) / SQRT2 / scale))
+
+
+def norm_ppf(
+    p: Union[cs.SX, cs.MX, cs.DM],
+    loc: Union[cs.SX, cs.MX, cs.DM] = cs.DM(0),
+    scale: Union[cs.SX, cs.MX, cs.DM] = cs.DM(1),
+) -> Union[cs.SX, cs.MX, cs.DM]:
+    """Computes the quantile (invese of cdf) of a normal distribution. See
+    `scipy.stats.norm.ppf`.
+
+    Parameters
+    ----------
+    x : Union[cs.SX, cs.MX, cs.DM]
+        The value at which the quantile is computed.
+    loc : Union[cs.SX, cs.MX, cs.DM], optional
+        Mean of the normal distribution. By default, `loc=0`.
+    scale : Union[cs.SX, cs.MX, cs.DM], optional
+        Standard deviation of the normal distribution. By default, `scale=0`.
+
+    Returns
+    -------
+    Union[cs.SX, cs.MX, cs.DM]
+        The quantile of the normal distribution.
+    """
+    return SQRT2 * scale * cs.erfinv(2 * p - 1) + loc
+
+
+def nchoosek(n: Union[int, npt.ArrayLike], k: int) -> Union[int, np.ndarray]:
+    """Emulates the `nchoosek` function from Matlab. Returns the binomial coefficient,
+    i.e.,  the number of combinations of `n` items taken `k` at a time. If `n` is an
+    array, then it is flatten and all possible combinations of its elements are
+    returned.
+
+    Parameters
+    ----------
+    n : int or array_like
+        Number of elements or array of elements to choose from.
+    k : int
+        Number of elements to choose.
+
+    Returns
+    -------
+    int or array
+        Depending on the type of input `n`, the output is either the total number of
+        combinations or the combinations in a matrix.
+    """
+    return (
+        comb(n, k, exact=True)
+        if isinstance(n, int)
+        else np.row_stack(list(combinations(np.asarray(n).flatten(), k)))
+    )
+
+
+def monomial_powers(d: int, k: int) -> npt.NDArray[np.int64]:
+    """Computes the powers of all `d`-dimensional monomials of degree `k`.
+
+    Parameters
+    ----------
+    d : int
+        The number of monomial elements.
+    k : int
+        The degree of each monomial.
+
+    Returns
+    -------
+    array of ints
+        An array containing in each row the power of each index in order to obtain the
+        desired monomial of power `k`.
+    """
+    m = nchoosek(k + d - 1, d - 1)
+    dividers = np.column_stack(
+        (
+            np.zeros((m, 1), dtype=int),
+            np.row_stack(nchoosek(np.arange(1, k + d), d - 1)),  # type: ignore
+            np.full((m, 1), k + d, dtype=int),
+        )
+    )
+    return np.flipud(np.diff(dividers, axis=1) - 1).astype(int)
