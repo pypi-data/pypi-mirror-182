444c9a0cdc95f7b7f20c7321aa2a6858f9847047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:58 +0000

    Add package lightning/1.8.6/lightning-1.8.6.tar.gz

---
+++ packages/lightning/lightning-1.8.6.tar.gz/lightning-1.8.6/src/lightning/lite/plugins/__init__.py
@@ -0,0 +1,36 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from lightning.lite.plugins.environments.cluster_environment import ClusterEnvironment
+from lightning.lite.plugins.io.checkpoint_io import CheckpointIO
+from lightning.lite.plugins.io.torch_io import TorchCheckpointIO
+from lightning.lite.plugins.io.xla import XLACheckpointIO
+from lightning.lite.plugins.precision.deepspeed import DeepSpeedPrecision
+from lightning.lite.plugins.precision.double import DoublePrecision
+from lightning.lite.plugins.precision.native_amp import NativeMixedPrecision
+from lightning.lite.plugins.precision.precision import Precision
+from lightning.lite.plugins.precision.tpu import TPUPrecision
+from lightning.lite.plugins.precision.tpu_bf16 import TPUBf16Precision
+
+__all__ = [
+    "ClusterEnvironment",
+    "CheckpointIO",
+    "TorchCheckpointIO",
+    "XLACheckpointIO",
+    "Precision",
+    "DeepSpeedPrecision",
+    "DoublePrecision",
+    "NativeMixedPrecision",
+    "TPUPrecision",
+    "TPUBf16Precision",
+]
