80b4b7b8f0bacfdd18dae54524b7ece1e81b1414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:21 +0000

    Add package hspylib/0.11.140/hspylib-0.11.140.tar.gz

---
+++ packages/hspylib/hspylib-0.11.140.tar.gz/hspylib-0.11.140/hspylib/modules/cli/tui/table/__init__.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.140
+#
+# Package: main.hspylib.modules.cli.tui.table
+"""Package initialization."""
+
+__all__ = [
+  'table_renderer'
+]
+__version__ = '0.11.140'
