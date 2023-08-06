4d05d442ba89cf1d0e9b85dbbdbaa1cb30cc3329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:49 +0000

    Add package nonebot-plugin-dida/2.0.0.1/nonebot_plugin_dida-2.0.0.1.tar.gz

---
+++ packages/nonebot-plugin-dida/nonebot_plugin_dida-2.0.0.1.tar.gz/nonebot_plugin_dida-2.0.0.1/nonebot_plugin_dida/file/source/column.md
@@ -0,0 +1,12 @@
+# 分组字典字段
+| 字段名 |类型 | 说明 |
+|:----|:----|:----|
+|id | str | 分组的ID|
+|projectId |str | 分组所属清单的ID|
+| name| str|分组名 |
+|createdTime |str | 创建时间（UTC时间）|
+| modifiedTime|str | 修改时间（UTC时间）|
+
+
+注：
+- 未出现的字段暂时作用未知
---
+++ packages/nonebot-plugin-dida/nonebot_plugin_dida-2.0.0.1.tar.gz/nonebot_plugin_dida-2.0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys
+import os
+
+setup(
+    name="nonebot_plugin_dida",
+    version="2.0.0.1",
+    author="TDK",
+    author_email="tdk1969@foxmail.com",
+    description="基于Nonebot的滴答清单插件,可创建普通任务/子任务,按照条件查询任务",
+    long_description=open("README.rst", "r").read(),
+    include_package_data=True,
+    license="GNU",
+    url="https://github.com/TDK1969/nonebot_plugin_dida",
+    packages=['nonebot_plugin_dida'],
+    install_requires=[
+        "requests",
+        "nonebot_plugin_apscheduler",
+    ],
+    keywords='nonebot',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+
+)
