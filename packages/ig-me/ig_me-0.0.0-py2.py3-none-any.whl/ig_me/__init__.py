21359effe3412de2256a4517936984abb4a02505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:19 +0000

    Add package ig-me/0.0.0/ig_me-0.0.0-py2.py3-none-any.whl

---
+++ packages/ig-me/ig_me-0.0.0-py2.py3-none-any.whl/ig_me/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+__title__ = "ig_me"
+__author__ = "Leandro César Cassimiro"
+__email__ = "ccleandroc@gmail.com"
+__version__ = "0.0.0"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2022-present, Leandro César Cassimiro"
