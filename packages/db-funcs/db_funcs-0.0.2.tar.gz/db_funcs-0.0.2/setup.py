c616667fd7824d2a91b1358ed14daaa0e9d944e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:53 +0000

    Add package db-funcs/0.0.2/db_funcs-0.0.2.tar.gz

---
+++ packages/db-funcs/db_funcs-0.0.2.tar.gz/db_funcs-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='db_funcs',
+      version='0.0.2',
+      description='Databricks common functions, written by TrueMetrics.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://www.truemetrics.cn/',
+      author='TSO',
+      author_email='tlsong@truemetrics.cn',
+      packages=find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3.9",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      )
