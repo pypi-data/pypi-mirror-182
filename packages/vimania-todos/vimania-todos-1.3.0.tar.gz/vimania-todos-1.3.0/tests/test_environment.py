9c3f52ca78fec909f94868aea7a1a9a2123b1034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:17 +0000

    Add package vimania-todos/1.3.0/vimania-todos-1.3.0.tar.gz

---
+++ packages/vimania-todos/vimania-todos-1.3.0.tar.gz/vimania-todos-1.3.0/tests/test_environment.py
@@ -0,0 +1,17 @@
+import os
+
+from vimania_todos.environment import config
+
+
+class TestTwbmIntegration:
+    def test_twbm_not_installed(self, mocker):
+        mocker.patch("vimania_todos.environment.config.twbm_db_url", new=None)
+        print(config.dbfile_twbm)
+        assert config.dbfile_twbm is None
+        assert not config.is_installed_twbm
+
+    def test_twbm_installed(self, mocker):
+        mocker.patch("vimania_todos.environment.config.twbm_db_url", new="something")
+        print(config.dbfile_twbm)
+        assert config.dbfile_twbm == "something"
+        assert config.is_installed_twbm
