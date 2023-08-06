90fe85f5491d88597d7049e0bfc56e58da8cf748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:26 +0000

    Add package FormatFuncs/0.0.2/FormatFuncs-0.0.2.tar.gz

---
+++ packages/FormatFuncs/FormatFuncs-0.0.2.tar.gz/FormatFuncs-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='FormatFuncs',
+  version='0.0.2',
+  description='Functions to format strings',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='William Rivera - Captain William',
+  author_email='william.r.2048@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords= ['string', 'format', 'paragraph', 'line break'],  
+  packages=find_packages(),
+)
