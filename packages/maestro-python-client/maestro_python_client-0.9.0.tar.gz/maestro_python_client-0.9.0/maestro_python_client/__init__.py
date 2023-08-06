c7fb54d0661847f7cbd4c5ee8c5cc590d45558ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:07 +0000

    Add package maestro-python-client/0.9.0/maestro_python_client-0.9.0.tar.gz

---
+++ packages/maestro-python-client/maestro_python_client-0.9.0.tar.gz/maestro_python_client-0.9.0/maestro_python_client/__init__.py
@@ -0,0 +1,8 @@
+"""
+Maestro client implemented in python
+"""
+
+__version__ = "0.9.0"
+from maestro_python_client import CachedClient, Client
+from maestro_python_client.Cache.Cache import Cache
+from maestro_python_client.Cache.RedisCache import RedisCache
