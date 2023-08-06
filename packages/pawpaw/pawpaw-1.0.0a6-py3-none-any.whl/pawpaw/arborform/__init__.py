59759e25a7f59bdb17e7b71713005b4146ae8cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:20 +0000

    Add package pawpaw/1.0.0a6/pawpaw-1.0.0a6-py3-none-any.whl

---
+++ packages/pawpaw/pawpaw-1.0.0a6-py3-none-any.whl/pawpaw/arborform/__init__.py
@@ -0,0 +1,4 @@
+from .itorator import Itorator, Wrap, Reflect, Desc, Group, Split, Extract
+del itorator
+
+import pawpaw.arborform.postorator
---
+++ packages/pawpaw/pawpaw-1.0.0a6-py3-none-any.whl/pawpaw/visualization/__init__.py
@@ -0,0 +1,6 @@
+import pawpaw.visualization.sgr
+
+from .highlighter import Highlighter
+del highlighter
+
+import pawpaw.visualization.pepo
