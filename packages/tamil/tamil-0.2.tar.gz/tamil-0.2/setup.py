36ba8fee2039f0e319e85f0d2a36eaa61a876749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:43 +0000

    Add package tamil/0.2/tamil-0.2.tar.gz

---
+++ packages/tamil/tamil-0.2.tar.gz/tamil-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tamil',
+    version='0.2',
+    license='MIT',
+    author="Karpi",
+    author_email='karpiworld@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/karpigit/ravi-publish-pypi',
+    keywords='tamil',
+    install_requires=[
+      ],
+)
