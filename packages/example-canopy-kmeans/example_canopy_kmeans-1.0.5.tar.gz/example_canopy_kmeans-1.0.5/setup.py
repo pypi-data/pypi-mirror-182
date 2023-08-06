0c4cf759980d3fdadd39dad1c200702cc2a7ff8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:44 +0000

    Add package example-canopy-kmeans/1.0.5/example_canopy_kmeans-1.0.5.tar.gz

---
+++ packages/example-canopy-kmeans/example_canopy_kmeans-1.0.5.tar.gz/example_canopy_kmeans-1.0.5/setup.py
@@ -0,0 +1,18 @@
+import  setuptools
+
+
+setuptools.setup(
+    name="example_canopy_kmeans",
+    version="1.0.5",
+    author="YR",
+    author_email="395622703@qq.com",
+    description="聚类库",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
