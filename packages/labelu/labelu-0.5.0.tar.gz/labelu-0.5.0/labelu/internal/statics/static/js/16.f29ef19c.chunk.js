85490f2b35d3bda9db90da01dedeb68687bd2f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:00 +0000

    Add package labelu/0.5.0/labelu-0.5.0.tar.gz

---
+++ packages/labelu/labelu-0.5.0.tar.gz/labelu-0.5.0/labelu/internal/statics/static/js/16.f29ef19c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpweb=this.webpackJsonpweb||[]).push([[16],{971:function(t,s,e){"use strict";e.r(s),s.default=e.p+"static/media/tagImg.b40687d3.png"}}]);
+//# sourceMappingURL=16.f29ef19c.chunk.js.map
