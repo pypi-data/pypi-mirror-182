ab09f63fa64b4ffb3a041eff40af5441e4d6fb28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:33:27 +0000

    Add package Quelert2/2.0.0/Quelert2-2.0.0.tar.gz

---
+++ packages/Quelert2/Quelert2-2.0.0.tar.gz/Quelert2-2.0.0/Q/__init__.py
@@ -0,0 +1,87 @@
+# Name -> Quelert
+# Version -> Quelert1 [2.0.0]
+
+from tkinter import messagebox
+from tkinter import filedialog
+from datetime import datetime
+from PIL import Image
+
+import Stopwatch_ru
+import Stopwatch_en
+import Timer
+
+import webbrowser
+import datetime
+import calendar
+import qrcode
+import wget
+import rich
+import time
+import os
+
+
+class Quelert:
+    Version = "v2.0.0"
+    ReleaseDate = "22.12.2022"
+
+class QlBrowser:
+    NamesLib = ["Webbrowser", "Wget"]
+
+    def open(url):
+        webbrowser.open(url)
+
+    def download(url, pathsave):
+        wget.download(url, pathsave)
+
+class QlDateTime:
+    NamesLib = ["Datetime", "Calendar"]
+    Date = datetime.date.today()
+
+    def Time(self):
+        date_time = datetime.datetime.now()
+        current = date_time.time()
+        print(current)
+
+    def Calendar(Year, Month):
+        november = calendar.month(Year, Month)
+        print(november)
+
+class QlWidgets:
+    NamesLib = ["Tkinter"]
+    Stopwatch_Lang = ["ru", "en"]
+
+    def ERROR(title, message):
+        messagebox.showerror(title, message)
+
+    def WARNING(title, message):
+        messagebox.showwarning(title, message)
+
+    def INFO(title, message):
+        messagebox.showinfo(title, message)
+
+    def OpenDirectory():
+        filedialog.askdirectory()
+
+    def OpenNameFile():
+        filedialog.askopenfilename()
+
+    def Stopwatch(lang):
+        if lang == "ru":
+            Stopwatch_ru.stopwatch()
+
+        elif lang == "en":
+            Stopwatch_en.stopwatch()
+
+    def Timer(lang):
+        if lang == "ru":
+            Timer.ru()
+
+        elif lang == "en":
+            Timer.en()
+
+    def QrCode(text, namefile):
+        img = qrcode.make(text)
+        img.save(namefile)
+
+    def ColorMania():
+        os.system("CM\\ColorMania.exe")
---
+++ packages/Quelert2/Quelert2-2.0.0.tar.gz/Quelert2-2.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='Quelert2',
+	version='2.0.0',
+	author='Georgy2008',
+	author_email='zadvornow2908@gmail.com',
+	description='Quelert2.0.0',
+	packages=['Q'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
