91e55b5701eb82f625c156a33b13cbab02ee0d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:35:09 +0000

    Add package Quelert2/2.0.1/Quelert2-2.0.1.tar.gz

---
+++ packages/Quelert2/Quelert2-2.0.1.tar.gz/Quelert2-2.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='Quelert2',
+	version='2.0.1',
+	author='Georgy2008',
+	author_email='zadvornow2908@gmail.com',
+	description='Ql2',
+	packages=['q'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
