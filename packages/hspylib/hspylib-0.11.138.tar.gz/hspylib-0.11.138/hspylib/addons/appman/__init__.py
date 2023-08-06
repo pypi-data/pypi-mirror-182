9d2e4d5b03d339c9487fed99943533f6690b953d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:12 +0000

    Add package hspylib/0.11.138/hspylib-0.11.138.tar.gz

---
+++ packages/hspylib/hspylib-0.11.138.tar.gz/hspylib-0.11.138/hspylib/addons/appman/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.138
+#
+# Package: main.hspylib.addons.appman
+"""Package initialization."""
+
+__all__ = [
+  'appman', 
+  'appman_enums'
+]
+__version__ = '0.11.138'
