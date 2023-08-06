09de2428c94530be089ceec53c6167184db77a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:31 +0000

    Add package hspylib/0.11.149/hspylib-0.11.149.tar.gz

---
+++ packages/hspylib/hspylib-0.11.149.tar.gz/hspylib-0.11.149/hspylib/modules/cli/vt100/__init__.py
@@ -0,0 +1,15 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.149
+#
+# Package: main.hspylib.modules.cli.vt100
+"""Package initialization."""
+
+__all__ = [
+  'terminal', 
+  'vt_100', 
+  'vt_code', 
+  'vt_color', 
+  'vt_utils'
+]
+__version__ = '0.11.149'
