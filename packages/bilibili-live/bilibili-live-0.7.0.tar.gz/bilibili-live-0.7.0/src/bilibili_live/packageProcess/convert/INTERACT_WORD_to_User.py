dc64ce9c7e2f98a2e07c3af31601c6be084fb3c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:09 +0000

    Add package bilibili-live/0.7.0/bilibili-live-0.7.0.tar.gz

---
+++ packages/bilibili-live/bilibili-live-0.7.0.tar.gz/bilibili-live-0.7.0/src/bilibili_live/packageProcess/convert/INTERACT_WORD_to_User.py
@@ -0,0 +1,14 @@
+from ...events.eventData import FansMedal, User
+
+
+def convert(data):
+    return User(
+        uid=data.get("uid"),
+        uname=data.get("uname"),
+        fans_medal=FansMedal(
+            name=data.get("fans_medal").get("medal_name"), level=data.get("fans_medal").get("medal_level")
+        )
+        if data.get("medal_info")
+        else None,
+        guard_level=0,
+    ), data.get("timestamp")
