6e5a144d595bf3d1971e1bd89b5d4db094eafcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:42 +0000

    Add package client_chat_pyqt_september1/0.2/client_chat_pyqt_september1-0.2.tar.gz

---
+++ packages/client_chat_pyqt_september1/client_chat_pyqt_september1-0.2.tar.gz/client_chat_pyqt_september1-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='client_chat_pyqt_september1',
+      version='0.2',
+      description='Client packet',
+      packages=find_packages(),  # ,Будем искать пакеты тут(включаем авто поиск пакетов)
+      author_email='test@mail.ru',
+      author='ALexPy1',
+      install_requeres=['PyQt5', 'sqlalchemy', 'pycruptodome', 'pycryptodomex']
+      ##зависимости которые нужно до установить
+      )
+
