9bbd199f6fcaed613d4c143f699cb19a2b6238cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:43 +0000

    Add package MDS-big-advantrue/0.1/MDS_big_advantrue-0.1.tar.gz

---
+++ packages/MDS-big-advantrue/MDS_big_advantrue-0.1.tar.gz/MDS_big_advantrue-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+  
+setup(
+    name="MDS_big_advantrue", 
+    version="0.1",
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description="A small example package",
+    url='https://github.com/chen-yuhong/DATA533-step3.git',
+    author="Henry(Yiheng) Luo; Yuhong Chen",    
+    author_email="henryluo@student.ubc.ca"
+)
