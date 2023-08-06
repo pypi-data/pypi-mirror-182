d0951072160ba6d55f8b2d119f3efedcb36d89a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:13 +0000

    Add package powerml-app/0.0.18/powerml_app-0.0.18-py3-none-any.whl

---
+++ packages/powerml-app/powerml_app-0.0.18-py3-none-any.whl/powerml/decorator/powerml_filter.py
@@ -0,0 +1,25 @@
+import functools
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def powerml_filter(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print("\nRunning Filter: " + func.__name__)
+        # Load Data to be filtered
+        data_to_filter = kwargs['data']
+        passing_data = []
+        # pass data into filter function
+        for datapoint in data_to_filter:
+            result = func(datapoint["model_output"])
+            if result:
+                passing_data.append(datapoint)
+        print("Cases: %d" %
+              (len(data_to_filter)))
+        print("Result: %d%%" %
+              (100*len(passing_data)/len(data_to_filter)))
+        return passing_data
+    wrapper.is_filter_function = True
+    return wrapper
