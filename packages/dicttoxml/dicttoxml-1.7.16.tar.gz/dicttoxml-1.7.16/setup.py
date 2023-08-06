74bfea46dbdf528491a8af0a17a140381df8099e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:50 +0000

    Add package dicttoxml/1.7.16/dicttoxml-1.7.16.tar.gz

---
+++ packages/dicttoxml/dicttoxml-1.7.16.tar.gz/dicttoxml-1.7.16/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+version = '1.7.16'
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name = 'dicttoxml',
+    version = version,
+    description = 'Converts a Python dictionary or other native data type into a valid XML string. ',
+    long_description = long_description,
+    author = 'Ryan McGreal',
+    author_email = 'ryan@quandyfactory.com',
+    license = 'LICENCE.txt',
+    url = 'https://github.com/quandyfactory/dicttoxml',
+    py_modules = ['dicttoxml'],
+    platforms='Cross-platform',
+    python_requires=">=3.6",
+)
