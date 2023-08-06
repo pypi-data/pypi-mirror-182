b9703cbfb06ab98a6a43dec30a5051e7008cd497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:07 +0000

    Add package ziion/0.0.7/ziion-0.0.7.tar.gz

---
+++ packages/ziion/ziion-0.0.7.tar.gz/ziion-0.0.7/ziion_cli/__init__.py
@@ -0,0 +1,9 @@
+import os
+from ziion_cli.constants import (
+    SOLC_SELECT_DIR,
+    SOLC_ARTIFACTS_DIR,
+)
+
+if not os.path.exists(SOLC_SELECT_DIR):
+    os.mkdir(path=SOLC_SELECT_DIR)
+    os.mkdir(path=SOLC_ARTIFACTS_DIR)
