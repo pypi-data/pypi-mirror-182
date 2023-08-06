797c0b758c60bd79f9717ff580bc73889d9f7e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:00 +0000

    Add package pyroll-freiberg-flow-stress/2.0.0a1.post1/pyroll_freiberg_flow_stress-2.0.0a1.post1.tar.gz

---
+++ packages/pyroll-freiberg-flow-stress/pyroll_freiberg_flow_stress-2.0.0a1.post1.tar.gz/pyroll_freiberg_flow_stress-2.0.0a1.post1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyroll', 'pyroll.freiberg_flow_stress']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyroll-core>=2.0.0a,<3.0.0']
+
+setup_kwargs = {
+    'name': 'pyroll-freiberg-flow-stress',
+    'version': '2.0.0a1.post1',
+    'description': 'Plugin for PyRoll providing Freiberg flow stress approach and material database.',
+    'long_description': '# PyRoll Freiberg Flow Stress Model\n\nPlugin for PyRoll providing Freiberg flow stress approach and a material database.\n\nFor the docs, see [here](docs/index.md).\n\nThis project is licensed under the [BSD-3-Clause license](LICENSE).\n\nThe package is available via PyPi and can be installed with\n\n    pip install pyroll-freiberg-flow-stress',
+    'author': 'Max Weiner',
+    'author_email': 'max.weiner@imf.tu-freiberg.de',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
