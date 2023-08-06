3ed6e6b4d1fc1dfb8499b578831b88d41ed43ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:47 +0000

    Add package plot4gmns/0.1.0/plot4gmns-0.1.0.tar.gz

---
+++ packages/plot4gmns/plot4gmns-0.1.0.tar.gz/plot4gmns-0.1.0/plot4gmns/__init__.py
@@ -0,0 +1,9 @@
+
+from ._model import *
+
+__version__ = '0.1.0'
+__author__ = (
+    'Dr. Junhua Chen: cjh@bjtu.edu.cn'
+    'Zanyang Cui: zanyangcui@outlook.com'
+    'Xiangyong Luo: luoxiangyong01@gmail.com'
+)
