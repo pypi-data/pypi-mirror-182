961daf45c328666f6d2a879c4df91f39e0d79ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:15 +0000

    Add package zpp_menu/1.2.2/zpp_menu-1.2.2-py3-none-any.whl

---
+++ packages/zpp_menu/zpp_menu-1.2.2-py3-none-any.whl/zpp_menu/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.2.2'
+__author__ = 'ZephyrOff  (Alexandre Pajak)'
+__descriptor__ = 'Générateur de menu à touches fléchées'
+__lastupdate__ = '23/12/2022'
+__releasenote__ = 'Patch size of menu element'
+
+from .menu import *
