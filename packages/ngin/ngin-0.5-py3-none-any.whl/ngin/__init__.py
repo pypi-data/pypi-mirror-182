cc5343659071118e3a4ff3b29054dc73bd0163d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:56 +0000

    Add package ngin/0.5/ngin-0.5-py3-none-any.whl

---
+++ packages/ngin/ngin-0.5-py3-none-any.whl/ngin/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+from ngin.command_pb2 import Head, CStageInfo, JoystickDirectionals, ActionEvent, CmdInfo, CObject, CVisible, CPhysical, CAction, CActionType, BodyShape, BodyType, Cmd
+from ngin.ngin import Nx, EventHandler, CObjectInfo
+from ngin.plane import demo
