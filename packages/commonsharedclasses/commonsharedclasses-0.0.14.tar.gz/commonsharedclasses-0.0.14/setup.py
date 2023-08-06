936c6bb86a3037f52744fc1ae032ff7913a1018f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:53 +0000

    Add package commonsharedclasses/0.0.14/commonsharedclasses-0.0.14.tar.gz

---
+++ packages/commonsharedclasses/commonsharedclasses-0.0.14.tar.gz/commonsharedclasses-0.0.14/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='commonsharedclasses',
+    version='0.0.14',
+    description='This package will contain all common classes, version 0.0.14 updates:- Unity Catalog support for Tenant Lakehouse SQL',
+    author='Ankit Kumar',
+    packages=['commonsharedclasses'],
+    zip_safe=False
+)
