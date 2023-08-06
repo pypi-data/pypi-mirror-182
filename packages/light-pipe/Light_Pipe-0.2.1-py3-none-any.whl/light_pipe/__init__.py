867b658b615cfed462c4191e4b24e2f849d17f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:16 +0000

    Add package light-pipe/0.2.1/Light_Pipe-0.2.1-py3-none-any.whl

---
+++ packages/light-pipe/Light_Pipe-0.2.1-py3-none-any.whl/light_pipe/__init__.py
@@ -0,0 +1,26 @@
+__author__ = "Richard Correro (rcorrero@gmail.com)"
+
+
+from .data import *
+from .parallelizer import *
+from .transformer import *
+
+__doc__ = """
+# [Light-Pipe](https://github.com/rcorrero/light-pipe)
+
+---
+
+## Overview
+
+[Light-Pipe](https://www.light-pipe.io/) is an extensible, light-weight Python framework for data pipelines that scale. It provides a set of intuitive abstractions designed to decouple pipeline implementation from the operations performed within the pipeline. It is designed to scale effortlessly, being built from the ground-up to support concurrency in all its forms, and it has zero non-standard dependencies. Light-Pipe is released under a [BSD-3-Clause License](https://opensource.org/licenses/BSD-3-Clause).
+
+## More Information
+
+- [GitHub](https://github.com/rcorrero/light-pipe)
+
+- [Documentation](https://www.light-pipe.io/)
+
+---
+
+Copyright 2020-2023 [Richard Correro](https://www.richardcorrero.com/).
+"""
