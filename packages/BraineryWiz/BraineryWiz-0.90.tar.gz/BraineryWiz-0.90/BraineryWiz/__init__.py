027f8a22ed11e7ab4543b3034e64691b61a0ec52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:03 +0000

    Add package BraineryWiz/0.90/BraineryWiz-0.90.tar.gz

---
+++ packages/BraineryWiz/BraineryWiz-0.90.tar.gz/BraineryWiz-0.90/BraineryWiz/__init__.py
@@ -0,0 +1,28 @@
+# /* ****************************************************************** **
+# **                    BraineryWiz TCL version                         **
+# **              by Bijan Sayyafzadeh & Silvia Mazzoni                 **
+# **                                                                    **
+# **                                                                    **
+# ** (C) Copyright 2022, Bijan Sayyafzadeh & Silvia Mazzoni             **
+# **       SilviaBrainery.com                                           **
+# ** All Rights Reserved.                                               **
+# **                                                                    **
+# ** Commercial use of this program without express permission of the   **
+# ** https://www.silviasbrainery.com/ (BraineryWiz@Gmail.com), is       **
+# ** strictly prohibited.  See file 'COPYRIGHT'  in main directory      **
+# ** for information on usage and redistribution,  and for a            **
+# ** DISCLAIMER OF ALL WARRANTIES.                                      **
+# **                                                                    **
+# ** Developed by:                                                      **
+# **   Bijan SayyafZadeh (B.Sayyaf@yahoo.com)                           **
+# **   SilviaMazzoni     (SilviaMazzoni@Yahoo.com)                      **
+# **                                                                    **
+# ** ****************************************************************** */
+
+from .PlotModel import PlotModel
+from .PlotAnimation import RecorderReset, Record, PlotAnime, PlotAnimeGif
+from .PlotDefo import PlotDefo
+from .PlotModeShape import PlotModeShape
+from .RealTime import RealTimeObj, RealTimeUpdate
+from .FEMWiz import BaseData, DispData, EigenData
+
