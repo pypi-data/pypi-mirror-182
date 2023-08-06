0dacf2b964e4829d8c9474e2ec2b0c5f0598a369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:25 +0000

    Add package ipyslides/2.2.8/ipyslides-2.2.8.tar.gz

---
+++ packages/ipyslides/ipyslides-2.2.8.tar.gz/ipyslides-2.2.8/ipyslides/__init__.py
@@ -0,0 +1,11 @@
+from .__version__ import __version__
+
+version = __version__
+
+from .core import Slides
+
+__all__ = ['Slides']
+
+if __name__ == '__main__':
+    print('Use this package in Jupyter notebook!')
+    
