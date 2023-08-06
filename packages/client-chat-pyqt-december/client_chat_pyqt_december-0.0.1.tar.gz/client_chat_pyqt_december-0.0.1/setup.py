c9d899bd5c6c67823009472936efa3cfe0bb1e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:10 +0000

    Add package client-chat-pyqt-december/0.0.1/client_chat_pyqt_december-0.0.1.tar.gz

---
+++ packages/client-chat-pyqt-december/client_chat_pyqt_december-0.0.1.tar.gz/client_chat_pyqt_december-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='client_chat_pyqt_december',
+      version='0.0.1',
+      description='Client packet',
+      packages=find_packages(),
+      author_email='007mail.ru96@mail.ru',
+      author='Magomedov Magomed',
+      install_requeres=['PyQt5', 'sqlalchemy', 'pycruptodome', 'pycryptodomex']
+      )
