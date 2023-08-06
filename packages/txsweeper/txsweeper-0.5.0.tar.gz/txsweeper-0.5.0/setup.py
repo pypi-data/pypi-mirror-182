d22ef66fb1369ebd459bf80eec2818d19f666ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:50 +0000

    Add package txsweeper/0.5.0/txsweeper-0.5.0.tar.gz

---
+++ packages/txsweeper/txsweeper-0.5.0.tar.gz/txsweeper-0.5.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['txsweeper']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['textual==0.5.0']
+
+entry_points = \
+{'console_scripts': ['txSweeper = txsweeper.main:main']}
+
+setup_kwargs = {
+    'name': 'txsweeper',
+    'version': '0.5.0',
+    'description': 'An infinitely generative minesweeper game.',
+    'long_description': None,
+    'author': 'k01e',
+    'author_email': 'k01e.alm07@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '==3.10.1',
+}
+
+
+setup(**setup_kwargs)
