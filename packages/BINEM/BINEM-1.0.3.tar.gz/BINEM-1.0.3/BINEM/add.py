d1761bf6415c71d917c2b4398c7598c2bfa340f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:37 +0000

    Add package BINEM/1.0.3/BINEM-1.0.3.tar.gz

---
+++ packages/BINEM/BINEM-1.0.3.tar.gz/BINEM-1.0.3/BINEM/add.py
@@ -0,0 +1,12 @@
+import sys
+
+from .sum import sum
+
+# from def_function import * #导入该文件里的所有函数
+
+# 调用函数
+def main():
+    sum()
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ packages/BINEM/BINEM-1.0.3.tar.gz/BINEM-1.0.3/setup.py
@@ -0,0 +1,41 @@
+import codecs
+import os
+import sys
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+NAME = "BINEM"
+PACKAGES = ['BINEM']
+DESCRIPTION = "this is a data for package by myself upload to pypi"
+LONG_DESCRIPTION = "this is a data for package by myself upload to pypi"
+KEYWORDS = "keyword"
+AUTHOR = "renjunyi"
+AUTHOR_EMAIL = "2998345156@qq.com"
+URL = "https://github.com/lichanghong/wenyali.git"
+VERSION = "1.0.3"
+LICENSE = "MIT"
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords = KEYWORDS,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    license = LICENSE,
+    packages = PACKAGES,
+    include_package_data=True,
+    zip_safe=True,
+)
