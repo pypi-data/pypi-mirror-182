16eb0ec6aee86fdb71473764c730037785fbaecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:48 +0000

    Add package keyframed/0.0.3b0/keyframed-0.0.3b0-py3-none-any.whl

---
+++ packages/keyframed/keyframed-0.0.3b0-py3-none-any.whl/keyframed/__init__.py
@@ -0,0 +1,10 @@
+from .core import Keyframed
+from .decorators import frameContext
+from .utils import to_keyframed
+
+__all__ = [
+    'Keyframed',
+    'KeyframedBase',
+    'frameContext',
+    'to_keyframed',
+]
