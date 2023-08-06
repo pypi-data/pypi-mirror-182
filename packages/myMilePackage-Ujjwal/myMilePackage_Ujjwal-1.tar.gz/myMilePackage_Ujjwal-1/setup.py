9cd9078cb34a3e79a7945676eba8cee995a63e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:18 +0000

    Add package myMilePackage-Ujjwal/1/myMilePackage_Ujjwal-1.tar.gz

---
+++ packages/myMilePackage-Ujjwal/myMilePackage_Ujjwal-1.tar.gz/myMilePackage_Ujjwal-1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myMilePackage_Ujjwal',
+    version='1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A test python package',
+	url='https://github.com/khalad-hasan/myMileConverter',
+    author='Ujjwal Upadhyay',
+    author_email='upadhyay.ujjwal007@gmail.com'
+)
