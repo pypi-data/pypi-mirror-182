982200256e35ba4cc60a1451a19b6d2dbd6703f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:55 +0000

    Add package towncrier/22.12.0/towncrier-22.12.0.tar.gz

---
+++ packages/towncrier/towncrier-22.12.0.tar.gz/towncrier-22.12.0/src/towncrier/_version.py
@@ -0,0 +1,12 @@
+"""
+Provides towncrier version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update towncrier` to change this file.
+
+from incremental import Version
+
+
+__version__ = Version("towncrier", 22, 12, 0)
+__all__ = ["__version__"]
