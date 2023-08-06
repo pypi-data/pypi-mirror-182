f42fb182ffb5de7eef93967bb47870ca0311048b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:07 +0000

    Add package pyiron-contrib/0.1.6/pyiron_contrib-0.1.6.tar.gz

---
+++ packages/pyiron-contrib/pyiron_contrib-0.1.6.tar.gz/pyiron_contrib-0.1.6/setup.py
@@ -0,0 +1,59 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+
+setup(
+    name='pyiron_contrib',
+    version=versioneer.get_version(),
+    description='Repository for user-generated plugins to the pyiron IDE.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_contrib',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='huber@mpie.de',
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*"]),
+    install_requires=[        
+        'matplotlib>=3.6.2',
+        'numpy>=1.24.0',
+        'pyiron_base>=0.5.30',
+        'scipy>=1.9.3',
+        'seaborn>=0.12.1',
+        'pyparsing>=3.0.9'
+    ],
+    extras_require={
+        'atomistic': [
+            'ase>=3.22.1',
+            'pyiron_atomistics>=0.2.61',
+            'pycp2k>=0.2.2',
+        ],
+        'fenics': [
+            'fenics>=2019.1.0',
+            'mshr>=2019.1.0',
+        ],
+        'image': ['scikit-image>=0.19.3'],
+        'generic': [
+            'boto3>=1.26.32', 
+            'moto>=4.0.12'
+        ],
+    },
+    cmdclass=versioneer.get_cmdclass(),
+    
+)
