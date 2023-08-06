ec36699a82ef6f128d6bdf36b880430ed03567ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:39 +0000

    Add package nimsameed/0.8/nimsameed-0.8.tar.gz

---
+++ packages/nimsameed/nimsameed-0.8.tar.gz/nimsameed-0.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name="nimsameed",
+        version="0.8",
+        description="The game built on Reinforcement Q-learning. The agent in the game always try to win.",
+        author="Saad and Sameed",
+        packages=["nimsameed"],
+        zip_safe=False,
+    )
