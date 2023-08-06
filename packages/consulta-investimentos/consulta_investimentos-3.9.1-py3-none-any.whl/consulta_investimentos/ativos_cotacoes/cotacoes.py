ef8db18adb6d3ce4db27f6ae5483519e8ca22e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:41 +0000

    Add package consulta-investimentos/3.9.1/consulta_investimentos-3.9.1-py3-none-any.whl

---
+++ packages/consulta-investimentos/consulta_investimentos-3.9.1-py3-none-any.whl/consulta_investimentos/ativos_cotacoes/cotacoes.py
@@ -0,0 +1,24 @@
+"""Cotações da lista de empresas.
+
+Cotação dosa listados no site do Yahoo Finance.
+Pode ser ações, FII, stocks, Reits, títulos. etc.
+"""
+import locale
+import yfinance as yf
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+
+def consulta_cotacoes(empresas):
+
+    empresas_string = ''
+    for empresa in empresas:
+        empresas_string = empresas_string + empresa.lower() + ' '
+
+    tickers = yf.Tickers(empresas_string)
+
+    close = tickers.history(period="1mo")['Close']
+    dividendos = tickers.history(period="1mo")['Dividends']
+    splits = tickers.history(period="1mo")['Stock Splits']
+
+    return close, dividendos, splits
