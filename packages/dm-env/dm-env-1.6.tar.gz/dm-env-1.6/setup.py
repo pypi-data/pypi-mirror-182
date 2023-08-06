896eefa02b008a37af94d3e71c54ca9c6b573062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:49 +0000

    Add package dm-env/1.6/dm-env-1.6.tar.gz

---
+++ packages/dm-env/dm-env-1.6.tar.gz/dm-env-1.6/setup.py
@@ -0,0 +1,62 @@
+# pylint: disable=g-bad-file-header
+# Copyright 2019 The dm_env Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""Install script for setuptools."""
+
+from importlib import util
+from setuptools import find_packages
+from setuptools import setup
+
+
+def get_version():
+  spec = util.spec_from_file_location('_metadata', 'dm_env/_metadata.py')
+  mod = util.module_from_spec(spec)
+  spec.loader.exec_module(mod)
+  return mod.__version__
+
+
+setup(
+    name='dm-env',
+    version=get_version(),
+    description='A Python interface for Reinforcement Learning environments.',
+    author='DeepMind',
+    license='Apache License, Version 2.0',
+    keywords='reinforcement-learning python machine learning',
+    packages=find_packages(exclude=['examples']),
+    python_requires='>=3.7',
+    install_requires=[
+        'absl-py',
+        'dm-tree',
+        'numpy',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
