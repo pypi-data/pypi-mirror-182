e2a70be7ca898b8c4396229ef57c04446870cff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:34 +0000

    Add package arrow-sdk/1.0.16/arrow-sdk-1.0.16.tar.gz

---
+++ packages/arrow-sdk/arrow-sdk-1.0.16.tar.gz/arrow-sdk-1.0.16/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arrow-sdk',
+    version='1.0.16',
+    author='Arrow Markets',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    #py_modules=['arrow_sdk', 'utilities', 'constants'],
+    include_package_data=True,
+    package_data={'arrow_sdk': ['abis/*.json', 'abis/v4/*.json', 'abis/competition/*.json']},
+    install_requires=[
+        'web3',
+        'eth_utils',
+        'eth_account',
+        'requests',
+        'pytz'
+    ],
+)
