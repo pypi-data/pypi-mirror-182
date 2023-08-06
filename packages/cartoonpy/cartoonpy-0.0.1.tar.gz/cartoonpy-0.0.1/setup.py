c47b4ae75ae69ed8697fa6b2e43ced08077f176c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:36 +0000

    Add package cartoonpy/0.0.1/cartoonpy-0.0.1.tar.gz

---
+++ packages/cartoonpy/cartoonpy-0.0.1.tar.gz/cartoonpy-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="cartoonpy",
+    version="0.0.1",
+    author="Levin Hinder",
+    description="A library to convert images and videos into cartoons.",
+    packages=[]
+)
