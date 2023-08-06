a13549c3a5c88c700c6711d16f625becb45b8ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:13 +0000

    Add package devoud/1.0b2/devoud-1.0b2.tar.gz

---
+++ packages/devoud/devoud-1.0b2.tar.gz/devoud-1.0b2/devoud/lib/devoud_data.py
@@ -0,0 +1,33 @@
+from PySide6.QtCore import QUrl
+from PySide6 import QtCore
+from PySide6.QtCore import Qt, QUrl, QSize
+from PySide6.QtGui import *
+from PySide6.QtWebEngineCore import *
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWidgets import *
+from .filesystem import FileSystem
+import json
+import time
+
+program_name = 'Devoud'
+__version__ = '1.0b2'
+
+FS = FileSystem()
+path = FileSystem.path
+
+search_engines = {'Yandex': ('https://yandex.ru/search/?text=', 'https://ya.ru/', './ui/svg/yandex.svg'),
+                  'Google': (
+                      'https://www.google.com/search?q=', 'https://www.google.com/', './ui/svg/google.svg'),
+                  'StartPage': (
+                      'https://www.startpage.com/sp/search?query=', 'https://www.startpage.com/?t=dark',
+                      './ui/svg/startpage.svg'
+                  ),
+                  'DDGo': (
+                      'https://duckduckgo.com/?q=', 'https://duckduckgo.com/', './ui/svg/duckduckgo.svg')}
+
+new_page_dict = {'Заставка с часами': lambda: 'https://web.tabliss.io/',
+                 'Поисковик': lambda: search_engines[FS.get_option('searchEngine')][1],
+                 'Домашняя страница': lambda: QUrl.fromUserInput(FS.get_option('homePage')).toString()}
+
+new_page = new_page_dict.get(FS.get_option('newPage'))()
+systemFrame = FS.get_option('systemWindowFrame')
