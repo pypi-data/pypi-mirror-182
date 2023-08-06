5798c83dfbf4f6bf06e7e833995b15aa5c9fca61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:25 +0000

    Add package gpforecaster/0.3.8/gpforecaster-0.3.8.tar.gz

---
+++ packages/gpforecaster/gpforecaster-0.3.8.tar.gz/gpforecaster-0.3.8/gpforecaster/utils/logger.py
@@ -0,0 +1,31 @@
+import logging
+import os
+import time
+
+
+class Logger:
+    def __init__(self, name, to_file=None, log_level=logging.INFO, log_dir="."):
+        timestamp = time.strftime("%Y%m%d-%H%M%S", time.gmtime())
+
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(log_level)
+
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+        handler = logging.StreamHandler()  # Outputs the log to the console
+        if to_file:
+            log_file = os.path.join(log_dir, "logs", f"log_{timestamp}.txt")
+            handler = logging.FileHandler(log_file)  # Outputs the log to a file
+        handler.setFormatter(formatter)
+
+        self.logger.addHandler(handler)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
