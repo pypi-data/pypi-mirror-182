2787083a504aca45ce8b75700e60fcf5b84a55a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:13 +0000

    Add package nsj-rest-test-util/0.1.2/nsj_rest_test_util-0.1.2-py3-none-any.whl

---
+++ packages/nsj-rest-test-util/nsj_rest_test_util-0.1.2-py3-none-any.whl/util/enum_http_method.py
@@ -0,0 +1,10 @@
+
+from tests.src.entity.enum_custom import EnumCustom
+
+
+class HTTPMethod(EnumCustom):
+    POST = 'POST'
+    PUT = 'PUT'
+    DELETE = 'DELETE'
+    GET = 'GET'
+    PATCH = 'PATCH'
