cb27ff090f532ddd8a126acc0b86615d819be85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:04 +0000

    Add package satip/2.10.37/satip-2.10.37.tar.gz

---
+++ packages/satip/satip-2.10.37.tar.gz/satip-2.10.37/setup.py
@@ -0,0 +1,31 @@
+""" Satip
+
+https://badge.fury.io/py/satip
+
+Satip is a library for SATellite Image Processing (=SATIP), and provides all the
+functionality necessary for retrieving, and storing EUMETSAT data.
+
+For more detailed information, please check the accompanying README.md.
+"""
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+install_requires = (this_directory / "requirements.txt").read_text().splitlines()
+
+setup(
+    name="satip",
+    version="2.10.37",
+    license="MIT",
+    description="""Satip provides the functionality necessary for
+                   retrieving, and storing EUMETSAT data""",
+    author="Jacob Bieker, Ayrton Bourn, Jack Kelly",
+    author_email="info@openclimatefix.org",
+    company="Open Climate Fix Ltd",
+    install_requires=install_requires,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+)
