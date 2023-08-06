998c44bff0d3c2a20bebfc297207e4d17a5a422d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:20 +0000

    Add package pawpaw/1.0.0a6/pawpaw-1.0.0a6.tar.gz

---
+++ packages/pawpaw/pawpaw-1.0.0a6.tar.gz/pawpaw-1.0.0a6/tests/test_itorator.py
@@ -0,0 +1,191 @@
+import regex
+from pawpaw import Ito, Types
+from pawpaw.arborform import Reflect, Wrap
+from tests.util import _TestIto
+
+
+class TestItorator(_TestIto):
+    """Uses Reflect and Wrap classes, which have trivial implementation, to test base class functionality"""
+
+    def test_add_self(self):
+        i = Reflect()
+
+        with self.assertRaises(ValueError):
+            i.itor_children = i
+
+        with self.assertRaises(ValueError):
+            i.itor_next = i
+
+
+    def test_traverse(self):
+        s = 'abc'
+        root = Ito(s)
+        self.add_chars_as_children(root, 'Child')
+
+        reflect = Reflect()
+        rv = [*reflect.traverse(root)]
+            
+        self.assertEqual(1, len(rv))
+        ito = rv[0]
+        self.assertIsNot(root, ito)
+        self.assertEqual(root, ito)
+        self.assertEqual([*root.children], [*ito.children])
+
+    def test_traverse_with_next(self):
+        s = 'abc'
+        root = Ito(s)
+        self.add_chars_as_children(root, 'Child')
+
+        reflect = Reflect()
+        desc = 'x'
+        reflect.itor_next = Wrap(lambda ito: (ito.clone(desc=desc),))
+        rv = [*reflect.traverse(root)]
+            
+        self.assertEqual(1, len(rv))
+        ito = rv[0]
+        self.assertIsNot(root, ito)
+        self.assertEqual(desc, ito.desc)
+        self.assertEqual([*root.children], [*ito.children])
+
+    def test_traverse_with_children(self):
+        s = 'abc'
+        root = Ito(s)
+
+        reflect = Reflect()
+        desc = 'x'
+        reflect.itor_children = Wrap(lambda ito: tuple(ito.clone(i, i+1, desc) for i, c in enumerate(s)))
+        rv = [*reflect.traverse(root)]
+            
+        self.assertEqual(1, len(rv))
+        ito = rv[0]
+        self.assertIsNot(root, ito)
+        self.assertSequenceEqual(s, [str(i) for i in ito.children])
+        self.assertTrue(all(c.desc == desc for c in ito.children))
+        
+    def test_traverse_with_carry_through(self):
+        s = 'abc'
+        root = Ito(s)
+        d_changed = 'changed'
+
+        reflect = Reflect()
+        make_chars = Wrap(lambda ito: tuple(ito.clone(i, i+1, 'char') for i in range(*ito.span)))
+        reflect.itor_children = make_chars
+        rename = Wrap(lambda ito: tuple(ito.clone(desc=d_changed) if i.parent is not None else i for i in [ito]))
+        make_chars.itor_next = rename
+        rv = [*reflect.traverse(root)]
+            
+        self.assertEqual(1, len(rv))
+        ito = rv[0]
+        self.assertIsNot(root, ito)
+        self.assertSequenceEqual(s, [str(i) for i in ito.children])
+        self.assertTrue(all(c.desc == d_changed for c in ito.children))
+
+    def test_traverse_with_post_processor(self):
+        s = 'abc def ghi'
+        root = Ito(s)
+
+        reflect = Reflect()
+
+        word_splitter = Wrap(lambda ito: ito.str_split())
+        reflect.itor_next = word_splitter
+
+        char_splitter = Wrap(lambda ito: [*ito])
+
+        def simple_join(itos: Types.C_IT_ITOS) -> Types.C_IT_BITOS:
+            window = list(itos)
+            yield from (Types.C_BITO(False, i) for i in window)
+            yield Types.C_BITO(True, Ito.join(window))
+
+        with self.subTest(chain_length=1, scenario='itor_next'):
+            rv = [*reflect.traverse(root)]
+            self.assertEqual(3, len(rv))
+
+            word_splitter.postorator = simple_join
+            rv = [*reflect.traverse(root)]
+            self.assertEqual(1, len(rv))
+
+        with self.subTest(chain_length=2, scenario='itor_next'):
+            word_splitter.postorator = None
+            word_splitter.itor_next = char_splitter
+            rv = [*reflect.traverse(root)]
+            self.assertEqual(9, len(rv))
+
+            char_splitter.postorator = simple_join
+            rv = [*reflect.traverse(root)]
+            self.assertEqual(3, len(rv))
+
+            word_splitter.postorator = simple_join
+            rv = [*reflect.traverse(root)]
+            self.assertEqual(1, len(rv))
+
+            char_splitter.postorator = None
+            rv = [*reflect.traverse(root)]
+            self.assertEqual(1, len(rv))
+
+        with self.subTest(chain_length=2, scenario='itor_child'):
+            word_splitter.postorator = None
+            word_splitter.itor_next = None
+            word_splitter.itor_children = char_splitter
+            rv = [*reflect.traverse(root)]
+            self.assertEqual(3, len(rv))
+            self.assertEqual(9, sum(len(i.children) for i in rv))            
+
+            char_splitter.postorator = simple_join
+            rv = [*reflect.traverse(root)]
+            self.assertEqual(3, len(rv))
+            self.assertEqual(3, sum(len(i.children) for i in rv))            
+
+            word_splitter.postorator = simple_join
+            rv = [*reflect.traverse(root)]
+            self.assertEqual(1, len(rv))
+            self.assertEqual(0, len(rv[0].children))  # Ito.join doesn't include children
+
+    def test_traverse_complex(self):
+        basis = 'ABcd123'
+        s = f' {basis} - {basis} '
+        root = Ito(s, desc='root')
+        
+        func = lambda ito: [*ito.split(regex.compile(r'\-'), desc='phrase')]
+        splt_space = Wrap(func)
+        
+        func = lambda ito: [ito.str_strip()]
+        stripper = Wrap(func)
+        splt_space.itor_children = stripper
+        
+        func = lambda ito: [*ito.split(regex.compile(r'(?<=[A-Za-z])(?=\d)'))]
+        splt_alpha_num = Wrap(func)
+        stripper.itor_children = splt_alpha_num
+        
+        func = lambda ito: [ito.clone(desc='numeric' if str(ito).isnumeric() else 'alpha')]
+        namer = Wrap(func)
+        splt_alpha_num.itor_next = namer
+        
+        func = lambda ito: [*ito.split(regex.compile(r'(?<=\d)(?=\d)'), desc='digit')]
+        splt_digits = Wrap(func)
+        
+        func = lambda ito: [*ito.split(regex.compile(r'(?<=[A-Z])(?=[a-z])'), desc='upper or lower')]
+        splt_case = Wrap(func)
+        
+        namer.itor_children = lambda ito: splt_digits if ito.desc == 'numeric' else splt_case
+        
+        func = lambda ito: [ito.clone(i, i + 1, desc='char') for i in range(ito.start, ito.stop)]
+        splt_chars = Wrap(func)
+        splt_case.itor_children = splt_chars
+        
+        root.children.add(*splt_space.traverse(root))
+        
+        expected_child_count = s.count('-') + 1
+        self.assertEqual(expected_child_count, len(root.children))
+        
+        expected_alpha_count = expected_child_count
+        self.assertEqual(expected_alpha_count, sum(1 for i in root.find_all('**[d:alpha]')))
+        
+        expected_leaves_count = len(basis) * 2
+        self.assertEqual(expected_leaves_count, sum(1 for i in root.find_all('***')))
+            
+        depth = 1
+        cur = root
+        while len(cur.children) > 0:
+            cur = cur.children[0]
+            depth += 1
+        self.assertEqual(6, depth)
---
+++ packages/pawpaw/pawpaw-1.0.0a6.tar.gz/pawpaw-1.0.0a6/tinker.py
@@ -0,0 +1,221 @@
+import sys
+# Force Python XML parser, not faster C version so that we can hook methods
+sys.modules['_elementtree'] = None
+import xml.etree.ElementTree as ET
+from dataclasses import dataclass, field
+
+import regex
+import pawpaw
+from pawpaw import Ito
+from pawpaw.visualization import sgr, Highlighter, pepo
+
+
+import xml.etree.ElementTree as ET
+from pawpaw import xml
+text = """<?xml version="1.0"?>
+<music xmlns:mb="http://musicbrainz.org/ns/mmd-1.0#" xmlns="http://mymusic.org/xml/">
+    <?display table-view?>
+    <album genre="R&amp;B" mb:id="123-456-789-0">
+        Robson Jorge &amp; Lincoln Olivetti <!-- 1982, Vinyl -->
+    </album>
+</music>"""
+root = ET.fromstring(text, parser=xml.XmlParser())
+print(f'{type(root)=}')
+print()
+
+from pawpaw.visualization import pepo
+print(pepo.Tree().dumps(root.ito))
+exit(0)
+
+
+
+v_compact = pepo.Compact()
+v_tree = pepo.Tree()
+v_xml = pepo.Xml()
+v_json = pepo.Json()
+
+
+import requests
+import pawpaw
+
+sleepy_hollow = 'https://www.gutenberg.org/ebooks/41.txt.utf-8'
+with requests.get(sleepy_hollow) as r:
+    root = pawpaw.nlp.SimpleNlp().from_text(r.text)
+for para in root.children[-10:]:
+    print(pawpaw.visualization.pepo.Tree().dumps(para))
+exit(0)
+
+
+
+
+s = ' The quick brown fox. '
+i = Ito(s, 1, -1)
+i.children.add(*i.str_split())
+# for c in i.children:
+#     c.children.add(*c)
+
+for result in i.find_all('*([s:The] | ~[s:quick]) & [s:brown]'):
+    print(v_compact.dumps(result))
+exit(0)
+
+# TODO: Make query tests for:
+# even/odd '~'
+# precedence
+# parens: '*([s:The] | ~[s:quick]) & [s:brown]' versus '*[s:The] | (~[s:quick]) & [s:brown])'
+# ensure '~' in 'A & (~(B | C))' is considered against (B | C), not against B
+
+import pawpaw.visualization.ascii_box as box
+
+boxer = box.Box(vertical_style=box.Style(count=box.Style.Count.PARALLEL))
+for line in boxer.from_text('The quick\nbrown fox\njumped over the lazy\ndogs.'):
+    print(line)
+
+exit(0)
+
+chars = set()
+for orientation in box.Side.Orientation:
+    print(f'Orientation: {orientation}')
+    for count in box.Style.Count:
+        for weight in box.Style.Weight:
+            style = box.Style(weight, count)
+            try:
+                side = box.Side(style)
+                char = side[orientation]
+                chars.add(char)
+                print(f'\t{char}')
+            except:
+                pass
+
+print(f'Expected Side character count: {len(box.Side._characters):n}')
+print(f'Actual Side character count: {len(chars):n}')
+
+print()
+
+chars = set()
+for orientation in box.Side.Orientation:
+    print(f'Orientation: {orientation}')
+    for hcount in box.Style.Count:
+        for hweight in box.Style.Weight:
+            hz_style = box.Style(hweight, hcount)
+            for vcount in box.Style.Count:
+                for vweight in box.Style.Weight:
+                    vt_style = box.Style(vweight, vcount)
+                    try:
+                        corner = box.Corner(hz_style, vt_style)
+                        char = corner[orientation]
+                        chars.add(char)
+                        print(f'\t{char}')
+                    except:
+                        pass
+
+print(f'Expected Corner character count: {len(box.Corner._characters):n}')
+print(f'Actual Corner character count: {len(chars):n}')
+
+exit(0)
+
+import nltk
+
+s = 'Here is one sentence.  Here is another.'
+i = Ito(s)
+
+nltk_tok = nltk.tokenize
+sent_itor = pawpaw.arborform.Wrap(lambda ito: ito.from_substrings(ito, *nltk_tok.sent_tokenize(str(ito))))
+
+word_itor = pawpaw.arborform.Wrap(lambda ito: ito.from_substrings(ito, *nltk_tok.word_tokenize(str(ito))))
+sent_itor.itor_children = word_itor
+
+i.children.add(*(sent_itor.traverse(i)))
+print(v_tree.dumps(i))
+exit(0)
+
+
+ws_tok = nltk.tokenize.WhitespaceTokenizer()
+splitter = pawpaw.arborform.Split(regex.compile(ws_tok._pattern, ws_tok._flags))
+i = pawpaw.Ito('The quick brown fox.')
+[str(i) for i in splitter.traverse(i)]
+
+
+
+
+# # VERSION
+#
+# print(pawpaw.__version__)
+# print(pawpaw.__version__.major)
+# print(pawpaw.__version__.pre_release)
+# print(pawpaw.__version__._asdict())
+# exit(0)
+
+from pawpaw.arborform.itorator import Desc, Extract, Split, Wrap
+
+s = 'nine 9 ten 10 eleven 11 TWELVE 12 thirteen 13'
+re = regex.compile(r'(?P<phrase>(?P<word>(?P<char>\w)+) (?P<number>(?P<digit>\d)+)\s*)+')
+match = re.fullmatch(s)
+
+root = Ito.from_match_super2(match)
+print(dump.Compact().dumps(root))
+exit(0)
+
+root = next(Ito.from_match(re, s))
+# print(*root.find_all('**[d:digit]'), sep=', ')  # print all digits
+# print(*root.find_all('**[d:number]{</*[s:i]}'), sep=',')  # print numbers having leter 'i' in their names
+
+
+root = Ito(s, desc='root')
+
+phrases = Split(regex.compile('(?<=\d )'), desc='Phrase')
+
+wrds_nums = Extract(regex.compile(r'(?P<word>[a-z]+) (?P<number>\d+)'))
+phrases.itor_children = wrds_nums
+
+chrs_digs = Extract(regex.compile(r'(?P<char>[a-z])+|(?P<digit>\d)+'))
+wrds_nums.itor_children = chrs_digs
+
+root.children.add(*phrases.traverse(root))
+
+print(dump.Compact().dumps(root))
+exit(0)
+
+    
+# SGR    
+
+for effect in sgr.Intensity, sgr.Italic, sgr.Underline, sgr.Blink, sgr.Invert, sgr.Conceal, sgr.Strike, sgr.Font, sgr.Fore, sgr.Back:
+    print(f'{effect.__name__.upper()}')
+    
+    if effect in (sgr.Fore, sgr.Back):
+        attrs = {nc.name: effect(nc) for nc in sgr.Colors.Named}
+    else:
+        names = filter(lambda n: n.isupper() and not n.startswith('_') and not n.startswith('RESET'), dir(effect))
+        attrs = {name: getattr(effect, name) for name in names}
+        
+    for name, attr in attrs.items():
+        print(f'\t{name}: Before Sgr... {attr}Sgr applied!{effect.RESET} Sgr turned off.')
+    
+    print()
+
+from random import randint
+
+for line in range(1, 50):
+    for color in sgr.Fore, sgr.Back:
+        for col in range(1, 120):
+            rgb = sgr.Colors.Rgb(randint(0, 255), randint(0, 255), randint(0, 255))
+            print(effect(rgb), end='')
+            print(chr(ord('A') + randint(0, 25)) + effect.RESET, end='')
+        print()
+
+exit()
+
+
+# HIGHLIGHTER
+
+s = 'The quick brown fox'
+ito = pawpaw.Ito(s)
+ito.children.add(*ito.split(regex.compile('\s+')))
+
+highlighter = Highlighter(
+    sgr.Colors.Named.BRIGHT_CYAN,
+    sgr.Colors.Named.CYAN,
+    sgr.Colors.Named.BRIGHT_BLUE,
+    sgr.Colors.Named.BLUE,
+    sgr.Colors.Named.MAGENTA,
+)
+highlighter.print(ito)
