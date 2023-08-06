e6018d6bf928b3f2017342400f87952a9004c713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:13 +0000

    Add package handyplot/0.3.3/handyplot-0.3.3.tar.gz

---
+++ packages/handyplot/handyplot-0.3.3.tar.gz/handyplot-0.3.3/setup.py
@@ -0,0 +1,17 @@
+"""
+Type this in terminal to upgrade the package.
+>> python setup.py sdist bdist_wheel
+>> twine upload dist/*
+"""
+from setuptools import find_packages, setup
+
+setup(name="handyplot",
+      version="0.3.3",
+      description="Big refactoring.",
+      author="Hong Peng",
+      python_requires=">=3.8.0",
+      url="https://github.com/minho-hong/handyplot.git",
+      package_data={},
+      packages=find_packages(),
+      install_requires=["matplotlib", "numpy", "vtk"],
+      license="GPL")
