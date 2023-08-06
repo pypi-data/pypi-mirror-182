f83465157a1cedba65743cf8e113f70ff48c1d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:22 +0000

    Add package socha/1.0.0/socha-1.0.0-py3-none-any.whl

---
+++ packages/socha/socha-1.0.0-py3-none-any.whl/socha/__init__.py
@@ -0,0 +1,19 @@
+from socha.api.networking.player_client import IClientHandler
+from socha.api.plugin.penguins import *
+from socha.api.protocol.protocol import Result
+from socha.starter import Starter
+
+__all__ = [
+    'IClientHandler',
+    'Starter',
+    'Vector',
+    'CartesianCoordinate',
+    'HexCoordinate',
+    'Move',
+    'Team',
+    'Field',
+    'Board',
+    'Fishes',
+    'GameState',
+    'Result',
+]
