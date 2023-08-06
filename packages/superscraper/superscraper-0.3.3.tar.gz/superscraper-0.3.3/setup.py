e7ed3d29db112394c31fcacbde7eda3d5fb62bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:12 +0000

    Add package superscraper/0.3.3/superscraper-0.3.3.tar.gz

---
+++ packages/superscraper/superscraper-0.3.3.tar.gz/superscraper-0.3.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['superscraper']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4>=4.11.1,<5.0.0',
+ 'selenium>=4.2.0,<5.0.0',
+ 'webdriver-manager>=3.7.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'superscraper',
+    'version': '0.3.3',
+    'description': 'the friendliest scraper around',
+    'long_description': "# Super Scraper\nScraping couldn't get much easier.  \n\n**Super Scraper** is built with ease in mind - for those hard to scrape places. It drives with Selenium and parses with BeautifulSoup4. I've provided some convenience methods to make common actions even easier for you.\n\n# Example\n\n```\nfrom superscraper import SuperScraper, ScraperOptions, Browser, By\n\noptions = ScraperOptions()\noptions.show_process = True \noptions.incognito = True \n\nscraper = SuperScraper(\n    browser=Browser.CHROME,\n    options=options)\n\nscraper.search('https://www.google.com')\nscraper.fill_in(By.NAME, 'q', 'hello world')\nscraper.click(By.NAME, 'btnK')\n\nsearch_results = scraper.driver.find_elements(By.CLASS_NAME, 'g')\nfor result in search_results[:3]:\n\n    title = scraper.attempt(result.find_element, By.TAG_NAME, 'h3')\n    if title:\n        print(title.text)\n        a = result.find_element(By.TAG_NAME, 'a')\n        scraper.open_new_tab(By.LINK_TEXT, a.text)\n        scraper.close_current_tab(switch_to_tab=-1)\n```",
+    'author': 'Mbeebe',
+    'author_email': 'pyn-sol@beebe.dev',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
