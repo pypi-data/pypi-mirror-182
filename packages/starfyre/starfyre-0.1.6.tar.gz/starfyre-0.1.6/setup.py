e66b9f8383c36b2eae23d1eff0558a5b4bc0e723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:47 +0000

    Add package starfyre/0.1.6/starfyre-0.1.6.tar.gz

---
+++ packages/starfyre/starfyre-0.1.6.tar.gz/starfyre-0.1.6/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['starfyre']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'starfyre',
+    'version': '0.1.6',
+    'description': '',
+    'long_description': '# Starfyre ⭐🔥\n\nCreate reactive frontends using only Python\n',
+    'author': 'Sanskar Jethi',
+    'author_email': 'sansyrox@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
