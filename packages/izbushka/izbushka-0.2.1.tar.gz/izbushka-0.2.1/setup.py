a6e12f23d88e189096fbd9cfacaf289ea32e9357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:37 +0000

    Add package izbushka/0.2.1/izbushka-0.2.1.tar.gz

---
+++ packages/izbushka/izbushka-0.2.1.tar.gz/izbushka-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['izbushka']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyPika>=0.48.9,<0.49.0', 'PyYAML<6', 'clickhouse-connect>=0.4.1,<0.5.0']
+
+setup_kwargs = {
+    'name': 'izbushka',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Anton Evdokimov',
+    'author_email': 'meowmeowcode@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9',
+}
+
+
+setup(**setup_kwargs)
