b72f43fb0541000f703470182e6eafe3744ec56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:50 +0000

    Add package notional/0.5.1/notional-0.5.1.tar.gz

---
+++ packages/notional/notional-0.5.1.tar.gz/notional-0.5.1/src/notional/__main__.py
@@ -0,0 +1,8 @@
+"""Main entry point for notional.
+
+At the moment, this simply prints the current version and exits.
+"""
+
+import notional
+
+print(f"{notional.__pkgname__}-{notional.__version__}")
