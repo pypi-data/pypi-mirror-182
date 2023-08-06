17e1ed14512a7123a7d5956b0ee63d10a6bc5662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:46 +0000

    Add package EcoSpace/0.0.4/EcoSpace-0.0.4.tar.gz

---
+++ packages/EcoSpace/EcoSpace-0.0.4.tar.gz/EcoSpace-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import setuptools
+
+long_description = '''官方链接
+EcoSpace 官方链接：https://www.ecospace.top
+
+Official link
+EcoSpace Official link：https://www.ecospace.top
+'''
+
+packages = ['EcoSpace']
+setup(name = 'EcoSpace',
+    version = '0.0.4',
+    author = 'EcoSpace',
+    author_email = 'getmail@ecospace.top',
+    description = 'EcoSpace官方库',
+    long_description = long_description,
+    packages = packages, 
+    package_dir = {'requests': 'requests'},)
