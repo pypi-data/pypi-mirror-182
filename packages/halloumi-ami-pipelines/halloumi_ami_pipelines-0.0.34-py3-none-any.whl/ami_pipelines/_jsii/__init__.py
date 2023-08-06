3ecb074b2375315aabb363c36bbfee14695ad83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:15 +0000

    Add package halloumi-ami-pipelines/0.0.34/halloumi_ami_pipelines-0.0.34-py3-none-any.whl

---
+++ packages/halloumi-ami-pipelines/halloumi_ami_pipelines-0.0.34-py3-none-any.whl/ami_pipelines/_jsii/__init__.py
@@ -0,0 +1,44 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk.aws_codebuild._jsii
+import aws_cdk.aws_codecommit._jsii
+import aws_cdk.aws_codepipeline._jsii
+import aws_cdk.aws_codepipeline_actions._jsii
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_imagebuilder._jsii
+import aws_cdk.aws_kms._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.aws_logs_destinations._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_sns._jsii
+import aws_cdk.aws_sns_subscriptions._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "halloumi-ami-pipelines",
+    "0.0.34",
+    __name__[0:-6],
+    "halloumi-ami-pipelines@0.0.34.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
