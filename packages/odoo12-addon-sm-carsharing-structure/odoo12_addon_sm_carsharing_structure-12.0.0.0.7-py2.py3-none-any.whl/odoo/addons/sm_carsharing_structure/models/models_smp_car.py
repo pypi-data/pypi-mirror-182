44964eea89e253aef07c8944a343b3e5b9c232df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:15 +0000

    Add package odoo12-addon-sm-carsharing-structure/12.0.0.0.7/odoo12_addon_sm_carsharing_structure-12.0.0.0.7-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-sm-carsharing-structure/odoo12_addon_sm_carsharing_structure-12.0.0.0.7-py2.py3-none-any.whl/odoo/addons/sm_carsharing_structure/models/models_smp_car.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields
+from odoo.tools.translate import _
+
+
+class smp_car(models.Model):
+  _inherit = 'smp.sm_car'
+
+  cs_car_ids = fields.One2many('fleet.vehicle', 'db_car_id', string=_('CS Cars'))
