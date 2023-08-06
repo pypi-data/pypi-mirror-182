b9139549c71f11c9d678221df66f08cf7ebc277f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:42 +0000

    Add package onnx2tf/1.2.22/onnx2tf-1.2.22.tar.gz

---
+++ packages/onnx2tf/onnx2tf-1.2.22.tar.gz/onnx2tf-1.2.22/onnx2tf/ops/Min.py
@@ -0,0 +1,81 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from onnx2tf.utils.common_functions import (
+    get_constant_or_variable,
+    print_node_info,
+    inverted_operation_enable_disable,
+    make_tf_node_info,
+)
+
+
+@print_node_info
+@inverted_operation_enable_disable
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """Min
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    before_op_output_shape_trans = True
+    for graph_node_input in graph_node.inputs:
+        before_op_output_shape_trans_n = \
+            tf_layers_dict.get(graph_node_input.name, {}).get('before_op_output_shape_trans', True)
+        before_op_output_shape_trans = \
+            before_op_output_shape_trans and before_op_output_shape_trans_n
+
+    values = []
+    for graph_node_input in graph_node.inputs:
+        const_or_var = get_constant_or_variable(
+            graph_node_input,
+            before_op_output_shape_trans,
+        )
+        if isinstance(const_or_var, gs.Variable):
+            values.append(tf_layers_dict[const_or_var.name]['tf_node'])
+        else:
+            values.append(const_or_var)
+
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Generation of TF OP
+    minimumed_tesnor = values[0]
+    for i in range(1, len(values)):
+        minimumed_tesnor = tf.minimum(minimumed_tesnor, values[i])
+
+    tf_layers_dict[graph_node_output.name]['tf_node'] = minimumed_tesnor
+
+    # Generation of Debug Info
+    tf_inputs = {f"input{idx}": value for idx, value in enumerate(values)}
+    tf_layers_dict[graph_node_output.name]['tf_node_info'] = \
+        make_tf_node_info(
+            node_info={
+                'tf_op_type': tf.minimum,
+                'tf_inputs': tf_inputs,
+                'tf_outputs': {
+                    'output': tf_layers_dict[graph_node_output.name]['tf_node'],
+                },
+            }
+        )
