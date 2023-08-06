d13745373bef968ddff91f3f89d11e50a28c6baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:30:27 +0000

    Add package vrt-lss-agro/5.11.1826/vrt_lss_agro-5.11.1826-py3-none-any.whl

---
+++ packages/vrt-lss-agro/vrt_lss_agro-5.11.1826-py3-none-any.whl/vrt_lss_agro/__init__.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+
+"""
+    VRt.Agro [AG]
+
+    The version of the OpenAPI document: 5.11.1826
+    Contact: servicedesk@veeroute.com
+    Auto Generated
+"""
+
+
+__version__ = "5.11.1826"
+
+# import ApiClient
+from vrt_lss_agro.api_client import ApiClient
+
+# import Configuration
+from vrt_lss_agro.configuration import Configuration
+
+# import exceptions
+from vrt_lss_agro.exceptions import OpenApiException
+from vrt_lss_agro.exceptions import ApiAttributeError
+from vrt_lss_agro.exceptions import ApiTypeError
+from vrt_lss_agro.exceptions import ApiValueError
+from vrt_lss_agro.exceptions import ApiKeyError
+from vrt_lss_agro.exceptions import ApiException
