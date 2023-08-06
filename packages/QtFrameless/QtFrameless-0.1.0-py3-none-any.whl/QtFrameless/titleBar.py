4191b3cdf6442012ff989ef1f26371d50cfc7a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:21 +0000

    Add package QtFrameless/0.1.0/QtFrameless-0.1.0-py3-none-any.whl

---
+++ packages/QtFrameless/QtFrameless-0.1.0-py3-none-any.whl/QtFrameless/titleBar.py
@@ -0,0 +1,83 @@
+import os
+from PySide6.QtWidgets import QLabel, QWidget, QSizePolicy, QHBoxLayout
+from PySide6.QtGui import QPixmap
+from QtFrameless.button import TitleBarButton
+from QtFrameless.cursor import Cursor
+
+icon = os.path.join(os.path.dirname(__file__), "home.png")
+
+class TitleBar(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.label = QLabel()
+        self.setProperty("titleBar", "true")
+        pix = QPixmap(icon)
+        self.icon = QLabel()
+        self.icon.setPixmap(pix)
+        self.label.setText("TitleBar")
+        self.setMaximumHeight(50)
+        self.closeButton = TitleBarButton("close", parent=self)
+        self.minimizeButton = TitleBarButton("min", parent=self)
+        self.maximizeButton = TitleBarButton("max",parent=self)
+        sizePolicy = QSizePolicy()
+        sizePolicy.setHorizontalPolicy(QSizePolicy.Policy.Fixed)
+        self.closeButton.setSizePolicy(sizePolicy)
+        self.layout = QHBoxLayout(self)
+        self.layout.addWidget(self.icon)
+        self.layout.addStretch(1)
+        self.layout.addWidget(self.label)
+        self.layout.addStretch(1)
+        self.layout.addWidget(self.minimizeButton)
+        self.layout.addWidget(self.maximizeButton)
+        self.layout.addWidget(self.closeButton)
+        self.setMouseTracking(True)
+        self._pressed = False
+        self._cpos = None
+        self._cgeom = None
+        self._direction = None
+
+    def setWindowTitle(self, title: str) -> None:
+        """Sets the title bar label."""
+        self.label.setText(title)
+
+    def setWindowIcon(self, icon: str) -> None:
+        """Sets the window icon."""
+        pixmap = QPixmap(icon)
+        self.icon.setPixmap(pixmap)
+
+    def mousePressEvent(self, event):
+        pos = event.position().toPoint()
+        rect = self.window().rect()
+        direction = Cursor.match(pos, rect, exclusions=[5,3,2])
+        self._direction = direction
+        self._cgeom = self.window().geometry()
+        self._cpos = pos
+        self._pressed = True
+
+    def mouseReleaseEvent(self, _):
+        self._pressed = False
+        self._cpos = None
+        self._direction = None
+        self._cgeom = None
+
+    def mouseMoveEvent(self, event):
+        window = self.window()
+        geom = window.geometry()
+        pos = event.position().toPoint()
+        if self._pressed:
+            r = Cursor.resize(self._cpos, pos, geom, self._cgeom, self._direction)
+            min_width, min_height = window.minimumSizeHint().toTuple()
+            if r[2] < min_width or r[3] < min_height: return
+            return window.setGeometry(*r)
+        direction = Cursor.match(pos, geom, [5,3,2])
+        self.setCursor(direction["shape"])
+
+    def mouseDoubleClickEvent(self, event):
+        pos = event.position().toPoint()
+        std = Cursor.loc[8]
+        geom = self.window().geometry()
+        if std["range"](pos, geom):
+            if self.window().isMaximized():
+                self.window().showNormal()
+            else:
+                self.window().showMaximized()
---
+++ packages/QtFrameless/QtFrameless-0.1.0-py3-none-any.whl/QtFrameless/window.py
@@ -0,0 +1,92 @@
+import os
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PySide6.QtCore import Qt
+from QtFrameless.titleBar import TitleBar
+from QtFrameless.style import stylesheet
+from QtFrameless.cursor import Cursor
+
+class FramelessWindow(QMainWindow):
+    """A frameless MainWindow widget."""
+
+    def __init__(self, parent=None, titleBar=None):
+        """Construct new frameless Qt widget."""
+        super().__init__(parent=parent)
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setMouseTracking(True)
+        self.setStyleSheet(stylesheet)
+        self.setObjectName('MainWindow')
+        self._central = QWidget(parent=self)
+        self._central.setMouseTracking(True)
+        self._central.mouseMoveEvent = self.mouseMoveEvent
+        self._layout = QVBoxLayout(self._central)
+        self._layout.setContentsMargins(0,0,0,0)
+        if titleBar is not None:
+            self.titleBar = titleBar()
+        else:
+            self.titleBar = TitleBar(self)
+        self._layout.addWidget(self.titleBar)
+        self._layout.addStretch(1)
+        self._main = QWidget(self)
+        self._layout.addWidget(self._main)
+        self._direction = None
+        self._cgeom = None
+        self._cpos = None
+        self._pressed = None
+        super().setCentralWidget(self._central)
+
+    def setStyleSheet(self, qss):
+        if not isinstance(qss, str):
+            if hasattr(qss, "read"):
+                qss = qss.read()
+            elif hasattr(qss, "readText"):
+                qss = qss.readText()
+        elif os.path.exists(qss):
+            qss = open(qss, 'tr').read()
+        qss = stylesheet + qss
+        super().setStyleSheet(qss)
+
+    def setWindowTitle(self, title: str):
+        self.titleBar.setWindowTitle(title)
+
+    def setWindowIcon(self, icon: str):
+        self.titleBar.setWindowIcon(icon)
+
+    def setCentralWidget(self, widget):
+        item = self._layout.takeAt(2)
+        item.widget().deleteLater()
+        item.widget().destroy()
+        self._layout.takeAt(1)
+        self._layout.addWidget(widget)
+
+    def mousePressEvent(self, event):
+        pos = event.position().toPoint()
+        rect = self.rect()
+        self._direction = Cursor.match(pos, rect, [0, 4, 1])
+        self._cgeom = self.geometry()
+        self._cpos = pos
+        self._pressed = True
+
+    def mouseReleaseEvent(self, _):
+        self._pressed = False
+        self._cpos = None
+        self._direction = None
+        self._cgeom = None
+
+    def mouseMoveEvent(self, event):
+        geom = self.geometry()
+        pos = event.position().toPoint()
+        if self._pressed and self._direction["id"] != "standard":
+            r = Cursor.resize(self._cpos, pos, geom, self._cgeom, self._direction)
+            min_width, min_height = self.minimumSizeHint().toTuple()
+            if r[2] > min_width and r[3] > min_height:
+                return self.setGeometry(*r)
+        shape = Cursor.match(pos, geom, [0, 1, 4])["shape"]
+        self.setCursor(shape)
+
+
+
+def execute():
+    app = QApplication([])
+    window = FramelessWindow()
+    window.show()
+    app.exec()
