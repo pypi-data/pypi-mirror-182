8c721b2363222007b0c46c0277515a3a8686b314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:09 +0000

    Add package SigProfilerAssignment/0.0.23/SigProfilerAssignment-0.0.23-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.23-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.23'
+version = '0.0.23'
+Update = 'v0.0.23: ID Decomposition Plots exome fix and ID tests'
+
+    
+    
