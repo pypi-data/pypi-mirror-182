b87672a82de9477a437b51f4cf56a09c9058fb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:47 +0000

    Add package DobbyStockSimulation/0.1/DobbyStockSimulation-0.1.tar.gz

---
+++ packages/DobbyStockSimulation/DobbyStockSimulation-0.1.tar.gz/DobbyStockSimulation-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DobbyStockSimulation',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A python package for Stock Simulation',
+	url='https://github.com/vijina17/InClassCI2022N',
+    author='Yiwei Gan & Viji Rajagopalan',
+    author_email='gavinyw@outlook.com'
+)
