7c1cd62177b1571d8adc3e6558f82ccda0ba8ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:35 +0000

    Add package gamePointsCounter/0.6/gamePointsCounter-0.6.tar.gz

---
+++ packages/gamePointsCounter/gamePointsCounter-0.6.tar.gz/gamePointsCounter-0.6/README.md
@@ -0,0 +1,16 @@
+# gamePointsCounter
+
+## Prerequisites
+- Python 3
+
+## Installation Instructions
+It is very easy to install this application. You can easily git clone this code and download it onto a specific folder on your computer. 
+
+
+## How to Run
+Then running the program is as simple as using the command:
+```python main.py```
+
+
+## Creator Contact
+anjanam@umich.edu
---
+++ packages/gamePointsCounter/gamePointsCounter-0.6.tar.gz/gamePointsCounter-0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='gamePointsCounter',
+    version='0.6',
+    license='MIT',
+    author="Anjana",
+    author_email='anjanam@umich.edu',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/anjanamalay/gamePointsCounter',
+    keywords='game conuting project',
+    install_requires=[],
+)
