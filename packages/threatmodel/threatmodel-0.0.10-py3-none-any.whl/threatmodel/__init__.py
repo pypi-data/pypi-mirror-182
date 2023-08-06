b522a367ee614897e6773e7e84f83b7ed3c52bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:12 +0000

    Add package threatmodel/0.0.10/threatmodel-0.0.10-py3-none-any.whl

---
+++ packages/threatmodel/threatmodel-0.0.10-py3-none-any.whl/threatmodel/__init__.py
@@ -0,0 +1,40 @@
+from .threatlib import DEFAULT_THREATLIB
+from .model import Model, Element,Result
+from .trust_boundary import TrustBoundary
+from .threat import AttackCategory, Threat, Threatlib
+from .table_format import TableFormat
+from .risk import Likelihood, Severity, Impact, Treatment, Risk
+from .control import Control
+from .data_flow import Data, Protocol, Authentication, Authorization, DataFlow
+from .asset import Machine,Technology,DataFormat,TechnicalAsset,ExternalEntity,Process,DataStore
+
+
+__all__ = (
+    "AttackCategory",
+    "Data",
+    "Protocol",
+    "Authentication",
+    "Authorization",
+    "DataFlow",
+    "Element",
+    "Likelihood",
+    "Severity",
+    "Impact",
+    "Treatment",
+    "Risk",
+    "Model",
+    "TableFormat",
+    "Result",
+    "Control",
+    "Machine",
+    "Technology",
+    "DataFormat",
+    "TechnicalAsset",
+    "ExternalEntity",
+    "Process",
+    "DataStore",
+    "TrustBoundary"
+    "Threat",
+    "Threatlib",
+    "DEFAULT_THREATLIB"
+)
