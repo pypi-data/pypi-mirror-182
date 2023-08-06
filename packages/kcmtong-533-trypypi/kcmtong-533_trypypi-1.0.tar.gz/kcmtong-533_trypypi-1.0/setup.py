ae9ec2a4ed48eb60da221d945f31e3c6adb7aaf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:24 +0000

    Add package kcmtong-533-trypypi/1.0/kcmtong-533_trypypi-1.0.tar.gz

---
+++ packages/kcmtong-533-trypypi/kcmtong-533_trypypi-1.0.tar.gz/kcmtong-533_trypypi-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kcmtong-533_trypypi',
+    version='1.0',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A test python package',
+	url='https://github.com/khalad-hasan/myMileConverter',
+    author='MK Hasan',
+    author_email='khalad.hasan@gmail.com'
+)
