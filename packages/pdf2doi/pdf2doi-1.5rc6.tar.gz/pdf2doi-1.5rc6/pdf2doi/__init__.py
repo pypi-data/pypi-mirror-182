e71c3883dc0db3bc3126d28dc1c055365fcaf6c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:42 +0000

    Add package pdf2doi/1.5rc6/pdf2doi-1.5rc6.tar.gz

---
+++ packages/pdf2doi/pdf2doi-1.5rc6.tar.gz/pdf2doi-1.5rc6/pdf2doi/__init__.py
@@ -0,0 +1,32 @@
+import logging
+import importlib.util
+
+# Setup logging
+logger = logging.getLogger("pdf2doi")
+logger.setLevel(level=logging.INFO)
+if not logger.handlers:
+    formatter = logging.Formatter("[pdf2doi]: %(message)s")
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+logger.propagate = False
+
+from .config import config
+config.ReadParamsINIfile()  #Load all current configuration from the .ini file. If the .ini file is not present, it generates it using default values
+
+#Determine the list of libraries to be used to extract text from pdf files
+reader_libraries = ['pdfminer','PyPdf']
+
+is_textract_installed = importlib.util.find_spec('textract')
+if is_textract_installed:
+    reader_libraries. append('textract')
+    
+
+config.set('verbose',config.get('verbose')) #This is a quick and dirty way (to improve in the future) to make sure that the verbosity of the pdf2doi logger is properly set according
+                                            #to the current value of config.get('verbose') (see config.py file for details)
+from .main import pdf2doi, pdf2doi_singlefile
+from .finders import *
+#from .bibtex_makers import *
+from .utils_registry import install_right_click, uninstall_right_click
+
+
