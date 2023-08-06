8001e1437d5d041036b00949030df84711dc11c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:31 +0000

    Add package matplothelp/0.0.1/matplothelp-0.0.1-py3-none-any.whl

---
+++ packages/matplothelp/matplothelp-0.0.1-py3-none-any.whl/matplothelp/reverse.py
@@ -0,0 +1,14 @@
+print("1")
+"""
+message=input("enter text:")
+translated=""
+
+i=len(message)-1
+
+while i>=0:
+    translated=translated+message[i]
+    i=i-1
+print("encrypted text:",translated)
+print("original text:",message)
+"""
+
---
+++ packages/matplothelp/matplothelp-0.0.1-py3-none-any.whl/matplothelp/tabular.py
@@ -0,0 +1,12 @@
+print("1")
+"""
+import numpy as nm
+import matplotlib.pyplot as mtp
+import pandas as pd
+df = pd.read_csv("AirQualityUCI.csv", sep = ";", decimal = ",")
+print("ÄirQualityUCI data before iloc")
+print(df.head())
+df = df.iloc[ : ,0:14]
+print("ÄirQualityUCI data after iloc")
+print(df.head())
+"""
