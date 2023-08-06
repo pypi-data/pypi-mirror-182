a7533fc97580897b64db33b98f23e7e7a2c3f65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:01:12 +0000

    Add package distributions-mmmk/0.1/distributions_mmmk-0.1.tar.gz

---
+++ packages/distributions-mmmk/distributions_mmmk-0.1.tar.gz/distributions_mmmk-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_mmmk',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_mmmk'],
+      author = 'Mica Knox',
+      zip_safe=False)
