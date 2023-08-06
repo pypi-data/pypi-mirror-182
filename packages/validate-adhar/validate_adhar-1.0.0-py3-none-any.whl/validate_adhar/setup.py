e3ae3581b7d10781dd4f966009a1dc74a29a320d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:13 +0000

    Add package validate-adhar/1.0.0/validate_adhar-1.0.0-py3-none-any.whl

---
+++ packages/validate-adhar/validate_adhar-1.0.0-py3-none-any.whl/validate_adhar/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(
+    name="validate_adhar",
+    version="1.0.0",
+    description='This Package is a Function to validate Adhaar Car and name',
+    packages=['validate_adhar']
+)
