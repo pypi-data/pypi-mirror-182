fb7679d4dbe972c3ecae8f89e42d001c9d868e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:13 +0000

    Add package nikocraft/0.0.2/nikocraft-0.0.2.tar.gz

---
+++ packages/nikocraft/nikocraft-0.0.2.tar.gz/nikocraft-0.0.2/nikocraft/__init__.py
@@ -0,0 +1,49 @@
+"""Nikocraft Python Library"""
+
+# Environment variables
+import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = ""
+del os
+
+# Modules
+from .utils import time
+from .utils import file
+from .utils import log
+
+# Classes
+from .app import App
+from .utils.config import Config
+from .utils.enum import Enum
+from .window.window import Window
+from .window.vector2d import Vec
+from .window.vector3d import Vec3
+from .window.rgb import RGB
+from .window.rgb import RGBColor
+from .window.clock import Clock
+from .window.renderer import Renderer
+from .window.debug_screen import DebugScreen
+from .window.event_hook import EventHook
+
+# Constants
+from .constants import AUTHOR, VERSION, SCREEN_UPDATE_EVENT
+
+__all__ = [
+    "time",
+    "file",
+    "log",
+    "App",
+    "Config",
+    "Enum",
+    "Window",
+    "Vec",
+    "Vec3",
+    "RGB",
+    "RGBColor",
+    "Clock",
+    "Renderer",
+    "DebugScreen",
+    "EventHook",
+    "AUTHOR",
+    "VERSION",
+    "SCREEN_UPDATE_EVENT"
+]
---
+++ packages/nikocraft/nikocraft-0.0.2.tar.gz/nikocraft-0.0.2/nikocraft/utils/config.py
@@ -0,0 +1,103 @@
+"""Contains the config class for saving JSON configurations"""
+
+# Standard modules
+from typing import Any
+from abc import ABC
+from logging import Logger
+
+# Local modules
+from ..utils import file
+
+
+class Config(ABC):
+    """Config class for saving JSON configurations"""
+
+    def __init__(self, path: str, logger: Logger = None) -> None:
+
+        self.path: str = path
+        self.logger: Logger = logger
+
+    # METHODS
+
+    def load(self) -> None:
+        """Load data from JSON file at path"""
+
+        # Load file
+        if self.logger:
+            self.logger.debug(f"Load configuration file at '{self.path}' ...")
+        data = file.load_json(self.path, self.logger, create=False)
+
+        # Decode data
+        if self.logger:
+            self.logger.debug("Decode configuration attributes ...")
+        for attribute in vars(self):
+            if attribute in ("path", "logger"):
+                continue
+            result = self.load_attribute(attribute, data)
+            if result is None:
+                if attribute in data:
+                    setattr(self, attribute, data[attribute])
+            else:
+                setattr(self, attribute, result)
+
+    def save(self) -> None:
+        """Save data to JSON file at path"""
+
+        # Encode data
+        data = {}
+        if self.logger:
+            self.logger.debug("Encode configuration attributes ...")
+        for attribute in vars(self):
+            if attribute in ("path", "logger"):
+                continue
+            result = self.save_attribute(attribute, getattr(self, attribute), data)
+            if result is None:
+                data[attribute] = getattr(self, attribute)
+
+        # Save file
+        if self.logger:
+            self.logger.debug(f"Save configuration file at '{self.path}' ...")
+        file.save_json(self.path, data, self.logger)
+
+    # ABSTRACT METHODS
+
+    def load_attribute(self, attribute: str, data: dict) -> Any | None:
+        """Load an attribute from data (Return result or None for automatic loading)
+
+        *Called on loading -
+        Don't call this method manually*
+        """
+
+        pass
+
+    def save_attribute(self, attribute: str, value: Any, data: dict) -> Any | None:
+        """Save an attribute to data (Modify data or return None for automatic saving)
+
+        *Called on saving -
+        Don't call this method manually*
+        """
+
+        pass
+
+    def load_extras(self, data: dict) -> None:
+        """Additional loading tasks
+
+        *Called on loading -
+        Don't call this method manually*
+        """
+
+        pass
+
+    def save_extras(self, data: dict) -> None:
+        """Additional saving tasks
+
+        *Called on saving -
+        Don't call this method manually*
+        """
+
+        pass
+
+    # OVERLOADS
+
+    def __repr__(self) -> str:
+        return f"Config[id={id(self)}, path={self.path}]"
