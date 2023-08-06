6d52010d79a2c3db668df32179016391b033f69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:14 +0000

    Add package scippneutron/22.12.4/scippneutron-22.12.4.tar.gz

---
+++ packages/scippneutron/scippneutron-22.12.4.tar.gz/scippneutron-22.12.4/src/scippneutron/core/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+# @author Simon Heybrock
+
+# flake8: noqa
+
+import os
+
+from .conversions import convert, conversion_graph, deduce_conversion_graph
---
+++ packages/scippneutron/scippneutron-22.12.4.tar.gz/scippneutron-22.12.4/src/scippneutron/file_loading/_nexus.py
@@ -0,0 +1,4 @@
+from typing import Union
+import scipp as sc
+
+ScippData = Union[sc.Dataset, sc.DataArray, None]
