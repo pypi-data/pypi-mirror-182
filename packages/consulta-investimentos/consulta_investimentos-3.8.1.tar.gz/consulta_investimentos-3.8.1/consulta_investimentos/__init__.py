16bb40afa377496670bc7af574e6348fdc87c731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:58 +0000

    Add package consulta-investimentos/3.8.1/consulta_investimentos-3.8.1.tar.gz

---
+++ packages/consulta-investimentos/consulta_investimentos-3.8.1.tar.gz/consulta_investimentos-3.8.1/consulta_investimentos/__init__.py
@@ -0,0 +1,5 @@
+from .ativos_cotacoes.cotacoes import consulta_cotacoes
+from .ativos_dividendos.dividendos import consulta_dividendos
+from .ativos_eventos.eventos import consulta_eventos
+from .ativos_posicao.posicao import consulta_posicao
+from .moeda_cotacao.moedas import consulta_moedas
