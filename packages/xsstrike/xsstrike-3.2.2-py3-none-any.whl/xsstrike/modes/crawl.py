7d82654947577120ec944f475115a8bbe38a7fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:22 +0000

    Add package xsstrike/3.2.2/xsstrike-3.2.2-py3-none-any.whl

---
+++ packages/xsstrike/xsstrike-3.2.2-py3-none-any.whl/xsstrike/modes/crawl.py
@@ -0,0 +1,63 @@
+import copy
+import re
+
+import xsstrike.core.config
+from xsstrike.core.colors import green, end
+from xsstrike.core.config import xsschecker
+from xsstrike.core.filterChecker import filterChecker
+from xsstrike.core.generator import generator
+from xsstrike.core.htmlParser import htmlParser
+from xsstrike.core.requester import requester
+from xsstrike.core.log import setup_logger
+
+logger = setup_logger(__name__)
+
+
+def crawl(scheme, host, main_url, form, blindXSS, blindPayload, headers, delay, timeout, encoding):
+    if form:
+        for each in form.values():
+            url = each['action']
+            if url:
+                if url.startswith(main_url):
+                    pass
+                elif url.startswith('//') and url[2:].startswith(host):
+                    url = scheme + '://' + url[2:]
+                elif url.startswith('/'):
+                    url = scheme + '://' + host + url
+                elif re.match(r'\w', url[0]):
+                    url = scheme + '://' + host + '/' + url
+                if url not in xsstrike.core.config.globalVariables['checkedForms']:
+                    xsstrike.core.config.globalVariables['checkedForms'][url] = []
+                method = each['method']
+                GET = True if method == 'get' else False
+                inputs = each['inputs']
+                paramData = {}
+                for one in inputs:
+                    paramData[one['name']] = one['value']
+                    for paramName in paramData.keys():
+                        if paramName not in xsstrike.core.config.globalVariables['checkedForms'][url]:
+                            xsstrike.core.config.globalVariables['checkedForms'][url].append(paramName)
+                            paramsCopy = copy.deepcopy(paramData)
+                            paramsCopy[paramName] = xsschecker
+                            response = requester(
+                                url, paramsCopy, headers, GET, delay, timeout)
+                            occurences = htmlParser(response, encoding)
+                            positions = occurences.keys()
+                            occurences = filterChecker(
+                                url, paramsCopy, headers, GET, delay, occurences, timeout, encoding)
+                            vectors = generator(occurences, response.text)
+                            if vectors:
+                                for confidence, vects in vectors.items():
+                                    try:
+                                        payload = list(vects)[0]
+                                        logger.vuln('Vulnerable webpage: %s%s%s' %
+                                                    (green, url, end))
+                                        logger.vuln('Vector for %s%s%s: %s' %
+                                                    (green, paramName, end, payload))
+                                        break
+                                    except IndexError:
+                                        pass
+                            if blindXSS and blindPayload:
+                                paramsCopy[paramName] = blindPayload
+                                requester(url, paramsCopy, headers,
+                                          GET, delay, timeout)
