1bdecf239bb80604f747270625b2bef140ecd3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:50 +0000

    Add package tasktiger/0.17.0/tasktiger-0.17.0.tar.gz

---
+++ packages/tasktiger/tasktiger-0.17.0.tar.gz/tasktiger-0.17.0/tasktiger/logging.py
@@ -0,0 +1,16 @@
+from ._internal import g
+
+
+def tasktiger_processor(logger, method_name, event_dict):
+    """
+    TaskTiger structlog processor.
+
+    Inject the current task ID and queue for non-batch tasks.
+    """
+
+    if g["current_tasks"] is not None and not g["current_task_is_batch"]:
+        current_task = g["current_tasks"][0]
+        event_dict["task_id"] = current_task.id
+        event_dict["queue"] = current_task.queue
+
+    return event_dict
