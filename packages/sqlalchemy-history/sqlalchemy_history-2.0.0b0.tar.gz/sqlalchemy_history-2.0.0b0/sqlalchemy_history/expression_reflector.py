efa01507e76ce4d7bed45dda92528b4629c4170d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:15 +0000

    Add package sqlalchemy-history/2.0.0b0/sqlalchemy_history-2.0.0b0.tar.gz

---
+++ packages/sqlalchemy-history/sqlalchemy_history-2.0.0b0.tar.gz/sqlalchemy_history-2.0.0b0/sqlalchemy_history/expression_reflector.py
@@ -0,0 +1,29 @@
+"""This is ExpressionReflector used for 
+"""
+import sqlalchemy as sa
+from sqlalchemy.sql.expression import bindparam
+
+from sqlalchemy_history.utils import version_table
+
+
+class VersionExpressionReflector(sa.sql.visitors.ReplacingCloningVisitor):
+    def __init__(self, parent, relationship):
+        self.parent = parent
+        self.relationship = relationship
+
+    def replace(self, column):
+        if not isinstance(column, sa.Column):
+            return
+        try:
+            table = version_table(column.table)
+        except KeyError:
+            reflected_column = column
+        else:
+            reflected_column = table.c[column.name]
+            if column in self.relationship.local_columns and table == self.parent.__table__:
+                reflected_column = bindparam(column.key, getattr(self.parent, column.key))
+
+        return reflected_column
+
+    def __call__(self, expr):
+        return self.traverse(expr)
