8437f054c719eb3ca8ecb07904d67b2f88ac9cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:16 +0000

    Add package noliboskar/0.0.4/noliboskar-0.0.4.tar.gz

---
+++ packages/noliboskar/noliboskar-0.0.4.tar.gz/noliboskar-0.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+
+setup(name="noliboskar", version="0.0.4",
+      author="Oskar",
+      author_email="example@example.com",
+      license="MIT",
+      description="My test package",
+      long_description=(this_directory / "README.md").read_text(),
+      long_description_content_type="text/markdown",
+      packages=find_packages(exclude=("me")),
+      #install_requires=["pygame>='3.4'"],
+      include_package_data=True
+)
