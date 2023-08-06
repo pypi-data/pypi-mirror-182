43e6c33d2d782a206194a7acf850822027d01f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:57 +0000

    Add package sneks-dev/1.0.0a2/sneks-dev-1.0.0a2.tar.gz

---
+++ packages/sneks-dev/sneks-dev-1.0.0a2.tar.gz/sneks-dev-1.0.0a2/src/sneks/config/definition.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from typing import Optional, Tuple, List
+
+
+@dataclass(frozen=True)
+class Game:
+    rows: int
+    columns: int
+    dynamic_food: bool
+    food: int
+
+
+@dataclass(frozen=True)
+class Colors:
+    background: Tuple[int, int, int]
+    border: Tuple[int, int, int]
+    invalid: Tuple[int, int, int]
+    food: Tuple[int, int, int]
+    snake: List[Tuple[int, int, int]]
+
+
+@dataclass(frozen=True)
+class Graphics:
+    display: bool
+    headless: bool
+    cell_size: int
+    padding: int
+    colors: Colors
+    delay: int
+    record: bool
+    record_prefix: str
+
+
+@dataclass(frozen=True)
+class Config:
+    game: Game
+    graphics: Optional[Graphics]
+    runs: int
+    turn_limit: int
+    registrar_prefix: str
