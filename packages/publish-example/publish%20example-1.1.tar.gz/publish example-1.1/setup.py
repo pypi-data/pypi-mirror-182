9cc2bf6a2b77d50efeaff31405a7db17bf09334a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:20 +0000

    Add package publish-example/1.1/publish%20example-1.1.tar.gz

---
+++ packages/publish-example/publish%20example-1.1.tar.gz/publish example-1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '1.1'
+DESCRIPTION = "A simple example package"
+
+# Setting up
+setup(
+    name="publish example",
+    version=VERSION,
+    author="handsome victor 666",
+    author_email="handsomevictor0054@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy'],  # add any additional packages that
+)
