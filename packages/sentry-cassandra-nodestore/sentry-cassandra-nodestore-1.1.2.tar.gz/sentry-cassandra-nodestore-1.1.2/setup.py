afad94731cadcffd67a1e7f38ec5ac147157edaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:10 +0000

    Add package sentry-cassandra-nodestore/1.1.2/sentry-cassandra-nodestore-1.1.2.tar.gz

---
+++ packages/sentry-cassandra-nodestore/sentry-cassandra-nodestore-1.1.2.tar.gz/sentry-cassandra-nodestore-1.1.2/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+install_requires = [
+    'cassandra-driver'
+]
+
+name = 'sentry-cassandra-nodestore'
+version = '1.1.2'
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name=name,
+    version=version,
+    author='Lumanox',
+    author_email='opensource@lumanox.com',
+    description='A Sentry extension to add Cassandra as a NodeStore backend.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='BSD-style license',
+    zip_safe=False,
+    include_package_data=True,
+    install_requires = [
+        'cassandra-driver',
+    ],
+    packages=[
+        'sentry-cassandra-nodestore',
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
