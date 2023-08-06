57510ca85964fc31877589d9ae9a9527b0064cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:53 +0000

    Add package check-gh-actions/0.1.0/check_gh_actions-0.1.0.tar.gz

---
+++ packages/check-gh-actions/check_gh_actions-0.1.0.tar.gz/check_gh_actions-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['check_gh_actions']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'check-gh-actions',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '# Check gh actions',
+    'author': 'Maxim V4S',
+    'author_email': 'maxim.d@v4scale.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
