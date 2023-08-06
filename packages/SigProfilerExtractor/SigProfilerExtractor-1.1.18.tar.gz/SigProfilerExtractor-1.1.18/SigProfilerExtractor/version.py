2a37f4c59aac433b68b66e9e1fccaaac5e0965fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:31 +0000

    Add package SigProfilerExtractor/1.1.18/SigProfilerExtractor-1.1.18.tar.gz

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.18.tar.gz/SigProfilerExtractor-1.1.18/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.18'
+version = '1.1.18'
+Update = 'Add ID test and SPA version requirement for ID decomposition plot fix.'
+    
+    
