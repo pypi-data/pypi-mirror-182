689bd26c22df417853cc43d60462feea7d8a3afd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:10 +0000

    Add package server-chat-pyqt-december/0.0.1/server_chat_pyqt_december-0.0.1.tar.gz

---
+++ packages/server-chat-pyqt-december/server_chat_pyqt_december-0.0.1.tar.gz/server_chat_pyqt_december-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='server_chat_pyqt_december',
+      version='0.0.1',
+      description='Server packet',
+      packages=find_packages(),
+      author_email='007mail.ru96@mail.ru',
+      author='Magomedov Magomed',
+      install_requeres=['PyQt5', 'sqlalchemy', 'pycruptodome', 'pycryptodomex']
+      )
