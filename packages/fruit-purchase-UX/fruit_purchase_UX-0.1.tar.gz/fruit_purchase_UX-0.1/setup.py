9a60e81cd36bb871361e1779d0bdf70c9ed93295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:59 +0000

    Add package fruit-purchase-UX/0.1/fruit_purchase_UX-0.1.tar.gz

---
+++ packages/fruit-purchase-UX/fruit_purchase_UX-0.1.tar.gz/fruit_purchase_UX-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+name = 'fruit_purchase_UX',
+version = '0.1',
+packages = find_packages(exclude=['Test*']),
+license = 'MIT',
+description = 'A test python package for fruit purchase system.',
+url = 'https://github.com/ujjwal-upadhyay/DATA533_Project_Group_10_Step3',
+author = 'Ujjwal Upadhyay and Xiaoxiao Fu',
+author_email = 'upadhyay.ujjwal007@gmail.com'
+)
