11cc59ec525b6fd468a66daf6f98ffa8f5a25a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:16 +0000

    Add package dsapy/0.0.69/dsapy-0.0.69.tar.gz

---
+++ packages/dsapy/dsapy-0.0.69.tar.gz/dsapy-0.0.69/src/testall.py
@@ -0,0 +1,38 @@
+import dsapy as i
+
+arr1 = [1,2,4,6]
+num = 101010
+arr2 = [3,5,7,8,9]
+arr3 = [3,5,7,7,3,5,232,8,9]
+print(i.array.addtoall(arr1,1))
+print(i.number.factorial(5))
+print(i.number.gcd(4,3))
+print(i.dsa.binarysearch(arr1,0,4,3))
+print(i.dsa.deletion(arr1,2))
+print(i.number.bit.flip(num,1))
+# print(i.number.bit.flip(num,))
+print(i.dsa.mergeSort(arr1,arr2))
+print(i.problem.josephus(50,29))
+print(i.array.removeduplicates(arr3))
+# print(i.array.permutations([1,2,3,4,5]))
+
+print(i.array.powerset([1,2,3]))
+
+print(i.number.primearray(10))
+
+print(i.number.sumtilln(10))
+print(i.number.sumtilln(0))
+# print(i.number.sumtilln("rbher"))
+
+print(i.string.replaceallwith("pip","pi","3.14"))
+print(i.array.matrix.transpose([[1,2],[4,5]]))
+print("===========")
+s = i.stack()
+s.push(10)
+s.push("yo")
+s.push("1")
+print(s)
+a=0
+b=0
+if(a!=b):
+    
