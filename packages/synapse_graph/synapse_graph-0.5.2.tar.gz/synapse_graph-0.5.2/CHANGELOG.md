472aed993777ad277ccf98123eef5cc1eb347c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:37 +0000

    Add package synapse_graph/0.5.2/synapse_graph-0.5.2.tar.gz

---
+++ packages/synapse_graph/synapse_graph-0.5.2.tar.gz/synapse_graph-0.5.2/CHANGELOG.md
@@ -0,0 +1,10 @@
+## 0.4
+
+- Refactoring
+- Temporary deleted external users function
+
+## 0.5
+
+- [X] Relationship between users in DM simplified
+- [ ] More customization in html graph
+- [X] Add security fix for usernames
---
+++ packages/synapse_graph/synapse_graph-0.5.2.tar.gz/synapse_graph-0.5.2/main.py
@@ -0,0 +1,13 @@
+import os
+import synapse_graph
+
+MATRIX_TOKEN = os.getenv('MATRIX_TOKEN')
+MATRIX_HOMESERVER_DOMAIN = os.getenv('MATRIX_HOMESERVER_DOMAIN')
+HEADERS = {
+    'Authorization': f'Bearer {MATRIX_TOKEN}'
+}
+
+graph = synapse_graph.SynapseGraph(MATRIX_HOMESERVER_DOMAIN, HEADERS, MATRIX_HOMESERVER_DOMAIN, hide_usernames=False)
+graph.show()
+
+# print(graph.get_html())
