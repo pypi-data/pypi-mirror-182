2d6b521c35aee3b420ad072d954be97d09dae7e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:54 +0000

    Add package ubcmds/0.3/ubcmds-0.3.tar.gz

---
+++ packages/ubcmds/ubcmds-0.3.tar.gz/ubcmds-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ubcmds',
+    version='0.3',
+    packages=find_packages(exclude=['test*']),
+    license='MIT',
+    description='MDS quiz package',
+	url='https://github.com/Just249/DATA533_OOPProject/tree/main',
+    author='Justin',
+    author_email='chanpaklam321@gmail.com'
+)
