84313173eeb2a31dd10c8bb406d653e2224297eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:58 +0000

    Add package bounciepy/0.2.0/bounciepy-0.2.0.tar.gz

---
+++ packages/bounciepy/bounciepy-0.2.0.tar.gz/bounciepy-0.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+REPO_URL = "https://github.com/mandarons/bounciepy"
+VERSION = "0.2.0"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    required = fh.read().splitlines()
+
+setup(
+    name="bounciepy",
+    version=VERSION,
+    author="Mandar Patil",
+    author_email="mandarons@pm.me",
+    description="Python library to interact with bouncie.com API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=REPO_URL,
+    package_dir={".": ""},
+    packages=find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=required,
+)
