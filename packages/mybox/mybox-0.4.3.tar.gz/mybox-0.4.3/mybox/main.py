a22147f0fea981af5d0e01f134eb211145d9532f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:02 +0000

    Add package mybox/0.4.3/mybox-0.4.3.tar.gz

---
+++ packages/mybox/mybox-0.4.3.tar.gz/mybox-0.4.3/mybox/main.py
@@ -0,0 +1,49 @@
+import argparse
+from typing import Optional
+
+import trio
+from typed_argparse import TypedArgs
+
+from .driver import LocalDriver
+from .package import Package, load_packages
+from .state import DB, DB_PATH
+from .utils import flatten, parallel_map_tqdm
+
+
+class Args(TypedArgs):
+    component: list[str]
+
+
+def parse_args(args: Optional[list[str]] = None) -> Args:
+    result = argparse.ArgumentParser(description="Install the environment")
+    result.add_argument("component", nargs="*", help="The component to install")
+    return Args.from_argparse(result.parse_args(args))
+
+
+async def main():
+    db = DB(DB_PATH)
+    driver = LocalDriver()
+    args = parse_args()
+    components: frozenset[str] = frozenset(args.component) | {"base"}
+    packages = flatten(
+        load_packages(component, db=db, driver=driver) for component in components
+    )
+
+    async def process_and_record(package: Package) -> Optional[Package]:
+        if await package.ensure():
+            return package
+        return None
+
+    results = await parallel_map_tqdm(process_and_record, packages)
+
+    installed = list(filter(None, results))
+    if installed:
+        print(
+            f"{len(installed)} packages installed or updated: {', '.join(p.name for p in installed)}"
+        )
+    else:
+        print("Everything up to date.")
+
+
+def sync_main():
+    trio.run(main)
