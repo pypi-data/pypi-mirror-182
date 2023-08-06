4ecbb92b417e500b0f8863b6512aec734d12dfbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:48 +0000

    Add package xedocs/0.2.3/xedocs-0.2.3.tar.gz

---
+++ packages/xedocs/xedocs-0.2.3.tar.gz/xedocs-0.2.3/xedocs/schemas/__init__.py
@@ -0,0 +1,8 @@
+from .base_schemas import VersionedXeDoc, XeDoc
+from .detector_numbers import DetectorNumber
+from .fax import FaxConfig
+from .corrections import *
+from .calibrations import *
+from .operations_reports import *
+from .analysis import *
+from .pmt_data import *
