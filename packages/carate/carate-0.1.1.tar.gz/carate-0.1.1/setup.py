5bcb2d0b126d2c44b5772d27c5fccee6e23c57c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:56 +0000

    Add package carate/0.1.1/carate-0.1.1.tar.gz

---
+++ packages/carate/carate-0.1.1.tar.gz/carate-0.1.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+    name="carate",
+    version="0.1.1",
+    author="Julian M. Kleber",
+    author_email="julian.kleber@sail.black",
+    description="Graph-based encoder algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://www.codeberg/sail.black/serial-sphinx",
+    packages=setuptools.find_packages(include=["serial_sphinx"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "black",
+        "Click"
+    ],
+    entry_points={
+        'console_scripts': [
+            'carate = carate.main:train_algorithm',
+        ],
+    },
+    python_requires=">=3.9",
+)
+
