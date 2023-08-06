c372f8e1f8b9ac974d50ba083d6865cd9363116d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:32 +0000

    Add package satnogs-webscraper/1.3.5/satnogs_webscraper-1.3.5.tar.gz

---
+++ packages/satnogs-webscraper/satnogs_webscraper-1.3.5.tar.gz/satnogs_webscraper-1.3.5/satnogs_webscraper/observation_dataset.py
@@ -0,0 +1,26 @@
+import json
+import os
+import pandas as pd
+import satnogs_webscraper.constants as cnst
+
+
+def save_dataset(observation_list, save_name):
+    observations = []
+    for observation in observation_list:
+        file_name = os.path.join(cnst.directories['observations'], f'{observation}.json')
+        with open(file_name, "r") as file_in:
+            observations.append(json.load(file_in))
+
+    df = pd.DataFrame(observations)
+    df.to_csv(save_name, index=False)
+
+
+def get_dataset(observation_list):
+    observations = []
+    for observation in observation_list:
+        file_name = os.path.join(cnst.directories['observations'], f'{observation}.json')
+        with open(file_name, "r") as file_in:
+            observations.append(json.load(file_in))
+
+    df = pd.DataFrame(observations)
+    return df
