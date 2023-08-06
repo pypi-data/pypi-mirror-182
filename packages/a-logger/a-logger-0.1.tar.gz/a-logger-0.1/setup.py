fb48ac08cf4d3e9d83cd8d98d2acf46af618bc4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:00 +0000

    Add package a-logger/0.1/a-logger-0.1.tar.gz

---
+++ packages/a-logger/a-logger-0.1.tar.gz/a-logger-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "a-logger",
+    version = "0.1",
+    description="A simple logger",
+    long_description="A simple logger",
+    author="Aryan",
+    url="https://github.com/Aryan3301/logger",
+    install_requires = ['colorama']
+)
