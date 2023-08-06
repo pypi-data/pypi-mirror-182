39d4460526e41df595685af71987497a454d58bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:39 +0000

    Add package whitelist-roblox/0.1.1/whitelist_roblox-0.1.1-py3-none-any.whl

---
+++ packages/whitelist-roblox/whitelist_roblox-0.1.1-py3-none-any.whl/wl_magixx/__init__.py
@@ -0,0 +1,16 @@
+import requests
+
+apikey = ''
+api_url = 'https://1.kelprepl.repl.co/api/v1/'
+
+def add_whitelist(discord_id: int):
+    response = requests.post(api_url + f"premium_add?discord_id={discord_id}&api_key={apikey}")
+    return response.json()
+
+def del_whitelist(discord_id: int):
+    response = requests.post(api_url + f"premium_add?discord_id={discord_id}&api_key={apikey}")
+    return response.json()
+
+def whitelist(discord_id: int):
+    response = requests.post(api_url + f"premium_add?discord_id={discord_id}&api_key={apikey}")
+    return response.json()
