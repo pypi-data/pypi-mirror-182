8e76e247ea908a9c263a01483b0c9294b198263f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:14 +0000

    Add package apmath/0.0.1/apmath-0.0.1.tar.gz

---
+++ packages/apmath/apmath-0.0.1.tar.gz/apmath-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+
+setup(
+    name='apmath', version='0.0.1',
+    author='Aleksander Paradiuk', author_email="olekparadiuk@gmail.com",
+    license='MIT',
+    description='Mathematical packet for python.',
+    packages=find_packages(exclude=("venv", "p1.py", "p2.py")),#, include="packet"),
+    long_description=(this_directory / "README.md").read_text(),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    install_requires=["python_version>='3.8'"]#, "pygame"]
+)
