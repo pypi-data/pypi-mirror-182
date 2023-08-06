4983766e0086aa53e5322961ee734b5bf2eb8d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:43 +0000

    Add package sleepyemoji/0.1.8/sleepyemoji-0.1.8.tar.gz

---
+++ packages/sleepyemoji/sleepyemoji-0.1.8.tar.gz/sleepyemoji-0.1.8/toolchain/animals.py
@@ -0,0 +1,6 @@
+animalsDict = { 
+# Emoji    Discord             ios
+  '🦦' : [':otter:',      'otter'],
+  '🙀' : [':scream_cat:', 'cat face screaming in fear'],
+}
+# '' : ['::', ''],
---
+++ packages/sleepyemoji/sleepyemoji-0.1.8.tar.gz/sleepyemoji-0.1.8/toolchain/hands.py
@@ -0,0 +1,12 @@
+handsDict = { 
+# Emoji    Discord             ios
+  '👌' : [':ok_hand:',     'ok hand sign'],
+  '👍' : [':thumbsup:',    'thumbs up'],
+  '✌ ' : [':v:',           'peace sign hand'],
+  '👎' : [':thumbsdown:',  'thumbs down'],
+  '👏' : [':clap:',        'clapping hands'],
+  '👈' : [':point_left:',  '?'],
+  '👉' : [':point_right:', '?'],
+  '🙌' : [':raised_hands:', 'hands raised in celebration'],
+}
+# '' : ['::', ''],
