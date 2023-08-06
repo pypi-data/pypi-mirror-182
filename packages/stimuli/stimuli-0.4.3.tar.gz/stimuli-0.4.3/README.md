93c3491c070c8471f122260c96d98dd7a92ad784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:40 +0000

    Add package stimuli/0.4.3/stimuli-0.4.3.tar.gz

---
+++ packages/stimuli/stimuli-0.4.3.tar.gz/stimuli-0.4.3/README.md
@@ -0,0 +1,76 @@
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![Imports: isort](https://img.shields.io/badge/%20imports-isort-%231674b1?style=flat&labelColor=ef8336)](https://pycqa.github.io/isort/)
+[![codecov](https://codecov.io/gh/mscheltienne/simple-stimuli/branch/main/graph/badge.svg?token=92BKRPSD0V)](https://codecov.io/gh/mscheltienne/simple-stimuli)
+[![tests](https://github.com/mscheltienne/simple-stimuli/actions/workflows/pytest.yml/badge.svg?branch=main)](https://github.com/mscheltienne/simple-stimuli/actions/workflows/pytest.yml)
+[![build](https://github.com/mscheltienne/simple-stimuli/actions/workflows/build.yml/badge.svg?branch=main)](https://github.com/mscheltienne/simple-stimuli/actions/workflows/build.yml)
+[![doc](https://github.com/mscheltienne/simple-stimuli/actions/workflows/doc.yml/badge.svg?branch=main)](https://github.com/mscheltienne/simple-stimuli/actions/workflows/doc.yml)
+[![PyPI version](https://badge.fury.io/py/stimuli.svg)](https://badge.fury.io/py/stimuli)
+[![Downloads](https://static.pepy.tech/personalized-badge/stimuli?period=total&units=international_system&left_color=grey&right_color=blue&left_text=pypi%20downloads)](https://pepy.tech/project/stimuli)
+[![Conda Version](https://img.shields.io/conda/vn/conda-forge/stimuli.svg)](https://anaconda.org/conda-forge/stimuli)
+[![Conda Downloads](https://img.shields.io/conda/dn/conda-forge/stimuli.svg)](https://anaconda.org/conda-forge/stimuli)
+[![Conda Platforms](https://img.shields.io/conda/pn/conda-forge/stimuli.svg)](https://anaconda.org/conda-forge/stimuli)
+[![DOI](https://zenodo.org/badge/497873209.svg)](https://zenodo.org/badge/latestdoi/497873209)
+
+# Simple-stimuli
+
+This repository contains simple auditory and visual stimuli that do not require
+[PsychoPy](https://www.psychopy.org/). The auditory stimuli use the python
+[sounddevice](https://python-sounddevice.readthedocs.io/en/0.4.4/) library and
+the visual stimuli use the python [opencv](https://docs.opencv.org/4.x/)
+library.
+
+# Installation
+
+This repository is available for `python ≥ 3.8` on `pip` with the command
+`pip install stimuli` or on `conda-forge` with the command
+`conda install -c conda-forge stimuli`.
+
+# Usage
+
+## Audio stimulus
+
+```
+from stimuli.audio import Tone
+
+sound = Tone(volume=80, frequency=1000)
+sound.play()
+```
+
+The volume can be set independently for each channel (stereo) by providing a tuple
+`(L, R)`.
+
+## Visual stimulus
+
+Visual stimulus can be grouped into 2 categories:
+
+- simple visuals that are drawn on top of each other
+- feedback visuals that are drawn once and updated
+
+### Simple visual
+
+```
+from stimuli.visuals import Text
+
+visual = Text()
+visual.background = "lightgrey"  # equivalent to visual.draw_background()
+visual.putText("Top secret not-so-secret instructions!")
+visual.show()
+```
+
+### Feedback visual
+
+```
+import numpy as np
+
+from stimuli.visuals import FillingBar
+
+visual = FillingBar()
+visual.background = "lightgrey"  # equivalent to visual.draw_background()
+visual.putBar(length=200, width=20, margin=2, color="black", fill_color="teal")
+
+for k in np.arange(0, 1, 0.1):
+    visual.fill_perc = k  # update the visual
+    visual.show(100)  # wait 100 ms
+
+visual.close()
+```
---
+++ packages/stimuli/stimuli-0.4.3.tar.gz/stimuli-0.4.3/stimuli/visuals/cross.py
@@ -0,0 +1,128 @@
+from typing import Optional, Tuple, Union
+
+import cv2
+
+from ..utils._checks import _check_type, _ensure_int
+from ..utils._docs import fill_doc
+from .base import BaseVisual
+
+
+@fill_doc
+class Cross(BaseVisual):
+    """Class to display a cross, e.g. a fixation cross.
+
+    Parameters
+    ----------
+    %(visual_window_name)s
+    %(visual_window_size)s
+    """
+
+    def __init__(
+        self,
+        window_name: str = "Visual",
+        window_size: Optional[Tuple[int, int]] = None,
+    ):
+        super().__init__(window_name, window_size)
+
+    @fill_doc
+    def putCross(
+        self,
+        length: int,
+        thickness: int,
+        color: Union[str, Tuple[int, int, int]],
+        position: Union[str, Tuple[int, int]] = "centered",
+    ) -> None:
+        """Draw a cross composed of 2 rectangles.
+
+        The rectangles are defined by length and thickness.
+        The rectangles are positioned to form a cross.
+
+        Parameters
+        ----------
+        length : int
+            Number of pixels used to draw the length of the cross.
+        thickness : int
+            Number of pixels used to draw the thickness of the cross.
+        color : str | tuple
+            Color used to fill the cross.
+        position : str | tuple
+            Position of the center of the cross. See notes for additional
+            information.
+
+        Notes
+        -----
+        %(visual_color)s
+        %(visual_position)s
+        """
+        length = Cross._check_length(length, self._window_size)
+        thickness = Cross._check_thickness(thickness, length)
+        color = BaseVisual._check_color(color)
+        position = Cross._check_position(
+            position, length, self._window_size, self._window_center
+        )
+
+        # Horizontal rectangle
+        # P1 --------------
+        # |                |
+        #  -------------- P2
+        xP1 = position[0] - length // 2
+        yP1 = position[1] - thickness // 2
+        xP2 = xP1 + length
+        yP2 = yP1 + thickness
+        cv2.rectangle(self._img, (xP1, yP1), (xP2, yP2), color, -1)
+
+        # Vertical rectangle
+        # P1 --
+        # |    |
+        # |    |
+        # |    |
+        # |    |
+        # |    |
+        #  -- P2
+        xP1 = position[0] - thickness // 2
+        yP1 = position[1] - length // 2
+        xP2 = xP1 + thickness
+        yP2 = yP1 + length
+        cv2.rectangle(self._img, (xP1, yP1), (xP2, yP2), color, -1)
+
+    # --------------------------------------------------------------------
+    @staticmethod
+    def _check_length(length: int, window_size: Tuple[int, int]) -> int:
+        """Check that the length is valid."""
+        length = _ensure_int(length, "length")
+        assert 0 < length
+        assert all(length <= size for size in window_size)
+        return length
+
+    @staticmethod
+    def _check_thickness(thickness: int, length: int) -> int:
+        """Check that the thickness is valid."""
+        thickness = _ensure_int(thickness, "thickness")
+        assert 0 < thickness
+        assert thickness < length
+        return thickness
+
+    @staticmethod
+    def _check_position(
+        position: Union[str, Tuple[int, int]],
+        length: int,
+        window_size: Tuple[int, int],
+        window_center: Tuple[int, int],
+    ) -> Tuple[int, int]:
+        """Check that the cross position is coherent with the window size.
+
+        The position of the cross is given as the center of the cross.
+        """
+        _check_type(position, (str, tuple), "position")
+        if isinstance(position, str):
+            position = position.lower().strip()
+            assert position in ["centered", "center"]
+            position = window_center
+        for pos in position:
+            _check_type(pos, ("int",))
+        assert len(position) == 2
+        assert 0 <= position[0] - length // 2
+        assert position[0] - length // 2 + length <= window_size[0]
+        assert 0 <= position[1] - length // 2
+        assert position[1] - length // 2 + length <= window_size[1]
+        return position
