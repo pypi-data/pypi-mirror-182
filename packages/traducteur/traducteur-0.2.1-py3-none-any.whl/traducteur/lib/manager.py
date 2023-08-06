266aff992dd480897fbc9e6770a7bc791bd90d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:14 +0000

    Add package traducteur/0.2.1/traducteur-0.2.1-py3-none-any.whl

---
+++ packages/traducteur/traducteur-0.2.1-py3-none-any.whl/traducteur/lib/manager.py
@@ -0,0 +1,192 @@
+from traducteur.lib.context import MongoContext, SQLite3Context
+from bson import ObjectId
+from pymongo.cursor import Cursor
+from pymongo.collection import Collection
+
+class BaseModelManager:
+    def __init__(self, connection_string: str, database_name):
+        self.connection_string = connection_string
+        self.database_name = database_name
+
+    def exists(self, col: str, id: str):
+        pass
+
+    def exists_where(self, col: str, query):
+        pass
+
+    def insert_one(self, item):
+        pass
+
+    def get_one(self, col: str, id: str):
+        pass
+
+    def get_many(self, col: str, query, limit: int):
+        pass
+
+    def update_one(self, update):
+        pass
+
+    def delete_one(self, query):
+        pass
+
+
+
+
+class SQLModelManager(BaseModelManager):
+    def __init__(self, connection_string: str, database_name):
+        super().__init__(connection_string, database_name)
+
+    def __match_python_type_sql(self, type_name: str):
+        match = {
+            'string': 'TEXT',
+            'integer': 'INT',
+            'float': 'FLOAT(24)',
+        }
+
+        if match[type_name] == None:
+            raise Exception('No SQL type found for ' + type_name)
+
+        return match[type_name]
+
+    def __check_table(self, name: str):
+        query = f"SELECT name FROM sqlite_master WHERE type='table' AND name='{name}';"
+        with SQLite3Context(self.connection_string, self.database_name) as db:
+            cursor = db.execute(query)
+            result = cursor.fetchone()
+            if result and len(result) > 0 and result[0] == name:
+                return True
+            return False
+        
+
+    def create_table(self, schema: dict):
+        if self.__check_table(schema['title']):
+            return True
+
+        cols = []
+        for col, val in schema['properties'].items():
+            sql_type = self.__match_python_type_sql(val['type'])
+            req = f"\tNOT NULL" if col in schema['required'] else ""
+            subquery = f"{col}\t{sql_type}{req}"
+            cols.append(subquery)
+
+        query = f"CREATE TABLE {schema['title']} ({', '.join(cols)});"
+        self.query(query)
+
+    # model actions
+    def query(self, query: str):
+        with SQLite3Context(self.connection_string, self.database_name) as db:
+            return db.execute(query)
+
+    def exists(self, col: str, id: str):
+        with SQLite3Context(self.connection_string, self.database_name) as db:
+            query = f"SELECT COUNT(id) FROM {col} WHERE id = ?;"
+            cursor = db.execute(query, id)
+            return len(cursor) >= 1
+
+    def get_one(self, col: str, id: str):
+        with SQLite3Context(self.connection_string, self.database_name) as db:
+            query = f"SELECT * FROM {col} WHERE id = ?;"
+            cursor = db.execute(query, id)
+            return cursor.fetchone()
+
+    def get_many(self, col: str, query, limit: int):
+        with SQLite3Context(self.connection_string, self.database_name) as db:
+            query = f"SELECT * FROM {col} WHERE {', '.join(query)} LIMIT {limit};"
+            cursor = db.execute(query)
+            return cursor.fetchall()
+    
+    def get_all(self, col: str, **kwargs):
+        with SQLite3Context(self.connection_string, self.database_name) as db:
+            query = f"SELECT * FROM {col};"
+            cursor = db.execute(query)
+            return cursor.fetchall()
+
+    def insert_one(self, item):
+        with SQLite3Context(self.connection_string, self.database_name) as db:
+            query = f"INSERT INTO {item._col_name} ({item.sql_columns}) VALUES ({item.q_marks});"
+            cursor = db.execute(query, item.column_values)
+            return cursor.lastrowid
+
+    def update_one(self, update):
+        with SQLite3Context(self.connection_string, self.database_name) as db:
+            query = f"UPDATE {update._col_name} SET {update.sql_update} WHERE id = ?;"
+            cursor = db.execute(query, update.id)
+            return cursor.lastrowid
+
+    def delete_one(self, item):
+        with SQLite3Context(self.connection_string, self.database_name) as db:
+            query = f"DELETE FROM {item._col_name} WHERE id = ?;"
+            cursor = db.execute(query, item.id)
+            return True
+
+
+
+
+class MongoModelManager(BaseModelManager):
+    def __init__(self, connection_string: str, database_name):
+        super().__init__(connection_string, database_name)
+
+    def _after(self, cursor: Cursor, **kwargs):
+        if 'limit' in kwargs:
+            cursor.limit(kwargs.get('limit'))
+
+        if 'sort' in kwargs:
+            if 'sortdirection' in kwargs:
+                cursor.sort(kwargs.get('sort'), kwargs.get('sortdirection'))
+            else:
+                cursor.sort(kwargs.get('sort'))
+
+        return cursor
+
+    def find(self, column: Collection, query = {}, **kwargs):
+        if 'select' in kwargs:
+            return column.find(query, kwargs.get('select'))
+        
+        return column.find(query)
+
+    def find_one(self, column: Collection, query = {}, **kwargs):
+        if 'select' in kwargs:
+            return column.find_one(query, kwargs.get('select'))
+        
+        return column.find_one(query)
+
+    def exists(self, col: str, id: str):
+        with MongoContext(self.connection_string, self.database_name, col) as column:
+            return column.count_documents({'_id': ObjectId(id)}) > 0
+
+    def exists_where(self, col: str, query):
+        with MongoContext(self.connection_string, self.database_name, col) as column:
+            return column.count_documents(query) > 0
+
+    def get_one(self, col: str, id: str, **kwargs):
+        with MongoContext(self.connection_string, self.database_name, col) as column:
+            query = {'_id': ObjectId(id)}
+            return self.find_one(column, query, **kwargs)
+
+    def get_all(self, col: str, **kwargs):
+        with MongoContext(self.connection_string, self.database_name, col) as column:
+            cursor = self.find(column, **kwargs)
+            return list(self._after(cursor, **kwargs))
+
+    def get_many(self, col: str, query, **kwargs):
+        with MongoContext(self.connection_string, self.database_name, col) as column:
+            cursor = self.find(column, query, **kwargs)
+            return list(self._after(cursor, **kwargs))
+
+    def insert_one(self, item):
+        with MongoContext(self.connection_string, self.database_name, item._col_name) as column:
+            result = column.insert_one(item.dict())
+            item.id = result.inserted_id
+            return item
+
+    def update_one(self, update):
+        with MongoContext(self.connection_string, self.database_name, update._col_name) as column:
+            query = {'_id': ObjectId(update.id) }
+            column.update_one(query, {"$set": update.dict()})
+            return update
+
+    def delete_one(self, item):
+        with MongoContext(self.connection_string, self.database_name, item._col_name) as column:
+            query = {'_id': ObjectId(item.id)}
+            column.delete_one(query)
+            return item
---
+++ packages/traducteur/traducteur-0.2.1-py3-none-any.whl/traducteur/lib/mongo.py
@@ -0,0 +1,42 @@
+from pydantic import Field
+from typing import Optional
+from bson import ObjectId
+import os
+
+from traducteur.lib.model import BaseModel
+from traducteur.lib.manager import MongoModelManager
+
+class PydanticObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid objectid')
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')
+
+
+class BaseMongoModel(BaseModel):
+    id: Optional[PydanticObjectId] = Field(alias='_id')
+
+    class Config:
+        arbitrary_types_allowed = True
+        allow_population_by_field_name = True
+        json_encoders = {
+            ObjectId: str,
+            PydanticObjectId: str
+        }
+
+    @classmethod
+    def __get_manager(cls):
+        con_str = os.environ['TRADUCTEUR_CONNECTION_STR']
+        db_name = os.environ['TRADUCTEUR_DATABASE']
+        return MongoModelManager(con_str, db_name)
+
+    
