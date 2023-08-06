5b317fa7528f9252d669246d5c2a42c30b251ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:36 +0000

    Add package BankAccountSystem-XiaCatQ/1.0/BankAccountSystem_XiaCatQ-1.0.tar.gz

---
+++ packages/BankAccountSystem-XiaCatQ/BankAccountSystem_XiaCatQ-1.0.tar.gz/BankAccountSystem_XiaCatQ-1.0/README.md
@@ -0,0 +1,2 @@
+# project-step-3-chenziying
+[![Build Status](https://app.travis-ci.com/XiaCatQ/project-step-3-chenziying.svg?branch=main)](https://app.travis-ci.com/XiaCatQ/project-step-3-chenziying)
---
+++ packages/BankAccountSystem-XiaCatQ/BankAccountSystem_XiaCatQ-1.0.tar.gz/BankAccountSystem_XiaCatQ-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BankAccountSystem_XiaCatQ',
+    version='1.0',
+    packages=['BankAccountSystem'],
+    license='MIT',
+    description='A system to simulate bank GUI python package',
+	url='https://github.com/XiaCatQ/project-step-3-chenziying/tree/main/BankAccountSystem',
+    author='XiaCatQ',
+    author_email='boziying@gmail.com'
+)
