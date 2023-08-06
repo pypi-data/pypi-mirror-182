536dbad77dd50e4ca5df3b8722e4879f172491aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:59:49 +0000

    Add package srg/0.0.1/srg-0.0.1-py3-none-any.whl

---
+++ packages/srg/srg-0.0.1-py3-none-any.whl/srg/__init__.py
@@ -0,0 +1,19 @@
+import sipher
+from sipher import *
+import srutil
+from srutil import *
+import zenpass
+from zenpass import *
+import browseon
+from browseon import *
+import savescreen
+from savescreen import *
+import ioutil
+from ioutil import *
+import local
+from local import *
+
+__author__ = 'srg'
+__version__ = '0.0.1'
+
+__all__ = [var for var in dir() if not var.startswith("__")]
