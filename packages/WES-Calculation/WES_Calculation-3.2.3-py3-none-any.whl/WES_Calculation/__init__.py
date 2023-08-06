92c1e8f83cac5627852648d5e3ac410eff9da866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:20 +0000

    Add package WES-Calculation/3.2.3/WES_Calculation-3.2.3-py3-none-any.whl

---
+++ packages/WES-Calculation/WES_Calculation-3.2.3-py3-none-any.whl/WES_Calculation/__init__.py
@@ -0,0 +1,18 @@
+'''
+Date         : 2022-12-18 12:32:53
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2022-12-18 12:47:54
+LastEditors  : BDFD
+Description  : 
+FilePath     : \WES_Calculation\__init__.py
+Copyright (c) 2022 by BDFD, All Rights Reserved. 
+'''
+
+from WES_Calculation.templateproj import add_one
+from WES_Calculation.templateproj import add_two
+from WES_Calculation.gumbel import gumbel
+from WES_Calculation.greenampt import greenampt
+from WES_Calculation.wavespectra import wave
+from WES_Calculation.windspeed import windspeed
+
