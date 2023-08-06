6eeaa013658163af812a75ddaa66d1fbf1b1d4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:30 +0000

    Add package hspylib/0.11.143/hspylib-0.11.143.tar.gz

---
+++ packages/hspylib/hspylib-0.11.143.tar.gz/hspylib-0.11.143/hspylib/modules/cli/__init__.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.143
+#
+# Package: main.hspylib.modules.cli
+"""Package initialization."""
+
+__all__ = [
+  'icons', 
+  'keyboard', 
+  'tui', 
+  'vt100'
+]
+__version__ = '0.11.143'
