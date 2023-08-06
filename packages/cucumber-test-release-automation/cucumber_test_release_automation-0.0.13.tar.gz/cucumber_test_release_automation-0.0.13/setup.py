a21606f55b3b4eb1137f0dbf372004bdfeb22860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:44 +0000

    Add package cucumber-test-release-automation/0.0.13/cucumber_test_release_automation-0.0.13.tar.gz

---
+++ packages/cucumber-test-release-automation/cucumber_test_release_automation-0.0.13.tar.gz/cucumber_test_release_automation-0.0.13/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cucumber_test_release_automation']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'cucumber-test-release-automation',
+    'version': '0.0.13',
+    'description': '',
+    'long_description': '',
+    'author': 'Matt Wynne',
+    'author_email': 'matt@cucumber.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
