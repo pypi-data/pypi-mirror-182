eb4c43c3b5b38b873d523020d495ff7f9f144195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:20 +0000

    Add package mypydbsimple/1.0.0/mypydbsimple-1.0.0.tar.gz

---
+++ packages/mypydbsimple/mypydbsimple-1.0.0.tar.gz/mypydbsimple-1.0.0/src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mypydbsimple',
+      version='1.0.1',
+      description='Simple mysql db wrapper for python',
+      url='https://github.com/mikerr1/mypydbsimple',
+      author='miker1',
+      author_email='michaelraymond2@gmail.com',
+      license='MIT',
+      packages=['mypydbsimple'],
+      zip_safe=False)
