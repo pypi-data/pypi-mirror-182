e04a77cbb4a2efdbf92c91c7369c72c461851410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:13 +0000

    Add package climetlab-eumetnet-postprocessing-benchmark/0.2.3/climetlab_eumetnet_postprocessing_benchmark-0.2.3.tar.gz

---
+++ packages/climetlab-eumetnet-postprocessing-benchmark/climetlab_eumetnet_postprocessing_benchmark-0.2.3.tar.gz/climetlab_eumetnet_postprocessing_benchmark-0.2.3/climetlab_eumetnet_postprocessing_benchmark/config.py
@@ -0,0 +1,4 @@
+
+baseurl = "https://storage.ecmwf.europeanweather.cloud/eumetnet-postprocessing-benchmark-training-dataset/"
+
+EUPP_baseurl = "https://storage.ecmwf.europeanweather.cloud/eumetnet-postprocessing-benchmark-1st-phase-training-dataset/"
