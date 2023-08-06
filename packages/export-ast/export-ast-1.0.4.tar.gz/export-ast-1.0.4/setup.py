e3108b3250f66d34de07f6ee7c24f97f05828b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:12 +0000

    Add package export-ast/1.0.4/export-ast-1.0.4.tar.gz

---
+++ packages/export-ast/export-ast-1.0.4.tar.gz/export-ast-1.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+long_description = open("README.md").read()
+
+setup(
+    name="export-ast",
+    version="1.0.4",
+    author="Tom Draper",
+    author_email="tomjdraper1@gmail.com",
+    license="MIT",
+    description="Export a Python AST to a dictionary.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tom-draper/ast-export",
+    key_words="ast syntax trees export dict json dictionary",
+    install_requires=[],
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
