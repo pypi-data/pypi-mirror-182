aed9d8eb1350e62af6f0b738d99f1fca4c32695e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:42 +0000

    Add package windrose/1.8.1/windrose-1.8.1.tar.gz

---
+++ packages/windrose/windrose-1.8.1.tar.gz/windrose-1.8.1/windrose/__init__.py
@@ -0,0 +1,24 @@
+"""Draw windrose (also known as a polar rose plot)"""
+
+from matplotlib.projections import register_projection
+
+from .windrose import D_KIND_PLOT  # noqa
+from .windrose import DEFAULT_THETA_LABELS  # noqa
+from .windrose import DPI_DEFAULT  # noqa
+from .windrose import FIGSIZE_DEFAULT  # noqa
+from .windrose import WindAxes  # noqa
+from .windrose import WindAxesFactory  # noqa
+from .windrose import WindroseAxes  # noqa
+from .windrose import clean  # noqa
+from .windrose import clean_df  # noqa
+from .windrose import plot_windrose  # noqa
+from .windrose import plot_windrose_df  # noqa
+from .windrose import plot_windrose_np  # noqa
+from .windrose import wrbar  # noqa
+from .windrose import wrbox  # noqa
+from .windrose import wrcontour  # noqa
+from .windrose import wrcontourf  # noqa
+from .windrose import wrpdf  # noqa
+from .windrose import wrscatter  # noqa
+
+register_projection(WindroseAxes)
