2a78bbc35a17bd67618697a47eb2f7a29264f39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:23 +0000

    Add package my-own-package/0.1.0/my_own_package-0.1.0.tar.gz

---
+++ packages/my-own-package/my_own_package-0.1.0.tar.gz/my_own_package-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    main="alex-moldovan-first-package",
+    version="0.1.0",
+    author="Alex Moldovan",
+    author_email="alex_moldovan1@yahoo.com",
+    packages=["my_own_package"],
+    package_dir={"": "src\\"},
+    include_package_data=True,
+    description="my_first_package"
+)
