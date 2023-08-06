7aad60fa5bd5e55760ecae271fee82c193a02989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:02 +0000

    Add package MPSPlots/0.1.6/MPSPlots-0.1.6-py3-none-any.whl

---
+++ packages/MPSPlots/MPSPlots-0.1.6-py3-none-any.whl/MPSPlots/__init__.py
@@ -0,0 +1,12 @@
+from . import CMAP
+from .Render3D import Scene3D
+from .Render2D import ( Scene2D, 
+	                    Axis, 
+	                    ColorBar, 
+	                    Contour,
+	                    Mesh,
+	                    FillLine,
+	                    STDLine,
+	                    Text,
+	                    Line,
+	                    Multipage )
