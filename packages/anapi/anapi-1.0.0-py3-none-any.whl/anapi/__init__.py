173e2272961de5e9872d48e6a836999ad94c8637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:33 +0000

    Add package anapi/1.0.0/anapi-1.0.0-py3-none-any.whl

---
+++ packages/anapi/anapi-1.0.0-py3-none-any.whl/anapi/__init__.py
@@ -0,0 +1,15 @@
+import requests
+
+class ChatAI:
+    def __init__(self, version="last", api_key="standart_key"):
+        if version=="last":
+            self.v = "last"
+        else:
+            self.v = f"v{version}"
+
+        self.api_key = api_key
+
+    def get_answer(self, text):
+        return requests.get(
+            f"https://anapi.wsm001.repl.co/api/{self.v}",
+            json={"text":text, "id":self.id}).json()["answ"]
