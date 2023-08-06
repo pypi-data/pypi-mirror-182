900657bfb21eff2112483bff55f945ff3be3a6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:58 +0000

    Add package labelu/0.5.0/labelu-0.5.0-py3-none-any.whl

---
+++ packages/labelu/labelu-0.5.0-py3-none-any.whl/labelu/internal/statics/static/js/12.48591ff2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpweb=this.webpackJsonpweb||[]).push([[12],{967:function(e,s,t){"use strict";t.r(s),s.default=t.p+"static/media/lineImg.cf097119.png"}}]);
+//# sourceMappingURL=12.48591ff2.chunk.js.map
---
+++ packages/labelu/labelu-0.5.0-py3-none-any.whl/labelu/internal/statics/static/js/15.720526b2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpweb=this.webpackJsonpweb||[]).push([[15],{970:function(t,e,s){"use strict";s.r(e),e.default=s.p+"static/media/rectImg.2c8bbdaf.png"}}]);
+//# sourceMappingURL=15.720526b2.chunk.js.map
