733c30e7b831a06d216d7eaf51dd0e0c349bc94a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:52 +0000

    Add package cloudzero-uca-tools/0.7.2/cloudzero-uca-tools-0.7.2.zip

---
+++ packages/cloudzero-uca-tools/cloudzero-uca-tools-0.7.2.zip/cloudzero-uca-tools-0.7.2/uca/exceptions.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2021 CloudZero, Inc. All rights reserved.
+# Licensed under the BSD License. See LICENSE file in the project root for full license information.
+# Direct all questions to support@cloudzero.com
+
+class MalformedUrl(Exception):
+    """Thrown when we have a malformed URL"""
+    pass
+
+
+class InvalidDate(Exception):
+    """Thrown when we have a un-parsable date"""
+    pass
