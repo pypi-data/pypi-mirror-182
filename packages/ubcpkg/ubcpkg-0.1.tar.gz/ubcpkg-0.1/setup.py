e69f5c6ffaadb734dc70fb2bbee0c8046141fe67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:17 +0000

    Add package ubcpkg/0.1/ubcpkg-0.1.tar.gz

---
+++ packages/ubcpkg/ubcpkg-0.1.tar.gz/ubcpkg-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ubcpkg',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='MDS quiz package',
+	url='https://github.com/lilitang2022/InClassCI2022.git',
+    author='Justin',
+    author_email='chanpaklam321@gmail.com'
+)
