f3feeedc6e3f2476d869701f97a738df6b5164dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:36 +0000

    Add package hibernation-no1/0.1.14/hibernation_no1-0.1.14-py3-none-any.whl

---
+++ packages/hibernation-no1/hibernation_no1-0.1.14-py3-none-any.whl/hibernation_no1/mmdet/__init__.py
@@ -0,0 +1,74 @@
+
+from .checkpoint import load_checkpoint
+from .registry import Registry, build_from_cfg
+from .utils import to_2tuple, to_tensor, load_ext
+
+
+from .data.api.coco import COCO
+
+from .data.dataset.datacontainer import DataContainer
+from .data.dataset.dataloader import build_dataloader
+from .data.dataset.dataset import build_dataset, CustomDataset
+from .data.dataset.sampler import GroupSampler
+
+from .data.transforms.collect import Collect
+from .data.transforms.compose import Compose
+from .data.transforms.defaultformatbundle import DefaultFormatBundle
+from .data.transforms.loadannotations import LoadAnnotations
+from .data.transforms.loadimagefronfile import LoadImageFromFile
+from .data.transforms.normalize import Normalize
+from .data.transforms.pad import Pad
+from .data.transforms.randomflip import RandomFlip
+from .data.transforms.resize import Resize
+from .data.transforms.utils import imrescale, rescale_size, imresize, imflip
+
+
+from .modules.base.module import BaseModule, ModuleList
+
+from .modules.base.initialization.constant import constant_init
+from .modules.base.initialization.initialize import initialize
+from .modules.base.initialization.kaiming import kaiming_init
+from .modules.base.initialization.normal import NormalInit, trunc_normal_init
+from .modules.base.initialization.utils import BaseInit, update_init_info, _no_grad_trunc_normal_
+from .modules.base.initialization.xavier import XavierInit
+
+from .modules.maskrcnn.bbox_head import RoIAlignFunction
+from .modules.maskrcnn.maskrcnn import MaskRCNN
+
+from .modules.swin_transformer.adaptivepadding import AdaptivePadding
+from .modules.swin_transformer.ffn import FFN
+from .modules.swin_transformer.patch import PatchEmbed, PatchMerging
+from .modules.swin_transformer.shiftwindow_msa import ShiftWindowMSA, WindowMSA
+from .modules.swin_transformer.swin_block import SwinBlockSequence, SwinBlock
+from .modules.swin_transformer.swin_transformer import SwinTransformer
+
+
+__all__ = [
+    "load_checkpoint", 
+    "Registry", "build_from_cfg", 
+    'to_2tuple', 'to_tensor', 'load_ext',
+    
+    
+    "COCO",
+    
+    "Collect", 'Compose', "DefaultFormatBundle", "LoadAnnotations", "LoadImageFromFile", "Normalize", "Pad", "RandomFlip", "Resize",
+    "imrescale", "rescale_size", "imresize", "imflip",
+    
+    'DataContainer', "build_dataset", "CustomDataset", "GroupSampler", "build_dataloader",
+
+    
+    "BaseModule", "ModuleList",
+    
+    "initialize", 
+    "NormalInit", "XavierInit", "kaiming_init", "constant_init",
+    "BaseInit", "update_init_info", "_no_grad_trunc_normal_", "trunc_normal_init",
+    
+    "MaskRCNN", "RoIAlignFunction",
+    
+    'AdaptivePadding', "FFN", "PatchEmbed", "PatchMerging", "ShiftWindowMSA", 'WindowMSA', "SwinBlockSequence", 'SwinBlock', "SwinTransformer"
+
+]
+
+
+
+
