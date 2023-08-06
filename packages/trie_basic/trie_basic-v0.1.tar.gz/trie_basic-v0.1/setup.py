b50a08b4ca331ee946e5871733aab811ba201fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:10 +0000

    Add package trie_basic/0.1/trie_basic-v0.1.tar.gz

---
+++ packages/trie_basic/trie_basic-v0.1.tar.gz/trie_basic-v0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'trie_basic',
+    packages = ['trie'],
+    version = 'v0.1',  
+    description = 'trie package',
+    author = 'asif',
+    author_email = 'asifiqbal7.iitkgp@gmail.com',
+    url = 'https://github.com/MaxAsif/trie',
+    download_url = 'https://github.com/MaxAsif/trie',
+    keywords = ['trie', 'v0.1'],
+    classifiers = [],
+)
