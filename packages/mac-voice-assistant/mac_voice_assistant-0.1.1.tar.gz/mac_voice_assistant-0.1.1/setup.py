4abb2892b6947a104aca084f5123bfdbbb99e1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:57 +0000

    Add package mac-voice-assistant/0.1.1/mac_voice_assistant-0.1.1.tar.gz

---
+++ packages/mac-voice-assistant/mac_voice_assistant-0.1.1.tar.gz/mac_voice_assistant-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+setup(
+    name='mac_voice_assistant',
+    version='0.1.1',
+    packages=find_packages(),
+    url='https://pypi.org/project/mac-voice-assistant/',
+    license='MIT',
+    author='Manish Raj',
+    author_email='manishraj1.618@gmail.com',
+    description='A generic voice assistant',
+    long_description=read('README.md'),
+    keywords="mac voice assistant",
+    install_requires=['numpy', 'nltk', 'tensorflow', 'portaudio', 'PyAudio', 'playsound', 'pyttsx3', 'SpeechRecognition'],
+)
