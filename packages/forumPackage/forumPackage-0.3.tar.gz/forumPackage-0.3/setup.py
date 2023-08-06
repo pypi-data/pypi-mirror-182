af3741c4d0f3db247d1b4b8ffda38724cca7cbc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:38 +0000

    Add package forumPackage/0.3/forumPackage-0.3.tar.gz

---
+++ packages/forumPackage/forumPackage-0.3.tar.gz/forumPackage-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='forumPackage',
+    version='0.3',
+    packages=find_packages(exclude=['Test*']),
+    license='MIT',
+    description='A python package about forum',
+    url='https://github.com/madison13g/Group-8-Step3',
+    author='Amethyst He',
+    author_email='hejinghongyue@outlook.com'
+)
