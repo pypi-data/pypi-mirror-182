8963e13bc7046c11d09e9f6b29971d87a59e07d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:18 +0000

    Add package td-grpc-reader/0.9/td_grpc_reader-0.9.tar.gz

---
+++ packages/td-grpc-reader/td_grpc_reader-0.9.tar.gz/td_grpc_reader-0.9/README.md
@@ -0,0 +1,3 @@
+# Teradata gRPC Connector
+
+<p>This package reads data from Teradata over gRPC Channel</p>
---
+++ packages/td-grpc-reader/td_grpc_reader-0.9.tar.gz/td_grpc_reader-0.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='td_grpc_reader',
+    version='0.9',
+    license='MIT',
+    author="Emre Yiğit",
+    author_email='emre.yigit01@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/bobmerevel/td_grpc_reader',
+    keywords='example project',
+    install_requires=[
+        'urllib3',
+        'grpcio-tools'
+    ],
+
+)
